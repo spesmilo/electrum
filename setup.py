f0f8d9b1f96c3d859a25564af443e8abe04e97e1
ThomasV <thomasv@gitorious>
2011-11-10 01:25:03 +0100

    distutils

---
+++ setup.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python
+from distutils.core import setup
+
+setup(name = "Electrum",
+    version = "0.2",
+    description = "Lightweight Bitcoin Wallet",
+    author = "thomasv",
+    license = "GNU GPLv3",
+    url = "http://ecdsa/electrum",
+    long_description = """Lightweight Bitcoin Wallet""" 
+) 
