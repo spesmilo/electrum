28427f0cfe20567606e1beb6b9c7110620bfaa7c
ThomasV <thomasv@gitorious>
2012-05-10 13:10:18 +0400

    remove webserver script from setup.py

--- setup.py
+++ setup.py
@@ -14,19 +14,4 @@ setup(name = "Electrum",
     long_description = """Lightweight Bitcoin Wallet""" 
 ) 
 
-if __name__ == '__main__':
-    import sys, re, shutil
-    if sys.argv[1]=='sdist':
-        _tgz="Electrum-%s.tar.gz"%version
-        _zip="Electrum-%s.zip"%version
-        shutil.copyfile("dist/"+_tgz ,'/var/www/electrum/'+_tgz)
-        shutil.copyfile("dist/"+_zip,'/var/www/electrum/'+_zip)
-        f = open("/var/www/electrum/index.html")
-        s = f.read()
-        f.close()
-        s = re.sub("Electrum-([\d\.a-z]*?)\.tar\.gz", _tgz, s)
-        s = re.sub("Electrum-([\d\.a-z]*?)\.zip", _zip, s)
-        f = open("/var/www/electrum/index.html","w")
-        f.write(s)
-        f.close()
         
