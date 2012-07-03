d0c1dfaa884d96bd930aa07bc88c2929db7f97bd
ThomasV <thomasv@gitorious>
2012-07-03 15:05:53 +0200

    version 0.61

--- lib/version.py
+++ lib/version.py
@@ -1,3 +1,3 @@
-ELECTRUM_VERSION = "0.60"
+ELECTRUM_VERSION = "0.61"
 SEED_VERSION = 4  # bump this everytime the seed generation is modified
-TRANSLATION_ID = 28079 # version of the wiki page 
+TRANSLATION_ID = 28344 # version of the wiki page 
