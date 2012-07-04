15a8e1167bbfcdd9f989138cda05622c463115ed
Amir Taaki <genjix@riseup.net>
2012-07-05 01:25:05 +0200

    get_resource_path("foo.png") -> "/usr/share/electrum/foo.png"

---
+++ util.py
@@ -0,0 +1,17 @@
+import os
+import platform
+import sys
+
+def appdata_dir():
+    if platform.system() == "Windows":
+        return os.path.join(os.environ["APPDATA"], "Electrum")
+    elif platform.system() == "Linux":
+        return os.path.join(sys.prefix, "share", "electrum")
+    elif platform.system() == "Darwin":
+        return "/Library/Application Support/Electrum"
+    else:
+        raise Exception("Unknown system")
+
+def get_resource_path(filename):
+    return os.path.join(appdata_dir(), filename)
+
