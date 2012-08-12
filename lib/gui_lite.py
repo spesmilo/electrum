1a26fa87d31e0556c9852bf8217ba365b713b088
Amir Taaki <genjix@riseup.net>
2012-08-12 20:25:31 +0100

    Rename main menu to 'Bitcoin' from 'Electrum'.

--- lib/gui_lite.py
+++ lib/gui_lite.py
@@ -175,20 +175,20 @@ class MiniWindow(QDialog):
         main_layout.addWidget(self.send_button, 2, 1)
 
         menubar = QMenuBar()
-        electrum_menu = menubar.addMenu(_("&Electrum"))
-        #electrum_menu.addMenu(_("&Servers"))
-        #electrum_menu.addSeparator()
+        electrum_menu = menubar.addMenu(_("&Bitcoin"))
+        electrum_menu.addMenu(_("&Servers"))
+        electrum_menu.addSeparator()
         electrum_menu.addAction(_("&Quit"))
 
         view_menu = menubar.addMenu(_("&View"))
         expert_gui = view_menu.addAction(_("&Pro Mode"))
         self.connect(expert_gui, SIGNAL("triggered()"), expand_callback)
-        #view_menu.addMenu(_("&Themes"))
-        #view_menu.addSeparator()
-        #view_menu.addAction(_("Show History"))
+        view_menu.addMenu(_("&Themes"))
+        view_menu.addSeparator()
+        view_menu.addAction(_("Show History"))
 
-        #settings_menu = menubar.addMenu(_("&Settings"))
-        #settings_menu.addAction(_("&Configure Electrum"))
+        settings_menu = menubar.addMenu(_("&Settings"))
+        settings_menu.addAction(_("&Configure Electrum"))
         
         help_menu = menubar.addMenu(_("&Help"))
         help_menu.addAction(_("&Contents"))
