66ede2e03ea84e5dda4279bda94840a26edecdb6
Jimbo77 <onlineregular@gmail.com>
2012-08-19 18:28:17 -0700

    created docstring for local_data_dir()

--- lib/util.py
+++ lib/util.py
@@ -3,14 +3,14 @@ import platform
 import sys
 
 def print_error(*args):
-    '''Print out the arguments to standard error.'''
+    """Print out the arguments to standard error."""
     for item in args:
       sys.stderr.write(str(item))
     sys.stderr.write("\n")
     sys.stderr.flush()
 
 def appdata_dir():
-    '''Find the path to the application data directory; add an electrum folder and return path.'''
+    """Find the path to the application data directory; add an electrum folder and return path."""
     if platform.system() == "Windows":
         return os.path.join(os.environ["APPDATA"], "Electrum")
     elif platform.system() == "Linux":
@@ -25,6 +25,7 @@ def get_resource_path(*args):
     return os.path.join(".", *args)
 
 def local_data_dir():
+    """Return path to the data folder."""
     assert sys.argv
     prefix_path = os.path.dirname(sys.argv[0])
     local_data = os.path.join(prefix_path, "data")
