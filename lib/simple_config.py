15eb4d7cb8fa8d64f52ac550c16e9629a722cb26
Amir Taaki <genjix@riseup.net>
2012-09-19 16:47:39 +0100

    reformat simple_config to comply with electrum and create config dir if it doesnt exist.

--- lib/simple_config.py
+++ lib/simple_config.py
@@ -3,36 +3,40 @@ import os
 from util import user_dir
 
 class SimpleConfig:
-  default_options = {"gui": "lite"}
 
-  def set_key(self, key, value, save = True):
-    self.config[key] = value
-    if save == True:
-      self.save_config()
-
-  def save_config(self):
-    f = open(self.config_file_path(), "w+")
-    f.write(json.dumps(self.config))
-
-  def load_config(self):
-    f = open(self.config_file_path(), "r")
-    file_contents = f.read()
-    if file_contents:
-      self.config = json.loads(file_contents)
-    else:
-      self.config = self.default_options
-      self.save_config()
-  
-  def config_file_path(self):
-    return "%s" % (self.config_folder + "/config.json")
-
-  def __init__(self):
-    # Find electrum data folder
-    self.config_folder = user_dir()
-    # Read the file
-    if os.path.exists(self.config_file_path()):
-      self.load_config()
-    else:
-      self.config = self.default_options
-      self.save_config()
+    default_options = {"gui": "lite"}
+    
+    def __init__(self):
+        # Find electrum data folder
+        self.config_folder = user_dir()
+        # Read the file
+        if os.path.exists(self.config_file_path()):
+            self.load_config()
+        else:
+            self.config = self.default_options
+            # Make config directory if it does not yet exist.
+            if not os.path.exists(self.config_folder):
+                os.mkdir(self.config_folder)
+            self.save_config()
         
+    def set_key(self, key, value, save = True):
+        self.config[key] = value
+        if save == True:
+            self.save_config()
+    
+    def save_config(self):
+        f = open(self.config_file_path(), "w+")
+        f.write(json.dumps(self.config))
+    
+    def load_config(self):
+        f = open(self.config_file_path(), "r")
+        file_contents = f.read()
+        if file_contents:
+            self.config = json.loads(file_contents)
+        else:
+            self.config = self.default_options
+            self.save_config()
+    
+    def config_file_path(self):
+        return "%s" % (self.config_folder + "/config.json")
+
