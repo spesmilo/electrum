5bb441ce3768f9daacb2453067df90fecba85595
ThomasV <thomasv@gitorious>
2012-11-04 19:40:17 +0100

    add mnemonic functions to module

--- lib/__init__.py
+++ lib/__init__.py
@@ -4,3 +4,5 @@ from verifier import WalletVerifier
 from interface import Interface, pick_random_server, DEFAULT_SERVERS
 from simple_config import SimpleConfig
 import bitcoin
+from mnemonic import mn_encode as mnemonic_encode
+from mnemonic import mn_decode as mnemonic_decode
