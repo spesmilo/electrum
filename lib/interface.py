8f85187545ed22ea04570a821b893b39532db703
thomasv <thomasv@gitorious>
2012-11-06 11:05:04 +0100

    readding bysh.me server

--- lib/interface.py
+++ lib/interface.py
@@ -29,9 +29,9 @@ DEFAULT_SERVERS = [
     #'electrum.novit.ro:50001:t', 
     #'electrum.bitcoins.sk:50001:t',
     #'uncle-enzo.info:50001:t',
-    #'electrum.bysh.me:50001:t',
     #'electrum.bitcoin.cz:50001:t',
     #'electrum.bitfoo.org:50001:t',
+    'electrum.bysh.me:50001:t',
     'electrum.pdmc.net:50001:t',
     'ecdsa.org:50001:t'
     ]
