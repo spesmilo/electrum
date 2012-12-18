a9181a38f2b9b4f9403ebfa3f6092c16db7b3d08
ThomasV <thomasv@gitorious>
2012-12-18 17:44:22 +0100

    bump version number for translations

--- lib/version.py
+++ lib/version.py
@@ -1,4 +1,4 @@
 ELECTRUM_VERSION = "1.5.7"  # version of the client package
 PROTOCOL_VERSION = '0.6'  # protocol version requested
 SEED_VERSION     = 4      # bump this everytime the seed generation is modified
-TRANSLATION_ID   = 32150  # version of the wiki page 
+TRANSLATION_ID   = 33853  # version of the wiki page 
