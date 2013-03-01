19d104f023890a0cdfc5be947a6b9beb47d25b2e
thomasv <thomasv@gitorious>
2013-03-01 13:33:11 +0100

    register commands, check for their arguments

--- lib/commands.py
+++ lib/commands.py
@@ -22,84 +22,59 @@ from bitcoin import *
 from decimal import Decimal
 import bitcoin
 
-known_commands = {
-    'help':'Prints this help',
-    'validateaddress':'Check that the address is valid', 
-    'getbalance': "Display the balance of your wallet or of an address.\nSyntax: getbalance [<address>]", 
-    'contacts': "Show your list of contacts", 
-    'create':'Create a wallet', 
-    'restore':'Restore a wallet', 
-    'payto':"""Create and broadcast a transaction.
-Syntax: payto <recipient> <amount> [label]
-<recipient> can be a bitcoin address or a label
-options:\n  --fee, -f: set transaction fee\n  --fromaddr, -s: send from address -\n  --changeaddr, -c: send change to address
-            """,
-    'sendrawtransaction':
-            'Broadcasts a transaction to the network. \nSyntax: sendrawtransaction <tx in hexadecimal>',
-    'password': 
-            "Changes your password",
-    'listaddresses':
-            """Shows your list of addresses.
-options:
-  -a: show all addresses, including change addresses
-  -b: include balance in results
-  -l: include labels in results
-  """,
+known_commands = {}
+offline_commands = []
+protected_commands = []
 
-    'history':"Shows the transaction history",
-    'setlabel':'Assign a label to an item\nSyntax: label <tx_hash> <label>',
-    'mktx':
-        """Create a signed transaction, password protected.
-Syntax: mktx <recipient> <amount> [label]
-options:\n  --fee, -f: set transaction fee\n  --fromaddr, -s: send from address -\n  --changeaddr, -c: send change to address
-        """,
-    'getseed':
-            "Print the generation seed of your wallet.",
-    'importprivkey': 
-            'Import a private key\nSyntax: importprivkey <privatekey>',
-    'signmessage':
-            'Signs a message with a key\nSyntax: signmessage <address> <message>\nIf you want to lead or end a message with spaces, or want double spaces inside the message make sure you quote the string. I.e. " Hello  This is a weird String "',
-    'verifymessage':
-             'Verifies a signature\nSyntax: verifymessage <address> <signature> <message>\nIf you want to lead or end a message with spaces, or want double spaces inside the message make sure you quote the string. I.e. " Hello  This is a weird String "',
-    'eval':  
-             "Run python eval() on an object\nSyntax: eval <expression>\nExample: eval \"wallet.aliases\"",
-    'get': 
-             "Get config parameter.",
-    'set': 
-             "Set config parameter.",
-    'deseed':
-            "Create a seedless, watching-only wallet.",
-    'freeze':'',
-    'unfreeze':'',
-    'prioritize':'',
-    'unprioritize':'',
-    'dumpprivkey':'similar to bitcoind\'s command',
-    'dumpprivkeys':'dump all private keys',
-    'listunspent':'similar to bitcoind\'s command',
-    'createmultisig':'similar to bitcoind\'s command',
-    'createrawtransaction':'similar to bitcoind\'s command',
-    'decoderawtransaction':'similar to bitcoind\'s command',
-    'signrawtransaction':'similar to bitcoind\'s command',
-    'get_history': 'get history for an address'
+def register_command(name, min_args, max_args, is_protected, is_offline, description, syntax = '', options_syntax = ''):
+    global known_commands, protected_commands, offline_commands
+    known_commands[name] = (min_args, max_args, description, syntax, options_syntax)
+    if is_protected:
+        protected_commands.append(name)
+    if is_offline:
+        offline_commands.append(name)
+
+
+payto_options = ' --fee, -f: set transaction fee\n --fromaddr, -s: send from address -\n --changeaddr, -c: send change to address'
+listaddr_options = " -a: show all addresses, including change addresses\n -b: include balance in results\n -l: include labels in results"
+
+
+register_command('contacts',             0, 0, False, True,  'Show your list of contacts')
+register_command('create',               0, 0, False, True,  'Create a new wallet')
+register_command('createmultisig',       2, 2, False, True,  'similar to bitcoind\'s command')
+register_command('createrawtransaction', 2, 2, False, True,  'similar to bitcoind\'s command')
+register_command('deseed',               0, 0, False, True,  'Create a seedless, watching-only wallet.')
+register_command('decoderawtransaction', 1, 1, False, True,  'similar to bitcoind\'s command')
+register_command('dumpprivkey',          1, 1, True,  True,  'similar to bitcoind\'s command')
+register_command('dumpprivkeys',         0, 0, True,  True,  'dump all private keys')
+register_command('eval',                 1, 1, False, True,  'Run python eval() on an object', 'eval <expression>\nExample: eval \"wallet.aliases\"')
+register_command('freeze',               1, 1, False, True,  'Freeze the funds at one of your wallet\'s addresses')
+register_command('getbalance',           0, 1, False, False, 'Display the balance of your wallet or of an address', 'getbalance [<address>]')
+register_command('getaddresshistory',    1, 1, False, False, 'get history for an address')
+register_command('getconfig',            1, 1, False, True,  'Return a configuration variable', 'getconfig <name>')
+register_command('getseed',              0, 0, True,  True,  'Print the generation seed of your wallet.')
+register_command('help',                 0, 1, False, True,  'Prints this help')
+register_command('history',              0, 0, False, False, 'Returns the transaction history of your wallet')
+register_command('importprivkey',        1, 1, True,  True,  'Import a private key', 'importprivkey <privatekey>')
+register_command('listaddresses',        3, 3, False, True,  'Returns your list of addresses.', '', listaddr_options)
+register_command('listunspent',          0, 0, False, True,  'Returns a list of unspent inputs in your wallet.')
+register_command('mktx',                 5, 5, True,  True,  'Create a signed transaction', 'mktx <recipient> <amount> [label]', payto_options)
+register_command('payto',                5, 5, True,  False, 'Create and broadcast a transaction.', "payto <recipient> <amount> [label]\n<recipient> can be a bitcoin address or a label", payto_options)
+register_command('password',             0, 0, True,  True,  'Change your password')
+register_command('restore',              0, 0, False, False, 'Restore a wallet')
+register_command('setconfig',            2, 2, False, True,  'Set a configuration variable', 'setconfig <name> <value>')
+register_command('setlabel',             2,-1, False, True,  'Assign a label to an item', 'setlabel <tx_hash> <label>')
+register_command('sendrawtransaction',   1, 1, False, False, 'Broadcasts a transaction to the network.', 'sendrawtransaction <tx in hexadecimal>')
+register_command('prioritize',           1, 1, False, True,  '')
+register_command('signrawtransaction',   1, 3, True,  True,  'similar to bitcoind\'s command')
+register_command('signmessage',          2,-1, True,  True,  'Sign a message with a key', 'signmessage <address> <message>\nIf you want to lead or end a message with spaces, or want double spaces inside the message make sure you quote the string. I.e. " Hello  This is a weird String "')
+register_command('unprioritize',         1, 1, False, True,  '')
+register_command('unfreeze',             1, 1, False, True,  '')
+register_command('validateaddress',      1, 1, False, True,  'Check that the address is valid')
+register_command('verifymessage',        3,-1, False, True,  'Verifies a signature', 'verifymessage <address> <signature> <message>\nIf you want to lead or end a message with spaces, or want double spaces inside the message make sure you quote the string. I.e. " Hello  This is a weird String "')
     
-    }
 
 
-
-offline_commands = [ 'password', 'mktx',
-                     'setlabel', 'contacts',
-                     'help', 'validateaddress',
-                     'signmessage', 'verifymessage',
-                     'eval', 'set', 'get', 'create', 'listaddresses',
-                     'importprivkey', 'getseed',
-                     'deseed',
-                     'freeze','unfreeze',
-                     'prioritize','unprioritize',
-                     'dumpprivkey','dumpprivkeys','listunspent',
-                     'createmultisig', 'createrawtransaction', 'decoderawtransaction', 'signrawtransaction'
-                     ]
-
-protected_commands = ['payto', 'password', 'mktx', 'getseed', 'importprivkey','signmessage', 'signrawtransaction', 'dumpprivkey', 'dumpprivkeys' ]
 
 class Commands:
 
@@ -118,7 +93,7 @@ class Commands:
             apply(self._callback, ())
         return result
 
-    def get_history(self, addr):
+    def getaddresshistory(self, addr):
         h = self.wallet.get_history(addr)
         if h is None: h = self.wallet.interface.synchronous_get([ ('blockchain.address.get_history',[addr]) ])[0]
         return h
