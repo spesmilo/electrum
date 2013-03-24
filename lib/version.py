de699150899fdaa6a735a310915ee1e2361d509c
ecdsa <ecdsa@github>
2013-03-24 09:56:21 +0100

    version 1.7.2

--- lib/version.py
+++ lib/version.py
@@ -1,4 +1,4 @@
-ELECTRUM_VERSION = "1.7.1"  # version of the client package
+ELECTRUM_VERSION = "1.7.2"  # version of the client package
 PROTOCOL_VERSION = '0.6'    # protocol version requested
 SEED_VERSION     = 4        # bump this every time the seed generation is modified
 TRANSLATION_ID   = 3992     # version of the wiki page 
