5e5a89ecb7b6ed95ce3088fd31ba9c0798115eac
thomasv <thomasv@gitorious>
2013-05-10 14:40:05 +0200

    version 1.7.4

--- lib/version.py
+++ lib/version.py
@@ -1,4 +1,4 @@
-ELECTRUM_VERSION = "1.7.3"  # version of the client package
+ELECTRUM_VERSION = "1.7.4"  # version of the client package
 PROTOCOL_VERSION = '0.6'    # protocol version requested
 SEED_VERSION     = 4        # bump this every time the seed generation is modified
 TRANSLATION_ID   = 4040     # version of the wiki page 
