1cfb22ba6d6fd51e42ab8bee65d3fe9170072a0b
rdymac <rdy@aol.es>
2013-06-17 19:16:25 +0200

    latest wikia version
    latest wikia version

--- lib/version.py
+++ lib/version.py
@@ -1,4 +1,4 @@
 ELECTRUM_VERSION = "1.8"    # version of the client package
 PROTOCOL_VERSION = '0.6'    # protocol version requested
 SEED_VERSION     = 4        # bump this every time the seed generation is modified
-TRANSLATION_ID   = 4073     # version of the wiki page 
+TRANSLATION_ID   = 4101     # version of the wiki page 
