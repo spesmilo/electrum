a4335cb25d5109344ed394dcd02957c0faddab4d
thomasv <thomasv@gitorious>
2013-09-13 09:40:34 +0200

    documentation on GUI

--- gui/__init__.py
+++ gui/__init__.py
@@ -1,3 +1,5 @@
-# do not remove this file
+# To create a new GUI, please add its code to this directory.
+# Two objects must be passed to the ElectrumGui: config and network
+# The Wallet object is instanciated by the GUI
 
-
+# Notifications about network events are sent to the GUI by using network.register_callback()
