f30fedc591003360530220973d9ad00e0d2c0a41
ThomasV <thomasv@gitorious>
2013-09-28 13:51:25 +0200

    fix import

--- gui/gtk.py
+++ gui/gtk.py
@@ -16,6 +16,9 @@
 # You should have received a copy of the GNU General Public License
 # along with this program. If not, see <http://www.gnu.org/licenses/>.
 
+# use this because this file is named gtk.py
+from __future__ import absolute_import
+
 import datetime
 import thread, time, ast, sys, re
 import socket, traceback
