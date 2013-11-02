56168e85b8e469f9c042eda6704a31d016efe26a
Ryan McGuire <ryan@enigmacurry.com>
2013-11-01 21:18:36 -0400

    Adds a settings dialog for qrscanner plugin - allows device selection

--- plugins/qrscanner.py
+++ plugins/qrscanner.py
@@ -1,14 +1,16 @@
 from electrum.util import print_error
 from urlparse import urlparse, parse_qs
-from PyQt4.QtGui import QPushButton, QMessageBox, QDialog, QVBoxLayout, QHBoxLayout, QGridLayout, QLabel
+from PyQt4.QtGui import QPushButton, QMessageBox, QDialog, QVBoxLayout, QHBoxLayout, QGridLayout, QLabel, QLineEdit, QComboBox
 from PyQt4.QtCore import Qt
 
 from electrum.i18n import _
 import re
+import os
 from electrum import Transaction
 from electrum.bitcoin import MIN_RELAY_TX_FEE, is_valid
 from electrum_gui.qt.qrcodewidget import QRCodeWidget
 from electrum import bmp
+from electrum_gui.qt import HelpButton, EnterButton
 import json
 
 try:
@@ -32,10 +34,11 @@ class Plugin(BasePlugin):
             return False
         try:
             proc = zbar.Processor()
-            proc.init()
+            proc.init(video_device=self.video_device())
         except zbar.SystemError:
             # Cannot open video device
-            return False
+            pass
+            #return False
 
         return True
 
@@ -62,7 +65,12 @@ class Plugin(BasePlugin):
 
     def scan_qr(self):
         proc = zbar.Processor()
-        proc.init()
+        try:
+            proc.init(video_device=self.video_device())
+        except zbar.SystemError, e:
+            QMessageBox.warning(self.gui.main_window, _('Error'), _(e), _('OK'))
+            return
+
         proc.visible = True
 
         while True:
@@ -242,6 +250,88 @@ class Plugin(BasePlugin):
         if 'message' in qrcode:
             self.gui.main_window.message_e.setText("%s (%s)" % (self.gui.main_window.message_e.text(), qrcode['message']))
                 
+    def video_device(self):
+        device = self.config.get("video_device", "default")
+        if device == 'default':
+            device = ''
+        return device
+
+    def requires_settings(self):
+        return True
+
+    def settings_widget(self, window):
+        return EnterButton(_('Settings'), self.settings_dialog)
+    
+    def _find_system_cameras(self):
+        device_root = "/sys/class/video4linux"
+        devices = {} # Name -> device
+        if os.path.exists(device_root):
+            for device in os.listdir(device_root):
+                name = open(os.path.join(device_root, device, 'name')).read()
+                devices[name] = os.path.join("/dev",device)
+        return devices
+
+    def settings_dialog(self):
+        system_cameras = self._find_system_cameras()
+
+        d = QDialog()
+        layout = QGridLayout(d)
+        layout.addWidget(QLabel("Choose a video device:"),0,0)
+
+        # Create a combo box with the available video devices:
+        combo = QComboBox()
+
+        # on change trigger for video device selection, makes the
+        # manual device selection only appear when needed:
+        def on_change(x):
+            combo_text = str(combo.itemText(x))
+            combo_data = combo.itemData(x)
+            if combo_text == "Manually specify a device":
+                custom_device_label.setVisible(True)
+                self.video_device_edit.setVisible(True)
+                if self.config.get("video_device") == "default":
+                    self.video_device_edit.setText("")
+                else:
+                    self.video_device_edit.setText(self.config.get("video_device"))
+            else:
+                custom_device_label.setVisible(False)
+                self.video_device_edit.setVisible(False)
+                self.video_device_edit.setText(combo_data.toString())
+
+        # on save trigger for the video device selection window,
+        # stores the chosen video device on close.
+        def on_save():
+            device = str(self.video_device_edit.text())
+            self.config.set_key("video_device", device)
+            d.accept()
+
+        custom_device_label = QLabel("Video device: ")
+        custom_device_label.setVisible(False)
+        layout.addWidget(custom_device_label,1,0)
+        self.video_device_edit = QLineEdit()
+        self.video_device_edit.setVisible(False)
+        layout.addWidget(self.video_device_edit, 1,1,2,2)
+        combo.currentIndexChanged.connect(on_change)
+
+        combo.addItem("Default","default")
+        for camera, device in system_cameras.items():
+            combo.addItem(camera, device)
+        combo.addItem("Manually specify a device",self.config.get("video_device"))
+
+        # Populate the previously chosen device:
+        index = combo.findData(self.config.get("video_device"))
+        combo.setCurrentIndex(index)
+
+        layout.addWidget(combo,0,1)
+
+        self.accept = QPushButton(_("Done"))
+        self.accept.clicked.connect(on_save)
+        layout.addWidget(self.accept,4,2)
+
+        if d.exec_():
+          return True
+        else:
+          return False
 
 
 
