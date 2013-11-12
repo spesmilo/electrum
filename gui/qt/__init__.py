ff289aebd21bf2c186187980f2a7f667c00bf5d0
Michael Wozniak <github@koh.ms>
2013-11-11 22:05:12 -0500

    The url string is decoded later.

--- gui/qt/__init__.py
+++ gui/qt/__init__.py
@@ -60,7 +60,7 @@ class OpenFileEventFilter(QObject):
     def eventFilter(self, obj, event):
         if event.type() == QtCore.QEvent.FileOpen:
             if len(self.windows) >= 1:
-                self.windows[0].set_url(event.url().toString())
+                self.windows[0].set_url(event.url().toEncoded())
                 return True
         return False
 
