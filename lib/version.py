be506af2460e47527d03dc7d864ec72a04d8d7e7
ThomasV <thomasv@gitorious>
2014-01-23 17:06:47 +0100

    json rpc daemon

--- lib/version.py
+++ lib/version.py
@@ -1,5 +1,5 @@
-ELECTRUM_VERSION = "1.9.7"  # version of the client package
-PROTOCOL_VERSION = '0.6'    # protocol version requested
+ELECTRUM_VERSION = "2.0"    # version of the client package
+PROTOCOL_VERSION = '0.9'    # protocol version requested
 SEED_VERSION     = 4        # bump this every time the seed generation is modified
 SEED_PREFIX      = '01'     # the hash of the mnemonic seed must begin with this
 
