35c270e075ef2762c94fc589b9b9c45bb4b1f7e2
ThomasV <thomasv@gitorious>
2014-05-21 16:04:58 +0200

    explicit type for imported wallets. fixes #698

--- lib/wallet.py
+++ lib/wallet.py
@@ -1086,6 +1086,7 @@ class Imported_Wallet(Abstract_Wallet):
         a = self.accounts.get(IMPORTED_ACCOUNT)
         if not a:
             self.accounts[IMPORTED_ACCOUNT] = ImportedAccount({'imported':{}})
+        self.storage.put('wallet_type', 'imported', True)
 
 
     def is_watching_only(self):
@@ -1634,8 +1635,7 @@ class Wallet(object):
         if storage.get('wallet_type') == '2of3':
             return Wallet_2of3(storage)
 
-        if storage.file_exists and not storage.get('seed'):
-            # wallet made of imported keys
+        if storage.get('wallet_type') == 'imported':
             return Imported_Wallet(storage)
 
 
