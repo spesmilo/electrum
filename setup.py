2e410a63687d4299fc3cb3e3deceae7f8d387a8a
ThomasV <thomasv@gitorious>
2014-06-24 20:31:39 +0200

    add missing module qrtextedit

--- setup.py
+++ setup.py
@@ -102,6 +102,7 @@ setup(
         'electrum_gui.qt.password_dialog',
         'electrum_gui.qt.paytoedit',
         'electrum_gui.qt.qrcodewidget',
+        'electrum_gui.qt.qrtextedit',
         'electrum_gui.qt.receiving_widget',
         'electrum_gui.qt.seed_dialog',
         'electrum_gui.qt.transaction_dialog',
