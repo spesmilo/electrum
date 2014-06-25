4675ea5321712d522d9bd5f35ee2745a8505ba30
Chris Glass <tribaal@gmail.com>
2014-06-25 10:50:21 +0200

    Added util test file

---
+++ lib/tests/test_util.py
@@ -0,0 +1,19 @@
+import unittest
+from lib.util import format_satoshis
+
+class TestUtil(unittest.TestCase):
+
+    def test_format_satoshis(self):
+        result = format_satoshis(1234)
+        expected = "0.00001234"
+        self.assertEqual(expected, result)
+
+    def test_format_satoshis_diff_positive(self):
+        result = format_satoshis(1234, is_diff=True)
+        expected = "+0.00001234"
+        self.assertEqual(expected, result)
+
+    def test_format_satoshis_diff_negative(self):
+        result = format_satoshis(-1234, is_diff=True)
+        expected = "-0.00001234"
+        self.assertEqual(expected, result)
