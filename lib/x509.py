8feb2a7b672ce9075fadb056d1f2f5320376795b
ThomasV <thomasv@gitorious>
2014-06-27 14:42:02 +0200

    fix missing import. fixes #738

--- lib/x509.py
+++ lib/x509.py
@@ -17,7 +17,8 @@
 # along with this program. If not, see <http://www.gnu.org/licenses/>.
 
 
-from datetime import datetime, timedelta
+from datetime import datetime
+import sys
 
 try:
     import pyasn1
