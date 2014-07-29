f7280e46374c442570a2f6b8a97f19a4a1b8944c
ThomasV <thomasv@gitorious>
2014-07-29 10:43:04 +0200

    cleanup daemon threads

--- lib/daemon.py
+++ lib/daemon.py
@@ -59,6 +59,7 @@ def get_daemon(config, start_daemon=True):
                 time.sleep(0.1)
 
 
+
 class ClientThread(threading.Thread):
 
     def __init__(self, server, s):
@@ -104,14 +105,15 @@ class ClientThread(threading.Thread):
 
 
 
-class NetworkServer:
+class NetworkServer(threading.Thread):
 
     def __init__(self, config):
+        threading.Thread.__init__(self)
+        self.daemon = True
         self.config = config
         self.network = Network(config)
         # network sends responses on that queue
         self.network_queue = Queue.Queue()
-        self.network.start(self.network_queue)
 
         self.running = False
         # daemon terminates after period of inactivity
@@ -127,9 +129,12 @@ class NetworkServer:
             return self.running
 
     def stop(self):
-        self.network.stop()
         with self.lock:
             self.running = False
+
+    def start(self):
+        self.running = True
+        threading.Thread.start(self)
 
     def add_client(self, client):
         for key in ['status','banner','updated','servers','interfaces']:
@@ -138,17 +143,13 @@ class NetworkServer:
         with self.lock:
             self.clients.append(client)
 
-
     def remove_client(self, client):
         with self.lock:
             self.clients.remove(client)
         print_error("client quit:", len(self.clients))
 
-
-
-    def main_loop(self):
-        self.running = True
-        threading.Thread(target=self.listen_thread).start()
+    def run(self):
+        self.network.start(self.network_queue)
         while self.is_running():
             try:
                 response = self.network_queue.get(timeout=0.1)
@@ -157,32 +158,35 @@ class NetworkServer:
             for client in self.clients:
                 client.daemon_pipe.get_queue.put(response)
 
-        print_error("Daemon exiting")
+        self.network.stop()
+        print_error("server exiting")
 
-    def listen_thread(self):
-        self.socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        self.socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-        self.daemon_port = self.config.get('daemon_port', DAEMON_PORT)
-        self.socket.bind(('', self.daemon_port))
-        self.socket.listen(5)
-        self.socket.settimeout(1)
+
+
+def daemon_loop(server):
+    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+    daemon_port = server.config.get('daemon_port', DAEMON_PORT)
+    s.bind(('', daemon_port))
+    s.listen(5)
+    s.settimeout(1)
+    t = time.time()
+    while server.running:
+        try:
+            connection, address = s.accept()
+        except socket.timeout:
+            if not server.clients:
+                if time.time() - t > server.timeout:
+                    print_error("Daemon timeout")
+                    break
+            else:
+                t = time.time()
+            continue
         t = time.time()
-        while self.running:
-            try:
-                connection, address = self.socket.accept()
-            except socket.timeout:
-                if not self.clients:
-                    if time.time() - t > self.timeout:
-                        print_error("Daemon timeout")
-                        break
-                else:
-                    t = time.time()
-                continue
-            t = time.time()
-            client = ClientThread(self, connection)
-            client.start()
-        self.stop()
-        print_error("listen thread exiting")
+        client = ClientThread(server, connection)
+        client.start()
+    server.stop()
+    print_error("Daemon exiting")
 
 
 if __name__ == '__main__':
@@ -190,8 +194,10 @@ if __name__ == '__main__':
     config = simple_config.SimpleConfig()
     util.set_verbosity(True)
     server = NetworkServer(config)
+    server.start()
     try:
-        server.main_loop()
+        daemon_loop(server)
     except KeyboardInterrupt:
         print "Ctrl C - Stopping server"
+        server.stop()
         sys.exit(1)
