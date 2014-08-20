2afe4e30f777ea3d4194e4c9227916def0a432a9
Maran <maran.hidskes@gmail.com>
2014-08-20 20:10:53 +0200

    Add a GUI messagebox when using a Trezor but can't connect to the device.

--- plugins/trezor.py
+++ plugins/trezor.py
@@ -94,7 +94,7 @@ class TrezorWallet(NewWallet):
         NewWallet.__init__(self, storage)
 
         self.seed = 'trezor'
-        
+
         self.storage.put('gap_limit', 20, False)    #obey BIP44 gap limit of 20
 
         self.use_encryption = False
@@ -133,6 +133,8 @@ class TrezorWallet(NewWallet):
                 d = HidTransport.enumerate()[0]
                 self.transport = HidTransport(d)
             except:
+                d = QDialog()
+                QMessageBox.warning(d, _('Warning'), _('Could not connect to your Trezor. Please verify the cable is connected and that no other app is using it.'), _('OK'))
                 raise Exception("Trezor not found")
             self.client = QtGuiTrezorClient(self.transport)
             self.client.set_tx_api(self)
