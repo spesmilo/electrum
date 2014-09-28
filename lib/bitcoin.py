c072ecda49d8d5efc67e899a965e05a231b74af7
wozz <wozz@users.noreply.github.com>
2014-09-28 11:58:23 -0400

    Update message signature magic string
    Update length on message signature magic string

--- lib/bitcoin.py
+++ lib/bitcoin.py
@@ -348,7 +348,7 @@ from ecdsa.util import string_to_number, number_to_string
 def msg_magic(message):
     varint = var_int(len(message))
     encoded_varint = "".join([chr(int(varint[i:i+2], 16)) for i in xrange(0, len(varint), 2)])
-    return "\x18Litecoin Signed Message:\n" + encoded_varint + message
+    return "\x19Litecoin Signed Message:\n" + encoded_varint + message
 
 
 def verify_message(address, signature, message):
