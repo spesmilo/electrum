72d24b65e6de1827244b801a676b5ccf49962996
Tafelpoot <stefaang@gmail.com>
2014-10-21 20:13:21 +0200

    Several QR code fixes
    Updated qrscanner.py to keep a reference to the QR processor. This fixes
    crashes after scanning on windows. The qrscanner module should init the QR
    processor before scanning. This requires the camera, which is stored in
    config. That is why ScanQRTextEdits require a link to a window with access
    to config.
    
    I made a split between ScanQRTextEdits and ShowQRTextEdits, because they
    function very differently. The only overlap between these classes is the
    fact that they have a QR icon in the edit box. No need to use
    setReadOnly(True) anymore.
    
    Update on show_master_pub_keys with a Combobox as it doesn't make sense to
    show multiple QR codes in 1 screen. It would not be useable as soon as
    you'd have 3+ accounts.
    
    Fixed the copy to clipboard of show_master_pub_keys QRcode window. It has
    no parent. Clipboard does not require parent() call anymore now.
    
    Added bitcoin URI parsing on scanning a QRcode from within the send tab.
    
    Not included yet:
    - fee update on URI scan.
    - ScanQRTextEdit for Add Contact dialog

--- lib/qrscanner.py
+++ lib/qrscanner.py
@@ -6,27 +6,36 @@ try:
 except ImportError:
     zbar = None
 
+proc = None
 
-def scan_qr(config):
+def init(config):
     if not zbar:
         raise BaseException("\n".join([_("Cannot start QR scanner."),_("The zbar package is not available."),_("On Linux, try 'sudo apt-get install python-zbar'")]))
     device = config.get("video_device", "default")
     if device == 'default':
         device = ''
+    global proc
     proc = zbar.Processor()
     proc.init(video_device=device)
+
+def scan_qr(self):
+    if not zbar:
+        raise BaseException("\n".join([_("Cannot start QR scanner."),_("The zbar package is not available."),_("On Linux, try 'sudo apt-get install python-zbar'")]))
+    if proc is None:
+        raise BaseException("Start proc first")
     proc.visible = True
     while True:
         try:
             proc.process_one()
         except Exception:
             # User closed the preview window
-            return {}
+            return ""
         for r in proc.results:
             if str(r.type) != 'QRCODE':
                 continue
+            # hiding the preview window stops the camera
+            proc.visible = False
             return r.data
-
 
 def _find_system_cameras():
     device_root = "/sys/class/video4linux"
