99f5d368e64334b7c2cb9cb1499fcd2d08e0d5b7
ThomasV <thomasv@gitorious>
2014-11-11 10:39:34 +0100

    move date column to first position in invoice tab

--- gui/qt/main_window.py
+++ gui/qt/main_window.py
@@ -1350,13 +1350,13 @@ class ElectrumWindow(QMainWindow):
 
 
     def create_invoices_tab(self):
-        l, w = self.create_list_tab([_('Requestor'), _('Memo'), _('Date'), _('Amount'), _('Status')])
+        l, w = self.create_list_tab([_('Date'), _('Requestor'), _('Memo'), _('Amount'), _('Status')])
         l.setColumnWidth(0, 150)
-        l.setColumnWidth(2, 150)
+        l.setColumnWidth(1, 150)
         l.setColumnWidth(3, 150)
         h = l.header()
         h.setStretchLastSection(False)
-        h.setResizeMode(1, QHeaderView.Stretch)
+        h.setResizeMode(2, QHeaderView.Stretch)
         l.setContextMenuPolicy(Qt.CustomContextMenu)
         l.customContextMenuRequested.connect(self.create_invoice_menu)
         self.invoices_list = l
@@ -1371,9 +1371,9 @@ class ElectrumWindow(QMainWindow):
             if status == PR_UNPAID and expiration_date and expiration_date < time.time():
                 status = PR_EXPIRED
             date_str = datetime.datetime.fromtimestamp(expiration_date).isoformat(' ')[:-3]
-            item = QTreeWidgetItem( [ domain, memo, date_str, self.format_amount(amount, whitespaces=True), format_status(status)] )
+            item = QTreeWidgetItem( [ date_str, domain, memo, self.format_amount(amount, whitespaces=True), format_status(status)] )
             item.setData(0, 32, key)
-            item.setFont(0, QFont(MONOSPACE_FONT))
+            item.setFont(1, QFont(MONOSPACE_FONT))
             item.setFont(3, QFont(MONOSPACE_FONT))
             l.addTopLevelItem(item)
         l.setCurrentItem(l.topLevelItem(0))
