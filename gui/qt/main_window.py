a8cea34aac363d57139327ed614a9bbac5666720
jennychang3 <jennychang207@gmail.com>
2014-11-21 13:53:36 +0400

    Update main_window.py

--- gui/qt/main_window.py
+++ gui/qt/main_window.py
@@ -35,7 +35,7 @@ from electrum.plugins import run_hook
 
 import icons_rc
 
-from electrum.util import format_satoshis, NotEnoughFunds
+from electrum.util import format_satoshis
 from electrum import Transaction
 from electrum import mnemonic
 from electrum import util, bitcoin, commands, Interface, Wallet
@@ -953,13 +953,10 @@ class ElectrumWindow(QMainWindow):
                 if not outputs:
                     addr = self.payto_e.payto_address if self.payto_e.payto_address else self.dummy_address
                     outputs = [('address', addr, amount)]
-                try:
-                    tx = self.wallet.make_unsigned_transaction(outputs, fee, coins = self.get_coins())
-                    self.not_enough_funds = False
-                except NotEnoughFunds:
-                    self.not_enough_funds = True
+                tx = self.wallet.make_unsigned_transaction(outputs, fee, coins = self.get_coins())
+                self.not_enough_funds = (tx is None)
                 if not is_fee:
-                    fee = None if self.not_enough_funds else self.wallet.get_tx_fee(tx)
+                    fee = self.wallet.get_tx_fee(tx) if tx else None
                     self.fee_e.setAmount(fee)
 
         self.payto_e.textChanged.connect(lambda:text_edited(False))
