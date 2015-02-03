2f7b8ce86490729ba3d158f311f8245009d8beab
ThomasV <thomasv@gitorious>
2015-02-03 13:14:35 +0100

    fix wallet.can_sign: use isinstance with BIP32 class

--- lib/wallet.py
+++ lib/wallet.py
@@ -1048,7 +1048,7 @@ class Abstract_Wallet(object):
             addr = bitcoin.public_key_to_bc_address(x_pubkey.decode('hex'))
             return self.is_mine(addr)
         elif x_pubkey[0:2] == 'ff':
-            if isinstance(self, OldWallet): return False
+            if not isinstance(self, BIP32_Wallet): return False
             xpub, sequence = BIP32_Account.parse_xpubkey(x_pubkey)
             return xpub in [ self.master_public_keys[k] for k in self.master_private_keys.keys() ]
         elif x_pubkey[0:2] == 'fe':
