acaaec840584d04c76271b5d81becf673cb6c76a
ThomasV <thomasv@gitorious>
2015-02-17 16:57:48 +0100

    fix comment: standard is not bip44

--- lib/version.py
+++ lib/version.py
@@ -5,5 +5,5 @@ OLD_SEED_VERSION = 4        # electrum versions < 2.0
 
 
 # The hash of the mnemonic seed must begin with this
-SEED_PREFIX      = '01'      # BIP44
+SEED_PREFIX      = '01'      # Electrum standard wallet
 SEED_PREFIX_2FA  = '101'     # extended seed for two-factor authentication
