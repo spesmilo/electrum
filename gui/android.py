217dc2f55b38c22c3bbaf3b42f03664419cfec50
ThomasV <thomasv@gitorious>
2015-02-25 18:00:38 +0100

    android: fix recv amount bug

--- gui/android.py
+++ gui/android.py
@@ -660,9 +660,9 @@ def receive_loop():
             modal_dialog('URI copied to clipboard', receive_URI)
 
         elif event["name"]=="amount":
-            amount = modal_input('Amount', 'Amount you want receive (in BTC). ', format_satoshis(receive_amount) if receive_amount else None, "numberDecimal")
+            amount = modal_input('Amount', 'Amount you want to receive (in BTC). ', format_satoshis(receive_amount) if receive_amount else None, "numberDecimal")
             if amount is not None:
-                receive_amount = 100000000 * Decimal(amount) if amount else None
+                receive_amount = int(100000000 * Decimal(amount)) if amount else None
                 out = 'receive'
 
         elif event["name"]=="message":
