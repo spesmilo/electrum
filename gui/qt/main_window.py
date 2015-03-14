c5f3bdc501f88d7f103f73084b2dde5c03e45de6
Kefkius <kefkius@mail.com>
2015-03-14 00:52:41 -0400

    GRS - bug report link

--- gui/qt/main_window.py
+++ gui/qt/main_window.py
@@ -421,7 +421,7 @@ class ElectrumWindow(QMainWindow):
 
     def show_report_bug(self):
         QMessageBox.information(self, "Electrum-GRS - " + _("Reporting Bugs"),
-            _("Please report any bugs as issues on github:")+" <a href=\"https://github.com/spesmilo/electrum/issues\">https://github.com/spesmilo/electrum/issues</a>")
+            _("Please report any bugs as issues on github:")+" <a href=\"https://github.com/groestlcoin/electrum/issues\">https://github.com/groestlcoin/electrum/issues</a>")
 
 
     def notify_transactions(self):
