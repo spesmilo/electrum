aa4ced7a26f6da2463849b392df2cead205b51fb
ThomasV <thomasv@gitorious>
2015-05-28 15:45:09 +0200

    add import

--- plugins/__init__.py
+++ plugins/__init__.py
@@ -16,6 +16,7 @@
 # You should have received a copy of the GNU General Public License
 # along with this program. If not, see <http://www.gnu.org/licenses/>.
 
+import electrum
 from electrum.i18n import _
 
 descriptions = [
