e879cb4552fefccb4a444c78232bb33a8c07f626
ThomasV <thomasv@gitorious>
2015-07-18 11:17:19 +0200

    https download of headers

--- lib/blockchain.py
+++ lib/blockchain.py
@@ -27,7 +27,7 @@ class Blockchain():
     def __init__(self, config, network):
         self.config = config
         self.network = network
-        self.headers_url = 'http://headers.electrum.org/blockchain_headers'
+        self.headers_url = 'https://headers.electrum.org/blockchain_headers'
         self.local_height = 0
         self.set_local_height()
 
