e96a0945caecba75801edf4b58ca1427bde41e3b
ThomasV <thomasv@gitorious>
2015-07-29 22:06:44 +0200

    do not use pycrypto for DNSSEC validation

--- setup.py
+++ setup.py
@@ -29,7 +29,6 @@ setup(
     name="Electrum",
     version=version.ELECTRUM_VERSION,
     install_requires=[
-        'pycrypto',
         'slowaes>=0.1a1',
         'ecdsa>=0.9',
         'pbkdf2',
