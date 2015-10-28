db0a5f9b31cdf5c09651465f32f9297e6d05ffe8
ThomasV <thomasv@electrum.org>
2015-10-28 12:10:43 +0100

    fix: missing import

--- gui/qt/network_dialog.py
+++ gui/qt/network_dialog.py
@@ -17,13 +17,15 @@
 # along with this program. If not, see <http://www.gnu.org/licenses/>.
 
 import sys, time, datetime, re, threading
-from electrum.i18n import _
-from electrum.util import print_error, print_msg
 import os.path, json, ast, traceback
 
 from PyQt4.QtGui import *
 from PyQt4.QtCore import *
+
+from electrum.i18n import _
+from electrum.util import print_error, print_msg
 from electrum import DEFAULT_PORTS
+from electrum.network import serialize_server, deserialize_server
 
 from util import *
 
