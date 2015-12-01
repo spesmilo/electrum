c7b1721f585ff8863e1df0f14452bd27b1291bbf
ThomasV <thomasv@electrum.org>
2015-12-01 12:00:18 +0100

    fix cmdline trezor

--- plugins/trezor/trezor.py
+++ plugins/trezor/trezor.py
@@ -273,13 +273,6 @@ class TrezorPlugin(BasePlugin):
             self.client = None
         self.wallet = None
 
-    @hook
-    def cmdline_load_wallet(self, wallet):
-        self.wallet = wallet
-        self.wallet.plugin = self
-        if self.handler is None:
-            self.handler = TrezorCmdLineHandler()
-
     def sign_transaction(self, tx, prev_tx, xpub_path):
         self.prev_tx = prev_tx
         self.xpub_path = xpub_path
@@ -449,27 +442,6 @@ class TrezorGuiMixin(object):
         log("Enter one word of mnemonic: ")
         word = raw_input()
         return proto.WordAck(word=word)
-
-
-class TrezorCmdLineHandler:
-
-    def get_passphrase(self, msg):
-        import getpass
-        print_msg(msg)
-        return getpass.getpass('')
-
-    def get_pin(self, msg):
-        t = { 'a':'7', 'b':'8', 'c':'9', 'd':'4', 'e':'5', 'f':'6', 'g':'1', 'h':'2', 'i':'3'}
-        print_msg(msg)
-        print_msg("a b c\nd e f\ng h i\n-----")
-        o = raw_input()
-        return ''.join(map(lambda x: t[x], o))
-
-    def stop(self):
-        pass
-
-    def show_message(self, msg):
-        print_msg(msg)
 
 
 
