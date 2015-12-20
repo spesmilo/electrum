25a2ceda237768c692fe9220efd05398d62d6a57
Neil Booth <kyuupichan@gmail.com>
2015-12-20 12:06:23 +0900

    Fix copyright.

--- lib/coinchooser.py
+++ lib/coinchooser.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 #
 # Electrum - lightweight Bitcoin client
-# Copyright (C) 2011 thomasv@gitorious
+# Copyright (C) 2015 kyuupichan@gmail
 #
 # This program is free software: you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
