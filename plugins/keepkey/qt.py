1f3ddad0cd687ac6216c26f257190ffa3471c5aa
Neil Booth <kyuupichan@gmail.com>
2015-12-28 23:35:34 +0900

    Update as for trezor/qt.py

--- plugins/keepkey/qt.py
+++ plugins/keepkey/qt.py
@@ -2,5 +2,5 @@ from plugins.trezor.qt_generic import QtPlugin
 from keepkeylib.qt.pinmatrix import PinMatrixWidget
 
 class Plugin(QtPlugin):
-    handler_class = KeepKeyQtHandler
+    pin_matrix_widget_class = PinMatrixWidget
     icon_file = ":icons/keepkey.png"
