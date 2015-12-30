2c797cd75be2447ef8a8ba5ecc640fec5e895c33
Eagle[TM] <eagletm@mpex.net>
2015-12-30 14:47:40 +0100

    Update default server list

--- lib/network.py
+++ lib/network.py
@@ -21,17 +21,21 @@ from version import ELECTRUM_VERSION, PROTOCOL_VERSION
 DEFAULT_PORTS = {'t':'50001', 's':'50002', 'h':'8081', 'g':'8082'}
 
 DEFAULT_SERVERS = {
-    'electrum.be':{'t':'50001', 's':'50002'},
-    'electrum.drollette.com':{'t':'50001', 's':'50002'},
     'erbium1.sytes.net':{'t':'50001', 's':'50002'},
     'ecdsa.net':{'t':'50001', 's':'110'},
     'electrum0.electricnewyear.net':{'t':'50001', 's':'50002'},
-    'kirsche.emzy.de':DEFAULT_PORTS,
     'VPS.hsmiths.com':{'t':'50001', 's':'50002'},
     'ELECTRUM.jdubya.info':{'t':'50001', 's':'50002'},
     'electrum.no-ip.org':{'t':'50001', 's':'50002', 'g':'443'},
-    'electrum.thwg.org':DEFAULT_PORTS,
-    'us.electrum.be':{'t':'50001', 's':'50002'},
+    'us.electrum.be':DEFAULT_PORTS,
+    'bitcoins.sk':{'t':'50001', 's':'50002'},
+    'electrum.petrkr.net':{'t':'50001', 's':'50002'},
+    'electrum.dragonzone.net':DEFAULT_PORTS,
+    'Electrum.hsmiths.com':{'t':'8080', 's':'995'},
+    'electrum3.hachre.de':{'t':'50001', 's':'50002'},
+    'elec.luggs.co':{'t':'80', 's':'443'},
+    'btc.smsys.me':{'t':'110', 's':'995'},
+    'electrum.online':{'t':'50001', 's':'50002'},
 }
 
 NODES_RETRY_INTERVAL = 60
