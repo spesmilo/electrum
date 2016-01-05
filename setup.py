60b595356b8c049adc296deb0ffe65b357500ab4
Andreas Hilboll <andreas@hilboll.de>
2016-01-05 12:13:15 +0100

    BUG: allow non-root installation on linux
    fixes #1607

--- setup.py
+++ setup.py
@@ -17,6 +17,11 @@ data_files = []
 
 if platform.system() in ['Linux', 'FreeBSD', 'DragonFly']:
     usr_share = os.path.join(sys.prefix, "share")
+    if not os.access(usr_share, os.W_OK):
+        if 'XDG_DATA_HOME' in os.environ.keys():
+            usr_share = os.environ['$XDG_DATA_HOME']
+        else:
+            usr_share = os.path.expanduser('~/.local/share')
     data_files += [
         (os.path.join(usr_share, 'applications/'), ['electrum.desktop']),
         (os.path.join(usr_share, 'pixmaps/'), ['icons/electrum.png'])
