1842ace158274b52f51c1edfbc32d9f4905d0f8a
BTChip <contact@btchip.com>
2016-01-29 01:33:21 +0100

    Align Ledger plugin with the others, somewhat.

--- plugins/ledger/__init__.py
+++ plugins/ledger/__init__.py
@@ -1,8 +1,9 @@
 from electrum.i18n import _
 
 fullname = 'Ledger Wallet'
-description = 'Provides support for Ledger hardware wallet'
-requires = [('btchip', 'github.com/ledgerhq/btchip-python')]
-requires_wallet_type = ['btchip']
-registers_wallet_type = ('hardware', 'btchip', _("Ledger wallet"))
+description = _('Provides support for Ledger hardware wallet')
+requires = [('btchip','github.com/ledgerhq/btchip-python')]
+requires_wallet_type = ['ledger']
+registers_wallet_type = ('hardware', 'ledger', _("Ledger wallet"))
 available_for = ['qt', 'cmdline']
+
--- plugins/ledger/qt.py
+++ plugins/ledger/qt.py
@@ -1,76 +1,10 @@
-import threading
+from ..ledger.qt_generic import qt_plugin_class
+from ledger import LedgerPlugin
 
-from PyQt4.Qt import (QDialog, QInputDialog, QLineEdit,
-                      QVBoxLayout, QLabel, SIGNAL)
-import PyQt4.QtCore as QtCore
+class Plugin(qt_plugin_class(LedgerPlugin)):
+    icon_file = ":icons/trezor.png"
 
-from electrum.i18n import _
-from electrum.plugins import hook
-from .ledger import LedgerPlugin, BTChipWallet
-
-class Plugin(LedgerPlugin):
-
-    @hook
-    def load_wallet(self, wallet, window):
-        if type(wallet) != BTChipWallet:
-            return
-        wallet.handler = BTChipQTHandler(window)
-        if self.btchip_is_connected(wallet):
-            if not wallet.check_proper_device():
-                window.show_error(_("This wallet does not match your Ledger device"))
-                wallet.force_watching_only = True
-        else:
-            window.show_error(_("Ledger device not detected.\nContinuing in watching-only mode."))
-            wallet.force_watching_only = True
-
-    def on_create_wallet(self, wallet, wizard):
-        assert type(wallet) == self.wallet_class
-        wallet.handler = BTChipQTHandler(wizard)
-#        self.select_device(wallet)
-        wallet.create_hd_account(None)
-
-class BTChipQTHandler:
-
-    def __init__(self, win):
-        self.win = win
-        self.win.connect(win, SIGNAL('btchip_done'), self.dialog_stop)
-        self.win.connect(win, SIGNAL('btchip_message_dialog'), self.message_dialog)
-        self.win.connect(win, SIGNAL('btchip_auth_dialog'), self.auth_dialog)
-        self.done = threading.Event()
-
-    def stop(self):
-        self.win.emit(SIGNAL('btchip_done'))
-
-    def show_message(self, msg):
-        self.message = msg
-        self.win.emit(SIGNAL('btchip_message_dialog'))
-
-    def prompt_auth(self, msg):
-        self.done.clear()
-        self.message = msg
-        self.win.emit(SIGNAL('btchip_auth_dialog'))
-        self.done.wait()
-        return self.response
-
-    def auth_dialog(self):
-        response = QInputDialog.getText(None, "Ledger Wallet Authentication", self.message, QLineEdit.Password)
-        if not response[1]:
-            self.response = None
-        else:
-            self.response = str(response[0])
-        self.done.set()
-
-    def message_dialog(self):
-        self.d = QDialog()
-        self.d.setModal(1)
-        self.d.setWindowTitle('Ledger')
-        self.d.setWindowFlags(self.d.windowFlags() | QtCore.Qt.WindowStaysOnTopHint)
-        l = QLabel(self.message)
-        vbox = QVBoxLayout(self.d)
-        vbox.addWidget(l)
-        self.d.show()
-
-    def dialog_stop(self):
-        if self.d is not None:
-            self.d.hide()
-            self.d = None
+#    @classmethod
+#    def pin_matrix_widget_class(self):
+#        from trezorlib.qt.pinmatrix import PinMatrixWidget
+#        return PinMatrixWidget
