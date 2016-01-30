1c8e39b635accea49b81c6d2a450987fff447370
BTChip <contact@btchip.com>
2016-01-30 08:11:41 +0100

    Override word_dialog for PIN entry

--- plugins/ledger/qt.py
+++ plugins/ledger/qt.py
@@ -35,3 +35,11 @@ class BTChipQTHandler(QtHandlerBase):
     def __init__(self, win):
         super(BTChipQTHandler, self).__init__(win, 'Ledger')
 
+
+    def word_dialog(self, msg):
+        response = QInputDialog.getText(self.top_level_window(), "Ledger Wallet Authentication", msg, QLineEdit.Password)
+        if not response[1]:
+            self.word = None
+        else:
+            self.word = str(response[0])
+        self.done.set()                
