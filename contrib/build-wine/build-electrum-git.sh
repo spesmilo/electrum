469c20ad0072ebf8a0d7740c5fbedff905d5d81e
Mitchell <admin@bitcoininformation.info>
2016-03-08 19:42:01 +0100

    Change: Massivly improved the Windows installer

--- contrib/build-wine/build-electrum-git.sh
+++ contrib/build-wine/build-electrum-git.sh
@@ -60,6 +60,7 @@ rm -rf dist/
 $PYTHON "C:/pyinstaller/pyinstaller.py" --noconfirm --ascii -w deterministic.spec
 
 # build NSIS installer
+# $VERSION could be passed to the electrum.nsi script, but this would require some rewriting in the script iself.
 wine "$WINEPREFIX/drive_c/Program Files (x86)/NSIS/makensis.exe" electrum.nsi
 
 cd dist
