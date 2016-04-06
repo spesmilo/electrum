62d6253d9310500d42a7a9c826c256d51fd1667a
ThomasV <thomasv@electrum.org>
2016-04-06 15:46:23 +0200

    follow up 355f2e07d4657f13d9d3844a51196ffe16c9082e

--- gui/kivy/uix/screens.py
+++ gui/kivy/uix/screens.py
@@ -164,7 +164,7 @@ class HistoryScreen(CScreen):
             ri.icon = icon
             ri.date = date_time
             ri.message = message
-            ri.value = value if value is not None else 0
+            ri.value = value or 0
             ri.value_known = value is not None
             ri.quote_text = quote_text
             ri.confirmations = conf
