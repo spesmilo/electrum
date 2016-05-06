a452356d0345378b8f6c4dccf23f778f95b08176
slush0 <slush@satoshilabs.com>
2016-05-06 05:44:23 +0200

    Use Bridge transport if available; implementing #1139 Bumping required TREZOR firmware to 1.3.3 (includes important fixes)

--- plugins/trezor/trezor.py
+++ plugins/trezor/trezor.py
@@ -9,13 +9,14 @@ class TrezorWallet(TrezorCompatibleWallet):
 class TrezorPlugin(TrezorCompatiblePlugin):
     firmware_URL = 'https://www.mytrezor.com'
     libraries_URL = 'https://github.com/trezor/python-trezor'
-    minimum_firmware = (1, 2, 1)
+    minimum_firmware = (1, 3, 3)
     wallet_class = TrezorWallet
     try:
         from .client import TrezorClient as client_class
         import trezorlib.ckd_public as ckd_public
         from trezorlib.client import types
         from trezorlib.transport_hid import HidTransport, DEVICE_IDS
+        from trezorlib.transport_bridge import BridgeTransport
         libraries_available = True
     except ImportError:
         libraries_available = False
