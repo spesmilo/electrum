0ccc812f869571f52b68b5b2efb7eed1792216cd
ThomasV <thomasv@electrum.org>
2016-06-15 11:16:29 +0200

    kivy: add multisig wallets to install wizard

--- gui/kivy/uix/dialogs/installwizard.py
+++ gui/kivy/uix/dialogs/installwizard.py
@@ -1,4 +1,5 @@
-from electrum import Wallet
+import os
+from electrum.wallet import Wallet, Multisig_Wallet
 from electrum_gui.kivy.i18n import _
 
 from kivy.app import App
@@ -12,7 +13,7 @@ import threading
 from functools import partial
 import weakref
 
-from create_restore import CreateRestoreDialog, ShowSeedDialog, RestoreSeedDialog, RestoreXpubDialog
+from create_restore import WizardChoiceDialog, ShowSeedDialog, RestoreSeedDialog, AddXpubDialog, WizardMultisigDialog
 from password_dialog import PasswordDialog
 
 
@@ -36,6 +37,7 @@ class InstallWizard(Widget):
         self.config  = config
         self.network = network
         self.storage = storage
+        self.wallet = None
 
     def waiting_dialog(self, task, msg, on_complete=None):
         '''Perform a blocking task in the background by running the passed
@@ -68,22 +70,51 @@ class InstallWizard(Widget):
         else:
             raise BaseException("unknown action", action)
 
+    def on_release(self, dialog, button):
+        if not button or button.action == 'cancel':
+            # soft back or escape button pressed
+            return self.dispatch('on_wizard_complete', None)
+        action = button.action if self.wallet is None else self.wallet.get_action()
+        print "action", action
+        dialog.close()
+        self.run(action)
+
+    def add_seed_or_xpub(self):
+        msg = ' '.join([
+            _("Do you want to create a new seed, or to restore a wallet using an existing seed?")
+        ])
+        choices = [
+            (_('Create a new seed'), 'create_seed'),
+            (_('I already have a seed'), 'restore_seed'),
+            (_('Watching-only wallet'), 'restore_xpub')
+        ]
+        WizardChoiceDialog(msg=msg, choices=choices, on_release=self.on_release).open()
+
     def new(self):
+        name = os.path.basename(self.storage.path)
+        msg = "\n".join([
+            _("Welcome to the Electrum installation wizard."),
+            _("The wallet '%s' does not exist.") % name,
+            _("What kind of wallet do you want to create?")
+        ])
+        choices = [
+            (_('Standard wallet'), 'create_standard'),
+            (_('Multi-signature wallet'), 'create_multisig'),
+        ]
+        WizardChoiceDialog(msg=msg, choices=choices, on_release=self.on_release).open()
+
+    def choose_cosigners(self):
         def on_release(dialog, button):
             if not button:
                 # soft back or escape button pressed
                 return self.dispatch('on_wizard_complete', None)
+            m = dialog.ids.m.value
+            n = dialog.ids.n.value
             dialog.close()
-            action = dialog.action
-            if button == dialog.ids.create:
-                self.run('create')
-            elif button == dialog.ids.restore_seed:
-                self.run('restore_seed')
-            elif button == dialog.ids.restore_xpub:
-                self.run('restore_xpub')
-            else:
-                self.dispatch('on_wizard_complete', None)
-        CreateRestoreDialog(on_release=on_release).open()
+            self.wallet_type = "%dof%d"%(m, n)
+            self.run('add_seed_or_xpub')
+        name = os.path.basename(self.storage.path)
+        WizardMultisigDialog(on_release=on_release).open()
 
     def restore_seed(self):
         def on_seed(_dlg, btn):
@@ -103,27 +134,59 @@ class InstallWizard(Widget):
                 self.run('new')
                 return
             text = _dlg.get_text()
-            self.run('add_seed', (text, None))
-
+            self.run('create_wallet', (text, None))
         msg = _('To create a watching-only wallet, paste your master public key, or scan it using the camera button.')
-        RestoreXpubDialog(test=Wallet.is_mpk, message=msg, on_release=on_xpub).open()
+        AddXpubDialog(test=Wallet.is_mpk, message=msg, on_release=on_xpub).open()
 
-    def add_seed(self, text, password):
-        def task():
+    def create_standard(self):
+        self.wallet_type = 'standard'
+        self.run('add_seed_or_xpub')
+
+    def create_multisig(self):
+        self.wallet_type = 'multisig'
+        self.run('choose_cosigners')
+
+    def create_wallet(self, text, password):
+        if self.wallet_type == 'standard':
             self.wallet = Wallet.from_text(text, password, self.storage)
+            self.run('create_addresses')
+        else:
+            self.storage.put('wallet_type', self.wallet_type)
+            self.wallet = Multisig_Wallet(self.storage)
+            self.wallet.add_seed(text, password)
+            self.wallet.create_master_keys(password)
+            action = self.wallet.get_action()
+            self.run(action)
+
+    def add_cosigners(self):
+        def on_xpub(_dlg, btn):
+            xpub = _dlg.get_text()
+            _dlg.close()
+            self.wallet.add_master_public_key("x%d/" % 2, xpub)
+            action = self.wallet.get_action()
+            self.run(action)
+        msg = _('Paste your cosigner xpub, or scan it using the camera button.')
+        AddXpubDialog(test=Wallet.is_xpub, message=msg, on_release=on_xpub).open()
+
+    def create_main_account(self):
+        self.wallet.create_main_account()
+        self.run('create_addresses')
+
+    def create_addresses(self):
+        def task():
             self.wallet.create_main_account()
             self.wallet.synchronize()
         msg= _("Electrum is generating your addresses, please wait.")
-        self.waiting_dialog(task, msg, self.terminate)
+        self.waiting_dialog(task, msg, on_complete=self.terminate)
 
-    def create(self):
+    def create_seed(self):
         from electrum.wallet import BIP32_Wallet
         seed = BIP32_Wallet.make_seed()
         msg = _("If you forget your PIN or lose your device, your seed phrase will be the "
                 "only way to recover your funds.")
         def on_ok(_dlg, _btn):
             _dlg.close()
-            if _btn == _dlg.ids.confirm:
+            if _btn == _dlg.ids.next:
                 self.run('confirm_seed', (seed,))
             else:
                 self.run('new')
@@ -134,7 +197,7 @@ class InstallWizard(Widget):
         def on_seed(_dlg, btn):
             if btn is _dlg.ids.back:
                 _dlg.close()
-                self.run('create')
+                self.run('create_seed')
                 return
             _dlg.close()
             self.run('enter_pin', (seed,))
@@ -143,7 +206,8 @@ class InstallWizard(Widget):
 
     def enter_pin(self, seed):
         def callback(pin):
-            self.run('confirm_pin', (seed, pin))
+            action = 'confirm_pin' if pin else 'create_wallet'
+            self.run(action, (seed, pin))
         popup = PasswordDialog()
         popup.init('Choose a PIN code', callback)
         popup.open()
@@ -151,7 +215,7 @@ class InstallWizard(Widget):
     def confirm_pin(self, seed, pin):
         def callback(conf):
             if conf == pin:
-                self.run('add_seed', (seed, pin))
+                self.run('create_wallet', (seed, pin))
             else:
                 app.show_error(_('PIN mismatch'), duration=.5)
                 self.run('enter_pin', (seed,))
