16ed48d52df28764fa52af2da53d8f67e9a13138
ThomasV <thomasv@electrum.org>
2016-08-31 13:50:46 +0200

    Format upgrade: increment seed_version to make sure that old versions of Electrum refuse to open new wallet files.

--- lib/version.py
+++ lib/version.py
@@ -1,7 +1,10 @@
 ELECTRUM_VERSION = '2.7.0'  # version of the client package
 PROTOCOL_VERSION = '0.10'   # protocol version requested
-NEW_SEED_VERSION = 11       # electrum versions >= 2.0
+
 OLD_SEED_VERSION = 4        # electrum versions < 2.0
+NEW_SEED_VERSION = 11       # electrum versions >= 2.0
+FINAL_SEED_VERSION = 12     # electrum >= 2.7 will set this to prevent
+                            # old versions from overwriting new format
 
 
 # The hash of the mnemonic seed must begin with this
