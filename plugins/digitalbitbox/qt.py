2ff98b13a0fa216032c186750aac235c47be5508
djb <djb@shiftdevices.com>
2016-12-28 22:49:09 +0100

    add Digital Bitbox hardware wallet

---
+++ plugins/digitalbitbox/qt.py
@@ -0,0 +1,17 @@
+from PyQt4.Qt import (QInputDialog, QLineEdit)
+from ..hw_wallet.qt import QtHandlerBase, QtPluginBase
+from digitalbitbox import DigitalBitboxPlugin
+
+
+class Plugin(DigitalBitboxPlugin, QtPluginBase):
+    icon_unpaired = ":icons/digitalbitbox_unpaired.png"
+    icon_paired = ":icons/digitalbitbox.png"
+
+    def create_handler(self, window):
+        return DigitalBitbox_Handler(window)
+
+
+class DigitalBitbox_Handler(QtHandlerBase):
+
+    def __init__(self, win):
+        super(DigitalBitbox_Handler, self).__init__(win, 'Digital Bitbox')
