5d83bcba1467808de5f08d37701ff2ad48b26a49
ThomasV <thomasv@electrum.org>
2017-01-07 17:25:09 +0100

    don't expose default servers and ports in __init__

--- gui/qt/network_dialog.py
+++ gui/qt/network_dialog.py
@@ -27,7 +27,7 @@ from PyQt4.QtGui import *
 from PyQt4.QtCore import *
 
 from electrum.i18n import _
-from electrum import DEFAULT_PORTS
+from electrum.network import DEFAULT_PORTS
 from electrum.network import serialize_server, deserialize_server
 
 from util import *
