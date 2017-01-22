09d1512759e8bc5fee1779a7c9cdad1199fb7968
Kacper Żuk <kacper.b.zuk@gmail.com>
2017-01-22 15:58:37 +0100

    Provide warnings about invalid BIP39 checksum in seed dialog

--- lib/tests/test_mnemonic.py
+++ lib/tests/test_mnemonic.py
@@ -1,4 +1,5 @@
 import unittest
+from lib import keystore
 from lib import mnemonic
 from lib import old_mnemonic
 
@@ -27,3 +28,11 @@ class Test_OldMnemonic(unittest.TestCase):
         words = 'hardly point goal hallway patience key stone difference ready caught listen fact'
         self.assertEquals(result, words.split())
         self.assertEquals(old_mnemonic.mn_decode(result), seed)
+
+class Test_BIP39Checksum(unittest.TestCase):
+
+    def test(self):
+        mnemonic = u'gravity machine north sort system female filter attitude volume fold club stay feature office ecology stable narrow fog'
+        is_checksum_valid, is_wordlist_valid = keystore.bip39_is_checksum_valid(mnemonic)
+        self.assertTrue(is_wordlist_valid)
+        self.assertTrue(is_checksum_valid)
