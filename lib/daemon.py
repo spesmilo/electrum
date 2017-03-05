eb7f9c98ce328f15866ab8fca0d58e8a4091b391
ThomasV <thomasv@electrum.org>
2017-03-05 20:33:36 +0100

    command:s inherit fee estimates from daemon

--- lib/daemon.py
+++ lib/daemon.py
@@ -239,6 +239,7 @@ class Daemon(DaemonThread):
         password = config_options.get('password')
         new_password = config_options.get('new_password')
         config = SimpleConfig(config_options)
+        config.fee_estimates = self.network.config.fee_estimates.copy()
         cmdname = config.get('cmd')
         cmd = known_commands[cmdname]
         if cmd.requires_wallet:
