482ad3b20f648a86d278663aab5034721c797987
ThomasV <thomasv@electrum.org>
2017-03-24 09:45:27 +0100

    version 2.8.3 and release notes

--- lib/version.py
+++ lib/version.py
@@ -1,4 +1,4 @@
-ELECTRUM_VERSION = '2.8.2'  # version of the client package
+ELECTRUM_VERSION = '2.8.3'  # version of the client package
 PROTOCOL_VERSION = '0.10'   # protocol version requested
 
 # The hash of the mnemonic seed must begin with this
