7f346648076e72f9d83098cf26d2f9dac66cf58a
ThomasV <thomasv@electrum.org>
2017-08-09 12:40:51 +0200

    package only gui.qt

--- setup-release.py
+++ setup-release.py
@@ -36,7 +36,7 @@ if sys.platform == 'darwin':
         app=[mainscript],
         options=dict(py2app=dict(argv_emulation=False,
                                  includes=['PyQt4.QtCore', 'PyQt4.QtGui', 'PyQt4.QtWebKit', 'PyQt4.QtNetwork', 'sip'],
-                                 packages=['lib', 'gui', 'plugins'],
+                                 packages=['lib', 'gui.qt', 'plugins'],
                                  iconfile='electrum.icns',
                                  plist=plist,
                                  resources=["icons"])),
