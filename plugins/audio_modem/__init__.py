85bf330036889ee6726ed018ad670c16a36d153d
Kefkius <kefkius@mail.com>
2017-08-21 12:32:32 -0400

    Mark incompatible plugins as unsupported

--- plugins/audio_modem/__init__.py
+++ plugins/audio_modem/__init__.py
@@ -3,5 +3,5 @@ from electrum_grs.i18n import _
 fullname =  _('Audio MODEM')
 description = _('Provides support for air-gapped transaction signing.')
 requires = [('amodem', 'http://github.com/romanz/amodem/')]
-available_for = ['qt']
+available_for = []
 
--- plugins/greenaddress_instant/__init__.py
+++ plugins/greenaddress_instant/__init__.py
@@ -2,4 +2,4 @@ from electrum_grs.i18n import _
 
 fullname = 'GreenAddress instant'
 description = _("Allows validating if your transactions have instant confirmations by GreenAddress")
-available_for = ['qt']
+available_for = []
