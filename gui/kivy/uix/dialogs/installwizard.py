bf4837c8471644d80788cb3724f2a854c60115cc
jon <jonaldfyookball@outlook.com>
2017-09-03 20:58:47 -0400

    Fix seed restore for android after cointype changes

--- gui/kivy/uix/dialogs/installwizard.py
+++ gui/kivy/uix/dialogs/installwizard.py
@@ -566,6 +566,7 @@ class RestoreSeedDialog(WizardDialog):
         self.message = _('Please type your seed phrase using the virtual keyboard.')
         self.title = _('Enter Seed')
         self.ext = False
+	self.is_bip39_145=False
 
     def options_dialog(self):
         from .seed_options import SeedOptionsDialog
@@ -666,7 +667,7 @@ class RestoreSeedDialog(WizardDialog):
             tis.focus = False
 
     def get_params(self, b):
-        return (self.get_text(), False, self.ext)
+        return (self.get_text(), False, self.ext, self.is_bip39_145)
 
 
 class ConfirmSeedDialog(RestoreSeedDialog):
