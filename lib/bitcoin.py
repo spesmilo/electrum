7dbd612d5dad13cd6f1c0df32534a578bad331ad
ThomasV <thomasv@electrum.org>
2017-09-12 15:04:57 +0200

    Read default server list from file. Update list with 1.1 servers. Remove nolnet option.

--- lib/bitcoin.py
+++ lib/bitcoin.py
@@ -69,8 +69,6 @@ NOLNET = False
 ADDRTYPE_P2PKH = 0
 ADDRTYPE_P2SH = 5
 SEGWIT_HRP = "bc"
-
-
 HEADERS_URL = "https://headers.electrum.org/blockchain_headers"
 GENESIS = "000000000019d6689c085ae165831e934ff763ae46a2a6c172b3f1b60a8ce26f"
 SERVERLIST = 'servers.json'
