a156ee94bf33cb6bf9fdfcd4180f472c16575210
Roman Zeyde <romanzeyde@google.com>
2017-09-17 22:03:14 +0300

    paytoedit: fix insertCompletion() string handling

--- gui/qt/paytoedit.py
+++ gui/qt/paytoedit.py
@@ -209,10 +209,10 @@ class PayToEdit(ScanQRTextEdit):
         if self.c.widget() != self:
             return
         tc = self.textCursor()
-        extra = completion.length() - self.c.completionPrefix().length()
+        extra = len(completion) - len(self.c.completionPrefix())
         tc.movePosition(QTextCursor.Left)
         tc.movePosition(QTextCursor.EndOfWord)
-        tc.insertText(completion.right(extra))
+        tc.insertText(completion[-extra:])
         self.setTextCursor(tc)
 
 
