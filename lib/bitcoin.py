9f6e174b4ffd5bff6280b205b9168ac765c27c80
ThomasV <thomasv@electrum.org>
2017-10-04 09:24:34 +0200

    fix #2961

--- lib/bitcoin.py
+++ lib/bitcoin.py
@@ -333,7 +333,6 @@ def script_to_p2wsh(script):
     return hash_to_segwit_addr(sha256(bfh(script)))
 
 def p2wpkh_nested_script(pubkey):
-    pubkey = safe_parse_pubkey(pubkey)
     pkh = bh2u(hash_160(bfh(pubkey)))
     return '00' + push_script(pkh)
 
