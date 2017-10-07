5480b7dff50eaac8a10af9fc7d17c8e0a6f410c9
ThomasV <thomasv@electrum.org>
2017-10-07 12:54:50 +0200

    add_input_info: add txin value for RBF with segwit inputs

--- lib/wallet.py
+++ lib/wallet.py
@@ -1073,6 +1073,12 @@ class Abstract_Wallet(PrintError):
         address = txin['address']
         if self.is_mine(address):
             txin['type'] = self.get_txin_type(address)
+            # segwit needs value to sign
+            if txin.get('value') is None and txin['type'] in ['p2wpkh', 'p2wsh', 'p2wpkh-p2sh', 'p2wsh-p2sh']:
+                received, spent = self.get_addr_io(address)
+                item = received.get(txin['prevout_hash']+':%d'%txin['prevout_n'])
+                tx_height, value, is_cb = item
+                txin['value'] = value
             self.add_input_sig_info(txin, address)
 
     def can_sign(self, tx):
