7cf075c754bde9855395ad503c16d778036c2116
ThomasV <thomasv@electrum.org>
2017-10-22 15:14:09 +0200

    follow up b8c7edf0702c17f4d411da72438f1bec19c6f18d

--- gui/qt/qrtextedit.py
+++ gui/qt/qrtextedit.py
@@ -61,7 +61,6 @@ class ScanQRTextEdit(ButtonsTextEdit, MessageBoxMixin):
         except BaseException as e:
             self.show_error(str(e))
             data = ''
-        data = str(data)
         self.setText(data)
         return data
 
