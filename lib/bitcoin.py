8623d8c258cf48662bbb10413412de82b81e33b8
ThomasV <thomasv@electrum.org>
2017-11-01 21:25:43 +0100

    fix magic number for p2wsh-p2sh

--- lib/bitcoin.py
+++ lib/bitcoin.py
@@ -56,14 +56,14 @@ def read_json_dict(filename):
 XPRV_HEADERS = {
     'standard': 0x0488ade4,
     'p2wpkh-p2sh': 0x049d7878,
-    'p2wsh-p2sh': 0x295b004,
+    'p2wsh-p2sh': 0x295b005,
     'p2wpkh': 0x4b2430c,
     'p2wsh': 0x2aa7a99
 }
 XPUB_HEADERS = {
     'standard': 0x0488b21e,
     'p2wpkh-p2sh': 0x049d7cb2,
-    'p2wsh-p2sh': 0x295b43e,
+    'p2wsh-p2sh': 0x295b43f,
     'p2wpkh': 0x4b24746,
     'p2wsh': 0x2aa7ed3
 }
