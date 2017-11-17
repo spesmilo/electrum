1799b8a7aff944699b8bb2d0a822049bb5cefc6f
SomberNight <SomberNight@users.noreply.github.com>
2017-11-17 01:54:48 +0100

    disallow editing labels in Coins tab (QT)

--- gui/qt/utxo_list.py
+++ gui/qt/utxo_list.py
@@ -69,3 +69,7 @@ class UTXOList(MyTreeWidget):
             menu.addAction(_("Details"), lambda: self.parent.show_transaction(tx))
 
         menu.exec_(self.viewport().mapToGlobal(position))
+
+    def on_permit_edit(self, item, column):
+        # disable editing fields in this tab (labels)
+        return False
