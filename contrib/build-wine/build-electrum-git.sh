fe0aa54e4dd54546c1c571a36c188c8c437b0f12
harryub <harry@ub.com>
2017-12-28 14:11:43 +0800

    Build from UB repository

--- contrib/build-wine/build-electrum-git.sh
+++ contrib/build-wine/build-electrum-git.sh
@@ -25,7 +25,7 @@ for repo in electrum electrum-locale electrum-icons; do
 	git checkout master
 	cd ..
     else
-	URL=https://github.com/spesmilo/$repo.git
+	URL=https://github.com/UnitedBitcoin/$repo.git
 	git clone -b master $URL $repo
     fi
 done
