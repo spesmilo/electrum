85fbe76b44201b478975468305fa7e44d7d82a1c
Neil Booth <kyuupichan@gmail.com>
2018-01-07 13:21:13 +0800

    Update attribution

--- lib/cashaddr.py
+++ lib/cashaddr.py
@@ -1,4 +1,5 @@
 # Copyright (c) 2017 Pieter Wuille
+# Copyright (c) 2017 Shammah Chancellor, Neil Booth
 #
 # Permission is hereby granted, free of charge, to any person obtaining a copy
 # of this software and associated documentation files (the "Software"), to deal
