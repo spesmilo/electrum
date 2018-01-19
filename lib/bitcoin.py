f2f2f6233e1c9654dce426150a49b39a08da8faf
Jon Layton <jon@seequ.com>
2018-01-18 18:29:18 -0600

    Zcash/Zclassic strMessageMagic value set

--- lib/bitcoin.py
+++ lib/bitcoin.py
@@ -712,7 +712,7 @@ from ecdsa.util import string_to_number, number_to_string
 
 def msg_magic(message):
     length = bfh(var_int(len(message)))
-    return b"\x18Bitcoin Signed Message:\n" + length + message
+    return b"\x19Zcash Signed Message:\n" + length + message
 
 
 def verify_message(address, sig, message):
