a30bca0c7f8772a15ce74aefedef7c1d282be13e
Lucas Betschart <lucasbetschart@gmail.com>
2018-02-22 10:45:57 +0100

    UI: Remove 2FA Option

--- electrum/base_wizard.py
+++ electrum/base_wizard.py
@@ -129,7 +129,6 @@ class BaseWizard(Logger):
         ])
         wallet_kinds = [
             ('standard',  _("Standard wallet")),
-            ('2fa', _("Wallet with two-factor authentication")),
             ('multisig',  _("Multi-signature wallet")),
             ('imported',  _("Import Bitcoin addresses or private keys")),
         ]
