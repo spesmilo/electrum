dc20eee98c50e6544fdda099b811f9abc6e65ba8
Lucas Betschart <lucasbetschart@gmail.com>
2018-02-23 16:00:39 +0100

    UI: Use FTC as default base

--- electrum/util.py
+++ electrum/util.py
@@ -70,7 +70,7 @@ base_units = {'BTC':8, 'mBTC':5, 'bits':2, 'sat':0}
 base_units_inverse = inv_dict(base_units)
 base_units_list = ['BTC', 'mBTC', 'bits', 'sat']  # list(dict) does not guarantee order
 
-DECIMAL_POINT_DEFAULT = 5  # mBTC
+DECIMAL_POINT_DEFAULT = 8  # mBTC
 
 
 class UnknownBaseUnit(Exception): pass
