a34f02019534449b8d87adbe2c77fe93d6af41a6
Gruve_p <gruve-p@users.noreply.github.com>
2018-03-06 16:04:10 +0100

    electrum - electrum-grs

--- gui/kivy/uix/dialogs/installwizard.py
+++ gui/kivy/uix/dialogs/installwizard.py
@@ -88,7 +88,7 @@ Builder.load_string('''
             height: self.minimum_height
             Label:
                 color: root.text_color
-                text: 'ELECTRUM'
+                text: 'ELECTRUM-GRS'
                 size_hint: 1, None
                 height: self.texture_size[1] if self.opacity else 0
                 font_size: '33sp'
