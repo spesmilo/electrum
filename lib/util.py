68e42854640e8b9161015525dbab108994e9fe54
Gruve_p <gruve-p@users.noreply.github.com>
2018-03-07 15:00:12 +0100

    update android_headers_dir

--- lib/util.py
+++ lib/util.py
@@ -227,7 +227,7 @@ def android_data_dir():
     return PythonActivity.mActivity.getFilesDir().getPath() + '/data'
 
 def android_headers_dir():
-    d = android_ext_dir() + '/org.electrum.electrum'
+    d = android_ext_dir() + '/org.groestlcoin.electrum'
     if not os.path.exists(d):
         os.mkdir(d)
     return d
