b3bd166620b6ca4eda82685611aa9d838a126c5b
Johann Bauer <bauerj@bauerj.eu>
2018-03-08 21:55:24 +0100

    Move Cosignerpool to HTTPS

--- plugins/cosigner_pool/qt.py
+++ plugins/cosigner_pool/qt.py
@@ -43,9 +43,7 @@ import sys
 import traceback
 
 
-PORT = 12344
-HOST = 'cosigner.electrum.org'
-server = ServerProxy('http://%s:%d'%(HOST,PORT), allow_none=True)
+server = ServerProxy('https://cosigner.electrum.org/', allow_none=True)
 
 
 class Listener(util.DaemonThread):
