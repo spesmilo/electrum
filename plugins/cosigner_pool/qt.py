87aee100470be979bb324d186559441e5c914b6a
SomberNight <somber.night@protonmail.com>
2018-03-12 21:23:37 +0100

    fix #4111

--- plugins/cosigner_pool/qt.py
+++ plugins/cosigner_pool/qt.py
@@ -173,7 +173,8 @@ class Plugin(BasePlugin):
         for window, xpub, K, _hash in self.cosigner_list:
             if not self.cosigner_can_sign(tx, xpub):
                 continue
-            message = bitcoin.encrypt_message(bfh(tx.raw), bh2u(K)).decode('ascii')
+            raw_tx_bytes = bfh(str(tx))
+            message = bitcoin.encrypt_message(raw_tx_bytes, bh2u(K)).decode('ascii')
             try:
                 server.put(_hash, message)
             except Exception as e:
