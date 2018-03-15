a9444e02a9fad18881df98e12c18fe6d466aa8ee
Janus <ysangkok@gmail.com>
2018-03-15 16:00:03 +0100

    lightning: march 2018 rebase, without integration

--- gui/kivy/main_window.py
+++ gui/kivy/main_window.py
@@ -70,6 +70,8 @@ Label.register('Roboto',
 
 from electrum.util import base_units, NoDynamicFeeEstimates
 
+from .uix.dialogs.lightning_payer import LightningPayerDialog
+from .uix.dialogs.lightning_channels import LightningChannelsDialog
 
 class ElectrumWindow(App):
 
@@ -578,6 +580,14 @@ class ElectrumWindow(App):
         self._settings_dialog.update()
         self._settings_dialog.open()
 
+    def lightning_payer_dialog(self):
+        d = LightningPayerDialog(self)
+        d.open()
+
+    def lightning_channels_dialog(self):
+        d = LightningChannelsDialog(self)
+        d.open()
+
     def popup_dialog(self, name):
         if name == 'settings':
             self.settings_dialog()
@@ -595,6 +605,8 @@ class ElectrumWindow(App):
                 ref.data = xpub
                 master_public_keys_layout.add_widget(ref)
             popup.open()
+        elif name.endswith("_dialog"):
+            getattr(self, name)()
         else:
             popup = Builder.load_file('gui/kivy/uix/ui_screens/'+name+'.kv')
             popup.open()
