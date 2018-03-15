8dd19a5920d5e586d6f03df405f07515482167fd
SomberNight <somber.night@protonmail.com>
2018-03-15 06:14:57 +0100

    wizard: remove unused method

--- lib/base_wizard.py
+++ lib/base_wizard.py
@@ -470,10 +470,6 @@ class BaseWizard(object):
     def show_xpub_and_add_cosigners(self, xpub):
         self.show_xpub_dialog(xpub=xpub, run_next=lambda x: self.run('choose_keystore'))
 
-    def on_cosigner(self, text, password, i):
-        k = keystore.from_master_key(text, password)
-        self.on_keystore(k)
-
     def choose_seed_type(self):
         title = _('Choose Seed type')
         message = ' '.join([
