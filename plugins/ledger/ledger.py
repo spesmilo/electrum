db9cb63e79ec199db1983ba20137d692bc6807b7
SomberNight <somber.night@protonmail.com>
2018-03-22 15:46:23 +0100

    ledger: mixed up error code..
    follow-up 2ce2b368466a848afdf8c85b65fad98a5f338254

--- plugins/ledger/ledger.py
+++ plugins/ledger/ledger.py
@@ -187,7 +187,7 @@ class Ledger_Client():
             try:
                 self.perform_hw1_preflight()
             except BTChipException as e:
-                if (e.sw == 0x6d00 or e.sw == 0x6f00):
+                if (e.sw == 0x6d00 or e.sw == 0x6700):
                     raise BaseException(_("Device not in Bitcoin mode")) from e
                 raise e
             self.preflightDone = True
