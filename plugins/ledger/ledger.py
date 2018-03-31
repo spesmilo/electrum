2e8b59eac6b444b8d2af90674bc070ba6a7f12c9
Gruve_p <gruve-p@users.noreply.github.com>
2018-03-31 23:13:04 +0200

    BTC > GRS

--- plugins/ledger/ledger.py
+++ plugins/ledger/ledger.py
@@ -174,7 +174,7 @@ class Ledger_Client():
                 self.perform_hw1_preflight()
             except BTChipException as e:
                 if (e.sw == 0x6d00):
-                    raise BaseException("Device not in Bitcoin mode")
+                    raise BaseException("Device not in Groestlcoin mode")
                 raise e
             self.preflightDone = True
 
