094b939a24a0bc0de0c66a023249e9618764a9ab
Janus <ysangkok@gmail.com>
2018-04-09 17:36:50 +0200

    lightning: qt channel dialog, fix for shutdown when lightning disabled

--- electrum/gui/qt/main_window.py
+++ electrum/gui/qt/main_window.py
@@ -89,6 +89,7 @@ from .installwizard import WIF_HELP_TEXT
 from .history_list import HistoryList, HistoryModel
 from .update_checker import UpdateCheck, UpdateCheckThread
 from .lightning_invoice_list import LightningInvoiceList
+from .lightning_channels_list import LightningChannelsList
 
 
 class StatusBarButton(QPushButton):
@@ -175,6 +176,9 @@ class ElectrumWindow(QMainWindow, MessageBoxMixin, Logger):
         if config.get("lightning", False):
             self.lightning_invoices_tab = self.create_lightning_invoices_tab(wallet)
             tabs.addTab(self.lightning_invoices_tab, _("Lightning Invoices"))
+
+            self.lightning_channels_tab = self.create_lightning_channels_tab(wallet)
+            tabs.addTab(self.lightning_channels_tab, _("Lightning Channels"))
 
         def add_optional_tab(tabs, tab, icon, description, name):
             tab.tab_icon = icon
@@ -877,6 +881,10 @@ class ElectrumWindow(QMainWindow, MessageBoxMixin, Logger):
     def create_lightning_invoices_tab(self, wallet):
         self.lightning_invoice_list = LightningInvoiceList(self, wallet.network.lightningworker, wallet.network.lightningrpc)
         return self.lightning_invoice_list
+
+    def create_lightning_channels_tab(self, wallet):
+        self.lightning_channels_list = LightningChannelsList(self, wallet.network.lightningworker, wallet.network.lightningrpc)
+        return self.lightning_channels_list
 
     def create_history_tab(self):
         self.history_model = HistoryModel(self)
