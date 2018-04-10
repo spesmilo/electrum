a9e62937da66dc2ed4a4c12b439e6e2ab2433421
harryub <harry@ub.com>
2018-04-10 18:13:58 +0800

    Upgrade version

--- lib/version.py
+++ lib/version.py
@@ -1,4 +1,4 @@
-ELECTRUM_VERSION = '3.0.9'   # version of the client package
+ELECTRUM_VERSION = '3.1.0'   # version of the client package
 PROTOCOL_VERSION = '1.1'     # protocol version requested
 
 # The hash of the mnemonic seed must begin with this
