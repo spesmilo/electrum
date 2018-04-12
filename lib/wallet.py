37d73c3165e361c32382d6a1b76a3775a1cd978e
ThomasV <thomasv@electrum.org>
2018-04-12 12:00:31 +0200

    lnbase: expose wallet object in LNWorker

--- lib/wallet.py
+++ lib/wallet.py
@@ -1313,6 +1313,9 @@ class Abstract_Wallet(PrintError):
                 network.lightningworker = LightningWorker(self, network, network.config)
                 network.lightningrpc = LightningRPC()
             network.lightninglock.release()
+            if network.config.get("lnbase", False):
+                from .lnbase import LNWorker
+                self.lnworker = LNWorker(self, network)
         else:
             self.verifier = None
             self.synchronizer = None
