41f533e00fb235fcea3802de62d5f25844a54309
ThomasV <thomasv@electrum.org>
2018-04-13 17:07:42 +0200

    lnbase: fix locktime and nsequence

--- lib/tests/test_lnbase.py
+++ lib/tests/test_lnbase.py
@@ -1,7 +1,7 @@
 import json
 import unittest
 from lib.util import bh2u
-from lib.lnbase import make_commitment, get_locktime
+from lib.lnbase import make_commitment, get_obscured_ctn
 from lib.transaction import Transaction
 from lib import bitcoin
 
@@ -18,7 +18,7 @@ class Test_LNBase(unittest.TestCase):
 
         local_payment_basepoint = bytes.fromhex('034f355bdcb7cc0af728ef3cceb9615d90684bb5b2ca5f859ab0f0b704075871aa')
         remote_payment_basepoint = bytes.fromhex('032c0b7cf95324a07d05398b240174dc0c2be444d96b159aa6c7f7b1e668680991')
-        locktime = get_locktime(42, local_payment_basepoint, remote_payment_basepoint)
+        obs = get_obscured_ctn(42, local_payment_basepoint, remote_payment_basepoint)
 
         local_funding_privkey = bytes.fromhex('30ff4956bbdd3222d44cc5e8a1261dab1e07957bdac5ae88fe3261ef321f374901')
         local_funding_pubkey = bytes.fromhex('023da092f6980e58d2c037173180e9a465476026ee50f96695963e8efe436f54eb')
@@ -47,7 +47,6 @@ class Test_LNBase(unittest.TestCase):
             local_revocation_pubkey, local_delayedpubkey,
             funding_tx_id, funding_output_index, funding_amount_satoshi)
         our_commit_tx.sign({bh2u(local_funding_pubkey): (local_funding_privkey[:-1], True)})
-
         ref_commit_tx_str = '02000000000101bef67e4e2fb9ddeeb3461973cd4c62abb35050b1add772995b820b584a488489000000000038b02b8002c0c62d0000000000160014ccf1af2f2aabee14bb40fa3851ab2301de84311054a56a00000000002200204adb4e2f00643db396dd120d4e7dc17625f5f2c11a40d857accc862d6b7dd80e0400473044022051b75c73198c6deee1a875871c3961832909acd297c6b908d59e3319e5185a46022055c419379c5051a78d00dbbce11b5b664a0c22815fbcc6fcef6b1937c383693901483045022100f51d2e566a70ba740fc5d8c0f07b9b93d2ed741c3c0860c613173de7d39e7968022041376d520e9c0e1ad52248ddf4b22e12be8763007df977253ef45a4ca3bdb7c001475221023da092f6980e58d2c037173180e9a465476026ee50f96695963e8efe436f54eb21030e9f7b623d2ccc7c9bd44d66d5ce21ce504c0acf6385a132cec6d3c39fa711c152ae3e195220'
         ref_commit_tx = Transaction(ref_commit_tx_str)
 
