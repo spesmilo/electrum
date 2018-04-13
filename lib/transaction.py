690c92a54af8dc8f5836dc767c0bd35c44f7c6d6
ThomasV <thomasv@electrum.org>
2018-04-13 15:17:14 +0200

    lnbase: create unit test for commitment tx

--- lib/transaction.py
+++ lib/transaction.py
@@ -730,11 +730,12 @@ class Transaction:
         return d
 
     @classmethod
-    def from_io(klass, inputs, outputs, locktime=0):
+    def from_io(klass, inputs, outputs, locktime=0, version=1):
         self = klass(None)
         self._inputs = inputs
         self._outputs = outputs
         self.locktime = locktime
+        self.version = version
         return self
 
     @classmethod
