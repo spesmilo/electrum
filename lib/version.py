df007baa575b292973cfa38c521920274f89b470
Neil Booth <kyuupichan@gmail.com>
2018-04-20 06:05:44 +0800

    Bump protocol version to 1.2

--- lib/version.py
+++ lib/version.py
@@ -1,5 +1,5 @@
 PACKAGE_VERSION = '3.1.6'     # version of the client package
-PROTOCOL_VERSION = '1.1'     # protocol version requested
+PROTOCOL_VERSION = '1.2'     # protocol version requested
 
 # The hash of the mnemonic seed must begin with this
 SEED_PREFIX      = '01'      # Standard wallet
