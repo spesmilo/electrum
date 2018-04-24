5284aef8209cbcfa39635d5fd36c0d6d00a2e57e
mhitcher <38318497+mhitcher@users.noreply.github.com>
2018-04-24 03:28:38 -0400

    added therocktrading exchange rate (#4272)

--- lib/exchange_rate.py
+++ lib/exchange_rate.py
@@ -114,7 +114,6 @@ class ExchangeBase(PrintError):
         rates = self.get_rates('')
         return sorted([str(a) for (a, b) in rates.items() if b is not None and len(a)==3])
 
-
 class BitcoinAverage(ExchangeBase):
 
     def get_rates(self, ccy):
@@ -325,9 +324,12 @@ class NegocieCoins(ExchangeBase):
         json = self.get_json('api.bitvalor.com', '/v1/ticker.json')
         return {'BRL': Decimal(json['ticker_1h']['exchanges']['NEG']['last'])}
 
-    def history_ccys(self):
-        return ['BRL']
+class TheRockTrading(ExchangeBase):
 
+    def get_rates(self, ccy):
+        json = self.get_json('api.therocktrading.com', 
+                             '/v1/funds/BTCEUR/ticker')
+        return {'EUR': Decimal(json['last'])}
 
 class Unocoin(ExchangeBase):
 
