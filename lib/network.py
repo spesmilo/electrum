ad56da2ab9f9930c21b395fa838c56f94801b8fb
Harm Aarts <harmaarts@gmail.com>
2018-05-22 10:32:10 +0200

    Remove explicit send from scripts

--- lib/network.py
+++ lib/network.py
@@ -1105,13 +1105,17 @@ class Network(util.DaemonThread):
         command = 'blockchain.scripthash.get_history'
         return (command, [hash])
 
-    def subscribe_to_headers(self):
+    def subscribe_to_headers(self, callback=None):
         command = 'blockchain.headers.subscribe'
-        return (command, [True])
+        invocation = lambda c: self.send((command, [True]), c)
 
-    def subscribe_to_address(self, address):
+        return Network.__invoke(invocation, callback)
+
+    def subscribe_to_address(self, address, callback=None):
         command = 'blockchain.address.subscribe'
-        return (command, [address])
+        invocation = lambda c: self.send((command, [address]), c)
+
+        return Network.__invoke(invocation, callback)
 
     def get_merkle_for_transaction(self, transaction_hash, transaction_height, callback=None):
         command = 'blockchain.transaction.get_merkle'
