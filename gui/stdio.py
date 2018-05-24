ed903a044ca3506aaf51c40ecdf93184241b887a
Gruve_p <gruve-p@users.noreply.github.com>
2018-05-24 10:54:44 +0200

    electrum > electrum-grs

--- gui/stdio.py
+++ gui/stdio.py
@@ -16,7 +16,7 @@ class ElectrumGui:
         self.network = daemon.network
         storage = WalletStorage(config.get_wallet_path())
         if not storage.file_exists:
-            print("Wallet not found. try 'electrum create'")
+            print("Wallet not found. try 'electrum-grs create'")
             exit()
         if storage.is_encrypted():
             password = getpass.getpass('Password:', stream=None)
@@ -208,12 +208,12 @@ class ElectrumGui:
             print(_('Error'))
 
     def network_dialog(self):
-        print("use 'electrum setconfig server/proxy' to change your network settings")
+        print("use 'electrum-grs setconfig server/proxy' to change your network settings")
         return True
 
 
     def settings_dialog(self):
-        print("use 'electrum setconfig' to change your settings")
+        print("use 'electrum-grs setconfig' to change your settings")
         return True
 
     def password_dialog(self):
