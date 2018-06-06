610b37890f13191e96b347570334779b2e062462
SomberNight <somber.night@protonmail.com>
2018-06-06 19:38:59 +0200

    kivy: after deleting wallet, try to open saved wallet
    saved wallet = "wallet that was last open during a graceful shutdown"
    
    previously we would just try to open "default_wallet". now, if the deleted wallet is not the saved wallet, we open the saved wallet; else try to open "default_wallet". if "default_wallet" does not exist, as before, the wizard launches.

--- gui/kivy/main_window.py
+++ gui/kivy/main_window.py
@@ -921,9 +921,7 @@ class ElectrumWindow(App):
         self.stop_wallet()
         os.unlink(wallet_path)
         self.show_error(_("Wallet removed: {}").format(basename))
-        d = os.listdir(dirname)
-        name = 'default_wallet'
-        new_path = os.path.join(dirname, name)
+        new_path = self.electrum_config.get_wallet_path()
         self.load_wallet_by_name(new_path)
 
     def show_seed(self, label):
