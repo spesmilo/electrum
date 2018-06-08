32965e5cd5c4576c128a27e5e6dac0d360650e7f
Roger Taylor <roger.taylor.email@gmail.com>
2018-06-08 21:02:03 +1200

    Improve the fix with a more helpful error message, given that people seem to be importing wallets.

--- gui/qt/qrtextedit.py
+++ gui/qt/qrtextedit.py
@@ -50,7 +50,7 @@ class ScanQRTextEdit(ButtonsTextEdit, MessageBoxMixin):
             with open(fileName, "r", encoding='utf-8') as f:
                 data = f.read()
         except UnicodeDecodeError as reason:
-            self.show_critical(_("Electron Cash was unable to open your file") + "\n" + str(reason), title=_("Unable to read file"))
+            self.show_critical(_("The selected file appears to be a binary file.") +"\n"+ _("Please ensure you only import text files."), title=_("Not a text file"))
             return
         self.setText(data)
 
