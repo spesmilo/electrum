1dec209ed17302521fd809ba76ac9a0fb6e1bc28
Janus <ysangkok@gmail.com>
2018-06-22 17:02:04 +0200

    add simnet support

--- lib/constants.py
+++ lib/constants.py
@@ -101,9 +101,20 @@ class BitcoinRegtest(BitcoinTestnet):
     CHECKPOINTS = []
 
 
+class BitcoinSimnet(BitcoinTestnet):
+
+    SEGWIT_HRP = "sb"
+    GENESIS = "683e86bd5c6d110d91b94b97137ba6bfe02dbbdb8e3dff722a669b5d69d77af6"
+    DEFAULT_SERVERS = read_json('servers_regtest.json', {})
+    CHECKPOINTS = []
+
+
 # don't import net directly, import the module instead (so that net is singleton)
 net = BitcoinMainnet
 
+def set_simnet():
+    global net
+    net = BitcoinSimnet
 
 def set_mainnet():
     global net
