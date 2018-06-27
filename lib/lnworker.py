96aeca00b4469a280501046fc1b9de3fc3e0a5b7
Janus <ysangkok@gmail.com>
2018-06-27 20:23:03 +0200

    ln: merge OpenChannel and HTLCStateMachine

--- lib/lnworker.py
+++ lib/lnworker.py
@@ -10,67 +10,12 @@ from . import constants
 from .bitcoin import sha256, COIN
 from .util import bh2u, bfh, PrintError
 from .constants import set_testnet, set_simnet
-from .lnbase import Peer, Outpoint, ChannelConfig, LocalState, RemoteState, Keypair, OnlyPubkeyKeypair, OpenChannel, ChannelConstraints, RevocationStore, calc_short_channel_id, privkey_to_pubkey
+from .lnbase import Peer, calc_short_channel_id, privkey_to_pubkey
 from .lightning_payencode.lnaddr import lnencode, LnAddr, lndecode
 from .ecc import ECPrivkey, CURVE_ORDER, der_sig_from_sig_string
 from .transaction import Transaction
 from .lnhtlc import HTLCStateMachine
-
-is_key = lambda k: k.endswith("_basepoint") or k.endswith("_key")
-
-def maybeDecode(k, v):
-    if k in ["node_id", "channel_id", "short_channel_id", "pubkey", "privkey", "current_per_commitment_point", "next_per_commitment_point", "per_commitment_secret_seed", "current_commitment_signature"] and v is not None:
-        return binascii.unhexlify(v)
-    return v
-
-def decodeAll(v):
-    return {i: maybeDecode(i, j) for i, j in v.items()} if isinstance(v, dict) else v
-
-def typeWrap(k, v, local):
-    if is_key(k):
-        if local:
-            return Keypair(**v)
-        else:
-            return OnlyPubkeyKeypair(**v)
-    return v
-
-def reconstruct_namedtuples(openingchannel):
-    openingchannel = decodeAll(openingchannel)
-    openingchannel=OpenChannel(**openingchannel)
-    openingchannel = openingchannel._replace(funding_outpoint=Outpoint(**openingchannel.funding_outpoint))
-    new_local_config = {k: typeWrap(k, decodeAll(v), True) for k, v in openingchannel.local_config.items()}
-    openingchannel = openingchannel._replace(local_config=ChannelConfig(**new_local_config))
-    new_remote_config = {k: typeWrap(k, decodeAll(v), False) for k, v in openingchannel.remote_config.items()}
-    openingchannel = openingchannel._replace(remote_config=ChannelConfig(**new_remote_config))
-    new_local_state = decodeAll(openingchannel.local_state)
-    openingchannel = openingchannel._replace(local_state=LocalState(**new_local_state))
-    new_remote_state = decodeAll(openingchannel.remote_state)
-    new_remote_state["revocation_store"] = RevocationStore.from_json_obj(new_remote_state["revocation_store"])
-    openingchannel = openingchannel._replace(remote_state=RemoteState(**new_remote_state))
-    openingchannel = openingchannel._replace(constraints=ChannelConstraints(**openingchannel.constraints))
-    return openingchannel
-
-def serialize_channels(channels_dict):
-    serialized_channels = []
-    for chan in channels_dict.values():
-        namedtuples_to_dict = lambda v: {i: j._asdict() if isinstance(j, tuple) else j for i, j in v._asdict().items()}
-        serialized_channels.append({k: namedtuples_to_dict(v) if isinstance(v, tuple) else v for k, v in chan.state._asdict().items()})
-    class MyJsonEncoder(json.JSONEncoder):
-        def default(self, o):
-            if isinstance(o, bytes):
-                return binascii.hexlify(o).decode("ascii")
-            if isinstance(o, RevocationStore):
-                return o.serialize()
-            return super(MyJsonEncoder, self)
-    dumped = MyJsonEncoder().encode(serialized_channels)
-    roundtripped = json.loads(dumped)
-    reconstructed = set(reconstruct_namedtuples(x) for x in roundtripped)
-    if reconstructed != set(x.state for x in channels_dict.values()):
-        raise Exception("Channels did not roundtrip serialization without changes:\n" + repr(reconstructed) + "\n" + repr(channels_dict))
-    return roundtripped
-
-
-
+from .lnbase import Outpoint
 
 # hardcoded nodes
 node_list = [
@@ -91,7 +36,7 @@ class LNWorker(PrintError):
         self.pubkey = ECPrivkey(self.privkey).get_public_key_bytes()
         self.config = network.config
         self.peers = {}
-        self.channels = {x.channel_id: HTLCStateMachine(x) for x in map(reconstruct_namedtuples, wallet.storage.get("channels", []))}
+        self.channels = {x.channel_id: x for x in map(HTLCStateMachine, wallet.storage.get("channels", []))}
         self.invoices = wallet.storage.get('lightning_invoices', {})
         peer_list = network.config.get('lightning_peers', node_list)
         self.channel_state = {chan.channel_id: "DISCONNECTED" for chan in self.channels.values()}
@@ -105,7 +50,7 @@ class LNWorker(PrintError):
 
     def channels_for_peer(self, node_id):
         assert type(node_id) is bytes
-        return {x: y for (x, y) in self.channels.items() if y.state.node_id == node_id}
+        return {x: y for (x, y) in self.channels.items() if y.node_id == node_id}
 
     def add_peer(self, host, port, node_id):
         peer = Peer(self, host, int(port), node_id, request_initial_sync=self.config.get("request_initial_sync", True))
@@ -120,12 +65,12 @@ class LNWorker(PrintError):
         self.channels[openchannel.channel_id] = openchannel
         for node_id, peer in self.peers.items():
             peer.channels = self.channels_for_peer(node_id)
-        if openchannel.state.remote_state.next_per_commitment_point == openchannel.state.remote_state.current_per_commitment_point:
+        if openchannel.remote_state.next_per_commitment_point == openchannel.remote_state.current_per_commitment_point:
             raise Exception("Tried to save channel with next_point == current_point, this should not happen")
-        dumped = serialize_channels(self.channels)
+        dumped = [x.serialize() for x in self.channels.values()]
         self.wallet.storage.put("channels", dumped)
         self.wallet.storage.write()
-        self.network.trigger_callback('channel', openchannel.state)
+        self.network.trigger_callback('channel', openchannel)
 
     def save_short_chan_id(self, chan):
         """
@@ -134,31 +79,31 @@ class LNWorker(PrintError):
         If the Funding TX has not been mined, return None
         """
         assert self.channel_state[chan.channel_id] in ["OPEN", "OPENING"]
-        peer = self.peers[chan.state.node_id]
-        conf = self.wallet.get_tx_height(chan.state.funding_outpoint.txid)[1]
-        if conf >= chan.state.constraints.funding_txn_minimum_depth:
-            block_height, tx_pos = self.wallet.get_txpos(chan.state.funding_outpoint.txid)
+        peer = self.peers[chan.node_id]
+        conf = self.wallet.get_tx_height(chan.funding_outpoint.txid)[1]
+        if conf >= chan.constraints.funding_txn_minimum_depth:
+            block_height, tx_pos = self.wallet.get_txpos(chan.funding_outpoint.txid)
             if tx_pos == -1:
                 self.print_error('funding tx is not yet SPV verified.. but there are '
                                  'already enough confirmations (currently {})'.format(conf))
                 return False
-            chan.state = chan.state._replace(short_channel_id = calc_short_channel_id(block_height, tx_pos, chan.state.funding_outpoint.output_index))
+            chan.short_channel_id = calc_short_channel_id(block_height, tx_pos, chan.funding_outpoint.output_index)
             self.save_channel(chan)
             return True
         return False
 
     def on_channel_utxos(self, chan, utxos):
         outpoints = [Outpoint(x["tx_hash"], x["tx_pos"]) for x in utxos]
-        if chan.state.funding_outpoint not in outpoints:
+        if chan.funding_outpoint not in outpoints:
             self.channel_state[chan.channel_id] = "CLOSED"
         elif self.channel_state[chan.channel_id] == 'DISCONNECTED':
-            peer = self.peers[chan.state.node_id]
+            peer = self.peers[chan.node_id]
             coro = peer.reestablish_channel(chan)
             asyncio.run_coroutine_threadsafe(coro, self.network.asyncio_loop)
 
     def on_network_update(self, event, *args):
         for chan in self.channels.values():
-            peer = self.peers[chan.state.node_id]
+            peer = self.peers[chan.node_id]
             if self.channel_state[chan.channel_id] == "OPENING":
                 res = self.save_short_chan_id(chan)
                 if not res:
@@ -167,7 +112,7 @@ class LNWorker(PrintError):
                 # this results in the channel being marked OPEN
                 peer.funding_locked(chan)
             elif self.channel_state[chan.channel_id] == "OPEN":
-                conf = self.wallet.get_tx_height(chan.state.funding_outpoint.txid)[1]
+                conf = self.wallet.get_tx_height(chan.funding_outpoint.txid)[1]
                 peer.on_network_update(chan, conf)
 
     async def _open_channel_coroutine(self, node_id, amount_sat, push_sat, password):
@@ -200,7 +145,7 @@ class LNWorker(PrintError):
         node_id, short_channel_id = path[0]
         peer = self.peers[node_id]
         for chan in self.channels.values():
-            if chan.state.short_channel_id == short_channel_id:
+            if chan.short_channel_id == short_channel_id:
                 break
         else:
             raise Exception("ChannelDB returned path with short_channel_id that is not in channel list")
@@ -228,18 +173,18 @@ class LNWorker(PrintError):
         self.wallet.storage.write()
 
     def list_channels(self):
-        return serialize_channels(self.channels)
+        return [str(x) for x in self.channels]
 
     def close_channel(self, chan_id):
         chan = self.channels[chan_id]
         # local_commitment always gives back the next expected local_commitment,
         # but in this case, we want the current one. So substract one ctn number
-        old_state = chan.state
-        chan.state = chan.state._replace(local_state=chan.state.local_state._replace(ctn=chan.state.local_state.ctn - 1))
+        old_local_state = chan.local_state
+        chan.local_state=chan.local_state._replace(ctn=chan.local_state.ctn - 1)
         tx = chan.local_commitment
-        chan.state = old_state
-        tx.sign({bh2u(chan.state.local_config.multisig_key.pubkey): (chan.state.local_config.multisig_key.privkey, True)})
-        remote_sig = chan.state.local_state.current_commitment_signature
+        chan.local_state = old_local_state
+        tx.sign({bh2u(chan.local_config.multisig_key.pubkey): (chan.local_config.multisig_key.privkey, True)})
+        remote_sig = chan.local_state.current_commitment_signature
         remote_sig = der_sig_from_sig_string(remote_sig) + b"\x01"
         none_idx = tx._inputs[0]["signatures"].index(None)
         tx.add_signature_to_txin(0, none_idx, bh2u(remote_sig))
