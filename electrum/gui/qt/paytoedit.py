651c689223931562da6d46fd075e28042eeb75ba
ThomasV <thomasv@electrum.org>
2018-07-16 11:40:04 +0200

    do not set background in frozen_style, it does not look good with dark theme

--- electrum/gui/qt/paytoedit.py
+++ electrum/gui/qt/paytoedit.py
@@ -38,7 +38,7 @@ from . import util
 
 RE_ALIAS = '(.*?)\s*\<([0-9A-Za-z]{1,})\>'
 
-frozen_style = "QWidget { background-color:none; border:none;}"
+frozen_style = "QWidget {border:none;}"
 normal_style = "QPlainTextEdit { }"
 
 
