80f7b09cfbe9e820de8b7a7aed2c053f2ef0054e
SomberNight <somber.night@protonmail.com>
2018-07-17 21:27:59 +0200

    allow paying invoice without amount. min feerate 253 sat/kw.

--- electrum/util.py
+++ electrum/util.py
@@ -127,6 +127,7 @@ class BitcoinException(Exception): pass
 class UserFacingException(Exception):
     """Exception that contains information intended to be shown to the user."""
 
+class InvoiceError(Exception): pass
 
 # Throw this exception to unwind the stack like when an error occurs.
 # However unlike other exceptions the user won't be informed.
