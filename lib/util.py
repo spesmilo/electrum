6314b9bc881f406b09400ed04eb8cb7fee384cc8
sunnypickle <sunny@ub.com>
2018-07-31 10:55:39 +0800

    just local test

--- lib/util.py
+++ lib/util.py
@@ -220,8 +220,8 @@ class ForkData:
     second_fork_max_index = 283
 
     third_fork_chunk_size = 1
-    third_fork_height = 551111 # 524000
-    third_fork_max_index = 4755
+    third_fork_height = 524111 # 524000
+    third_fork_max_index = 2055
     MINING_TYPE_POW = 0x02000000
     MINING_TYPE_POS = 0x01000000
 
@@ -410,7 +410,7 @@ def user_dir():
     elif os.name == 'posix':
         return os.path.join(os.environ["HOME"], ".ubtc_electrum")
     elif "APPDATA" in os.environ:
-        return os.path.join(os.environ["APPDATA"], "ubtc_Electrum")
+        return os.path.join(os.environ["APPDATA"], "ubtc_Electrum_test")
     elif "LOCALAPPDATA" in os.environ:
         return os.path.join(os.environ["LOCALAPPDATA"], "ubtc_Electrum")
     else:
