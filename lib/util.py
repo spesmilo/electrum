399c716bbdb2fa2191894030c8ae046075b83c5e
sunnypickle <sunny@ub.com>
2018-08-08 09:32:44 +0800

    fix multisig wallet call contract bugs

--- lib/util.py
+++ lib/util.py
@@ -220,10 +220,8 @@ class ForkData:
     second_fork_max_index = 283
 
     third_fork_chunk_size = 1
-    # third_fork_height = 551111 # 524000
-    # third_fork_max_index = 4755
-    third_fork_height = 524111  # 524000
-    third_fork_max_index = 2055
+    third_fork_height = 551111 # 524000
+    third_fork_max_index = 4755
     MINING_TYPE_POW = 0x02000000
     MINING_TYPE_POS = 0x01000000
 
@@ -306,8 +304,8 @@ def ub_default_diffculty(is_pos):
     if is_pos ==0:
         return 0x00000000ffffffffffffffffffffffffffffffffffffffffffffffffffffffff
     else:
-        #return 0x000000000000ffffffffffffffffffffffffffffffffffffffffffffffffffff
-        return 0x0fffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffff
+        return 0x000000000000ffffffffffffffffffffffffffffffffffffffffffffffffffff
+        #return 0x0fffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffff
 
 
 def android_ext_dir():
@@ -413,7 +411,7 @@ def user_dir():
     elif os.name == 'posix':
         return os.path.join(os.environ["HOME"], ".ubtc_electrum")
     elif "APPDATA" in os.environ:
-        return os.path.join(os.environ["APPDATA"], "ubtc_Electrum_test")
+        return os.path.join(os.environ["APPDATA"], "ubtc_Electrum")
     elif "LOCALAPPDATA" in os.environ:
         return os.path.join(os.environ["LOCALAPPDATA"], "ubtc_Electrum")
     else:
