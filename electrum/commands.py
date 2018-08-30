6ee689345fdba24fddf0a719e906fed406e0d448
SomberNight <somber.night@protonmail.com>
2018-08-30 18:34:04 +0200

    fix -v syntax
    After the introduction of arguments for -v, it would sometimes incorrectly consume the CLI cmd as its argument.
    This change keeps the old "-v" syntax working, at the cost of having to provide the arguments without a whitespace directly after -v (and the args need to be single letters).

--- electrum/commands.py
+++ electrum/commands.py
@@ -832,7 +832,7 @@ def add_global_options(parser):
     group = parser.add_argument_group('global options')
     # const is for when no argument is given to verbosity
     # default is for when the flag is missing
-    group.add_argument("-v", "--verbosity", dest="verbosity", help="Set verbosity filter", default='', const='*', nargs='?')
+    group.add_argument("-v", dest="verbosity", help="Set verbosity filter", default='', const='*', nargs='?')
     group.add_argument("-D", "--dir", dest="electrum_path", help="electrum directory")
     group.add_argument("-P", "--portable", action="store_true", dest="portable", default=False, help="Use local 'electrum_data' directory")
     group.add_argument("-w", "--wallet", dest="wallet_path", help="wallet path")
