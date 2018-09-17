dd5ea287195f7a3676f1cc957b4e6e512c9081ff
Gruve_p <gruve-p@users.noreply.github.com>
2018-09-17 21:55:12 +0200

    Bitcoin > Groestlcoin

--- plugins/hw_wallet/plugin.py
+++ plugins/hw_wallet/plugin.py
@@ -67,7 +67,7 @@ class HW_PluginBase(BasePlugin):
         if keystore is None:
             keystore = wallet.get_keystore()
         if not is_address(address):
-            keystore.handler.show_error(_('Invalid Bitcoin Address'))
+            keystore.handler.show_error(_('Invalid Groestlcoin Address'))
             return False
         if not wallet.is_mine(address):
             keystore.handler.show_error(_('Address not in wallet.'))
