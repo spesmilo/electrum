8421f642ab14c08f56cd4c47cc6b2a7c4b8cb5e9
ThomasV <thomasv@electrum.org>
2018-10-14 15:42:05 +0200

    start channel verifier in network.start()

--- electrum/network.py
+++ electrum/network.py
@@ -830,11 +830,6 @@ class Network(PrintError):
         self._jobs = jobs or []
         asyncio.run_coroutine_threadsafe(self._start(), self.asyncio_loop)
 
-    async def add_job(self, job):
-        async with self.restart_lock:
-            self._jobs.append(job)
-            await self.main_taskgroup.spawn(job)
-
     async def _stop(self, full_shutdown=False):
         self.print_error("stopping network")
         try:
