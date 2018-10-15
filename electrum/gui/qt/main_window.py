a0acec97203e01d41cc3d203bee893abd5f1d552
ThomasV <thomasv@electrum.org>
2018-10-15 11:47:42 +0200

    gather definitions of LN exceptions

--- electrum/gui/qt/main_window.py
+++ electrum/gui/qt/main_window.py
@@ -62,6 +62,7 @@ from electrum.util import (format_time, format_satoshis, format_fee_satoshis,
                            UnknownBaseUnit, DECIMAL_POINT_DEFAULT, UserFacingException,
                            get_new_wallet_name, send_exception_to_crash_reporter,
                            InvalidBitcoinURI, InvoiceError)
+from electrum.lnutil import PaymentFailure
 from electrum.transaction import Transaction, TxOutput
 from electrum.address_synchronizer import AddTransactionException
 from electrum.wallet import (Multisig_Wallet, CannotBumpFee, Abstract_Wallet,
