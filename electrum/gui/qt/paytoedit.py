a5ab431b4bd880ffa51b83c0dc43ce59c3e51f00
ThomasV <thomasv@electrum.org>
2018-10-24 11:42:01 +0200

    parse invoices with lightning: prefix

--- electrum/gui/qt/paytoedit.py
+++ electrum/gui/qt/paytoedit.py
@@ -131,6 +131,9 @@ class PayToEdit(CompletionTextEdit, ScanQRTextEdit, Logger):
             if data.startswith("bitcoin:"):
                 self.win.pay_to_URI(data)
                 return
+            l = data.lower()
+            if l.startswith("lightning:"):
+                data = l[10:]
             if data.startswith("ln"):
                 self.win.parse_lightning_invoice(data)
                 self.lightning_invoice = data
