f7cf463b6c3bf2b175ab690712c63c78411fc8de
Mark B Lundeberg <36528214+markblundeberg@users.noreply.github.com>
2018-11-04 23:30:45 -0800

    Further improvements on windows building (#920)
    * .gitignore tmp/ subdirectory made during build-wine
    
    * build-wine: default to hardcoded specific tag instead of master
    
    * build-wine : don't make tarballs if make_packages / pyrcc5 hasn't been run.
    
    also, add a sha256sum of icons_rc.py to help in case of discrepancy.
    
    * build-wine : add note to README about first-time failure from gpg

--- contrib/build-wine/build-electrum-git.sh
+++ contrib/build-wine/build-electrum-git.sh
@@ -3,6 +3,8 @@
 NAME_ROOT=electrum
 PYTHON_VERSION=3.5.4
 
+CHECKOUT_TAG=3.3.1
+
 # These settings probably don't need any change
 export WINEPREFIX=/opt/wine64
 export PYTHONDONTWRITEBYTECODE=1
@@ -54,10 +56,12 @@ pushd electrum
 
 if [ ! -z "$1" ]; then
     git checkout $1
+else
+    git checkout "$CHECKOUT_TAG"
 fi
 
 VERSION=`git describe --tags`
-echo "Last commit: $VERSION"
+echo "Version to release: $VERSION"
 find -exec touch -d '2000-11-11T11:11:11+00:00' {} +
 popd
 
@@ -99,7 +103,13 @@ cd dist
 mv electrum-setup.exe $NAME_ROOT-$VERSION-setup.exe
 
 cd ../../..
-python3 setup.py sdist --format=zip,gztar
-
+if [ -d packages -a -e gui/qt/icons_rc.py ] ; then
+    python3 setup.py sdist --format=zip,gztar
+    sha256sum gui/qt/icons_rc.py
+else
+    echo "Not creating source distribution since packages or icons file missing."
+    echo "Run './contrib/make_packages' and/or 'pyrcc5 icons.qrc -o gui/qt/icons_rc.py'"
+    echo "Then you can run 'python3 setup.py sdist --format=zip,gztar'"
+fi
 
 echo "Done."
