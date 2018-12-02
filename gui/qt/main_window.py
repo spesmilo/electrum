32635317a053710054de593af5224e6945a48fe2
Calin Culianu <calin.culianu@gmail.com>
2018-12-02 14:44:52 +0200

    Pet Peeve: Made Qt's Coins tab -> Details and multisig preview  -> Show Preview actually show you the tx description in the tx dialog opened

--- gui/qt/main_window.py
+++ gui/qt/main_window.py
@@ -1546,7 +1546,7 @@ class ElectrumWindow(QMainWindow, MessageBoxMixin, PrintError):
         def sign_done(success):
             if success:
                 if not tx.is_complete():
-                    self.show_transaction(tx)
+                    self.show_transaction(tx, tx_desc)
                     self.do_clear()
                 else:
                     self.broadcast_transaction(tx, tx_desc)
