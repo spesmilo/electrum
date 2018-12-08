258b5040003c62b57cad8c46843fb31195ddf4b8
SomberNight <somber.night@protonmail.com>
2018-12-08 06:31:28 +0100

    qt main window: unregister network callbacks

--- electrum/gui/qt/main_window.py
+++ electrum/gui/qt/main_window.py
@@ -3102,6 +3102,8 @@ class ElectrumWindow(QMainWindow, MessageBoxMixin, PrintError):
         self.wallet.thread.stop()
         if self.network:
             self.network.unregister_callback(self.on_network)
+            self.network.unregister_callback(self.on_quotes)
+            self.network.unregister_callback(self.on_history)
         self.config.set_key("is_maximized", self.isMaximized())
         if not self.isMaximized():
             g = self.geometry()
