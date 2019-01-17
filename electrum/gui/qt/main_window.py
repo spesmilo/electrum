6d48ed9ce5868daaae43a54f3ae17fd4679c8cdd
Janus <ysangkok@gmail.com>
2019-01-17 15:29:21 +0100

    request_list: select new items

--- electrum/gui/qt/main_window.py
+++ electrum/gui/qt/main_window.py
@@ -1001,6 +1001,7 @@ class ElectrumWindow(QMainWindow, MessageBoxMixin, PrintError):
             self.address_list.update()
         self.clear_receive_tab()
         self.request_list.update()
+        self.request_list.select_key(key)
 
     def create_bitcoin_request(self, amount, message, expiration):
         addr = self.wallet.get_unused_address()
@@ -1025,6 +1026,7 @@ class ElectrumWindow(QMainWindow, MessageBoxMixin, PrintError):
         else:
             self.sign_payment_request(addr)
             #self.save_request_button.setEnabled(False)
+        return addr
 
     def view_and_paste(self, title, msg, data):
         dialog = WindowModalDialog(self, title)
