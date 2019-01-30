b7d93e2e11b8f21d9b4005dbf24b5673869c9bcc
ThomasV <thomasv@electrum.org>
2019-01-30 19:16:04 +0100

    gui: display lightning balance in status bar

--- electrum/gui/qt/main_window.py
+++ electrum/gui/qt/main_window.py
@@ -371,6 +371,7 @@ class ElectrumWindow(QMainWindow, MessageBoxMixin, Logger):
             self.channels_list.update_rows.emit(*args)
         elif event == 'channel':
             self.channels_list.update_single_row.emit(*args)
+            self.update_status()
         elif event == 'ln_status':
             self.need_update_ln.set()
         else:
@@ -850,6 +851,9 @@ class ElectrumWindow(QMainWindow, MessageBoxMixin, Logger):
                     text +=  " [%s unconfirmed]"%(self.format_amount(u, is_diff=True).strip())
                 if x:
                     text +=  " [%s unmatured]"%(self.format_amount(x, is_diff=True).strip())
+                l = self.wallet.lnworker.get_balance()
+                if l:
+                    text += u'    \U0001f5f2 %s'%(self.format_amount_and_units(l).strip())
 
                 # append fiat balance and price
                 if self.fx.is_enabled():
