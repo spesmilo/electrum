b7e0fcba6dc50620528a2be3dd29cc01b2054761
Calin Culianu <calin.culianu@gmail.com>
2019-02-09 15:08:45 +0200

    [Lib] Refactored version related code into version.py
    util.normalize_version got moved into version.py.
    
    Cleaned up re's in version.py to compile on startup.

--- lib/version.py
+++ lib/version.py
@@ -9,6 +9,11 @@ def seed_prefix(seed_type):
     assert seed_type == 'standard'
     return SEED_PREFIX
 
+import re
+
+_RX_VERSION_PARSE = re.compile(r'(\d+)[.](\d+)[.]?(\d*)\s*(\S*)')
+_RX_NORMALIZER = re.compile(r'(\.0+)*$')
+
 def parse_package_version(pvstr):
     """
     Parse a package version string e.g.:
@@ -16,12 +21,18 @@ def parse_package_version(pvstr):
         '3.4.5_iOS' -> (3, 4, 5, '_iOS')
         '3.3.5' -> (3, 3, 5, '')
         '3.3' -> (3, 3, 0, '')
+        and.. perhaps unexpectedly:
+        '3.3.5.1_iOS' -> (3, 3, 5, '.1_iOS') .. so be sure not to have more than 3 version fields + 1 'extra' field!
         etc...
     """
-    import re
-    m = re.search(r'(\d+)[.](\d+)[.]?(\d*)\s*(\S*)', pvstr)
+    m = _RX_VERSION_PARSE.search(pvstr)
     if not m:
         raise ValueError('Failed to parse package version for: ' + str(pvstr))
     major, minor, rev, variant = int(m.group(1)), int(m.group(2)), m.group(3), m.group(4)
     rev = int(rev) if rev else 0
     return major, minor, rev, variant
+
+def normalize_version(v):
+    """Used for PROTOCOL_VERSION normalization, e.g '1.4.0' -> (1,4) """
+    return tuple(int(x) for x in _RX_NORMALIZER.sub('', v.strip()).split("."))
+
