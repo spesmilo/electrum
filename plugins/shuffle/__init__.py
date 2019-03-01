49b740233349bd15fe91702970aed9c37879802d
Calin Culianu <calin.culianu@gmail.com>
2019-03-01 02:23:48 +0200

    Updated CashShuffle plugin description

--- plugins/shuffle/__init__.py
+++ plugins/shuffle/__init__.py
@@ -1,5 +1,9 @@
 from electroncash.i18n import _
 
 fullname = _('CashShuffle')
-description = _("CashShuffle Protocol")
+description = ''.join([
+    _('Protect your privacy and anonymize your coins (UTXOs) by shuffling them with other users of CashShuffle.'), "\n\n",
+    _('A layered encryption scheme is used so that none of the participants know the outputs of the other participants.'), " ",
+    _('In addition, a blame protocol is used to mitigate time-wasting denial-of-service type attacks.')
+])
 available_for = ['qt']
