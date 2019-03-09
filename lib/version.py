f57c9c701b570a831343b12ebada43ec3c39cead
Calin Culianu <calin.culianu@gmail.com>
2019-03-09 13:23:05 +0200

    bump version to 3.9.7ShufBeta; perpare for release

--- lib/version.py
+++ lib/version.py
@@ -1,4 +1,4 @@
-PACKAGE_VERSION  = '3.9.6ShufBeta'  # version of the client package
+PACKAGE_VERSION  = '3.9.7ShufBeta'  # version of the client package
 PROTOCOL_VERSION = '1.4'     # protocol version requested
 
 # The hash of the mnemonic seed must begin with this
