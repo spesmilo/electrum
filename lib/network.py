spesmilo/electrum

commit b7e0fcba6dc50620528a2be3dd29cc01b2054761

[Lib] Refactored version related code into version.py
util.normalize_version got moved into version.py.

Cleaned up re's in version.py to compile on startup.

lib/network.py
@@ -42,7 +42,7 @@ from . import networks
 from .i18n import _
 from .interface import Connection, Interface
 from . import blockchain
-from .version import PACKAGE_VERSION, PROTOCOL_VERSION
+from . import version
 
 
 DEFAULT_AUTO_CONNECT = True
@@ -52,7 +52,6 @@ SERVER_RETRY_INTERVAL = 10
 
 def parse_servers(result):
     """ parse servers list into dict format"""
-    from .version import PROTOCOL_VERSION
     servers = {}
     for item in result:
         host = item[1]
@@ -79,7 +78,7 @@ def parse_servers(result):
 def filter_version(servers):
     def is_recent(version):
         try:
-            return util.normalize_version(version) >= util.normalize_version(PROTOCOL_VERSION)
+            return version.normalize_version(version) >= version.normalize_version(version.PROTOCOL_VERSION)
         except Exception as e:
             return False
     return {k: v for k, v in servers.items() if is_recent(v.get('version'))}
@@ -122,7 +121,7 @@ def servers_to_hostmap(servers):
         m[protocol] = port
         if need_add:
             m['pruning'] = '-' # hmm. this info is missing, so give defaults just to make the map complete.
-            m['version'] = PROTOCOL_VERSION
+            m['version'] = version.PROTOCOL_VERSION
             ret[host] = m
     return ret
 
@@ -814,7 +813,7 @@ class Network(util.DaemonThread):
             self.interfaces[server_key] = interface
 
         # server.version should be the first message
-        params = [PACKAGE_VERSION, PROTOCOL_VERSION]
+        params = [version.PACKAGE_VERSION, version.PROTOCOL_VERSION]
         self.queue_request('server.version', params, interface)
         # The interface will immediately respond with it's last known header.
         self.queue_request('blockchain.headers.subscribe', [], interface)

lib/util.py
@@ -43,9 +43,6 @@ def inv_dict(d):
 base_units = {'BCH':8, 'mBCH':5, 'cash':2}
 fee_levels = [_('Within 25 blocks'), _('Within 10 blocks'), _('Within 5 blocks'), _('Within 2 blocks'), _('In the next block')]
 
-def normalize_version(v):
-    return [int(x) for x in re.sub(r'(\.0+)*$','', v).split(".")]
-
 class NotEnoughFunds(Exception): pass
 
 class ExcessiveFee(Exception): pass

lib/version.py
@@ -9,6 +9,11 @@ def seed_prefix(seed_type):
     assert seed_type == 'standard'
     return SEED_PREFIX
 
+import re
+
+_RX_VERSION_PARSE = re.compile(r'(\d+)[.](\d+)[.]?(\d*)\s*(\S*)')
+_RX_NORMALIZER = re.compile(r'(\.0+)*$')
+
 def parse_package_version(pvstr):
     """
     Parse a package version string e.g.:
@@ -16,12 +21,18 @@ def parse_package_version(pvstr):
         '3.4.5_iOS' -> (3, 4, 5, '_iOS')
         '3.3.5' -> (3, 3, 5, '')
         '3.3' -> (3, 3, 0, '')
+        and.. perhaps unexpectedly:
+        '3.3.5.1_iOS' -> (3, 3, 5, '.1_iOS') .. so be sure not to have more than 3 version fields + 1 'extra' field!
         etc...
     """
-    import re
-    m = re.search(r'(\d+)[.](\d+)[.]?(\d*)\s*(\S*)', pvstr)
+    m = _RX_VERSION_PARSE.search(pvstr)
     if not m:
         raise ValueError('Failed to parse package version for: ' + str(pvstr))
     major, minor, rev, variant = int(m.group(1)), int(m.group(2)), m.group(3), m.group(4)
     rev = int(rev) if rev else 0
     return major, minor, rev, variant
+
+def normalize_version(v):
+    """Used for PROTOCOL_VERSION normalization, e.g '1.4.0' -> (1,4) """
+    return tuple(int(x) for x in _RX_NORMALIZER.sub('', v.strip()).split("."))
+

commit aa830ae59e9e53f4aae524733aa38d34f2152bbb

[Lib] Fixed a bug in filter_version introduced in a recent commit
b7e0fcba6dc50620528a2be3dd29cc01b2054761 was a bad refactoring job which
added a bug in the network.py filter_version function to make it stop
working right.

lib/network.py
@@ -76,9 +76,9 @@ def parse_servers(result):
     return servers
 
 def filter_version(servers):
-    def is_recent(version):
+    def is_recent(vv):
         try:
-            return version.normalize_version(version) >= version.normalize_version(version.PROTOCOL_VERSION)
+            return version.normalize_version(vv) >= version.normalize_version(version.PROTOCOL_VERSION)
         except Exception as e:
             return False
     return {k: v for k, v in servers.items() if is_recent(v.get('version'))}