82faf5177c175e7c397eb8b88c9d3bd45f828fa0
Calin Culianu <calin.culianu@gmail.com>
2019-03-24 08:33:00 +0200

    Updated libsecp submodule commit pointer, fixup in scripts

--- contrib/build-wine/build-secp256k1.sh
+++ contrib/build-wine/build-secp256k1.sh
@@ -5,6 +5,7 @@ set -e
 
 function fail {
     RED='\033[0;31m'
+    NC='\033[0m' # No Color
     printf "\r🗯 ${RED}ERROR:${NC} ${1}\n"
     exit 1
 }
@@ -30,7 +31,7 @@ build_dll() {
 }
 
 pushd ../secp256k1 || fail "Could not chdir to secp256k1"
-LIBSECP_VERSION="a1d5a30364d2ca8ed8bb3ef3dd345cc75708a8b2"  # According to Mark B. Lundeberg, using a commit hash guarantees no repository man-in-the-middle funny business as git is secure when verifying hashes.
+LIBSECP_VERSION="9896f7062e67e05f9a1aa7163099fb2e052db9e8"  # According to Mark B. Lundeberg, using a commit hash guarantees no repository man-in-the-middle funny business as git is secure when verifying hashes.
 git checkout $LIBSECP_VERSION || fail "Could not check out secp256k1 $LIBSECP_VERSION"
 git clean -f -x -q
 
