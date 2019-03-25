0479fa83b7966e7723579f5df93c35623c1dce67
Calin Culianu <calin.culianu@gmail.com>
2019-03-25 12:38:07 +0200

    Fix failing test after switching to Bitcoin ABC libsecp (#1214)
    * fix
    
    * a fixup and comments
    
    * fix a typo

--- lib/tests/test_bitcoin.py
+++ lib/tests/test_bitcoin.py
@@ -74,14 +74,32 @@ class Test_bitcoin(unittest.TestCase):
         sig1_b64 = base64.b64encode(sig1)
         sig2_b64 = base64.b64encode(sig2)
 
-        self.assertEqual(sig1_b64, b'H/9jMOnj4MFbH3d7t4yCQ9i7DgZU/VZ278w3+ySv2F4yIsdqjsc5ng3kmN8OZAThgyfCZOQxZCWza9V5XzlVY0Y=')
-        self.assertEqual(sig2_b64, b'G84dmJ8TKIDKMT9qBRhpX2sNmR0y5t+POcYnFFJCs66lJmAs3T8A6Sbpx7KA6yTQ9djQMabwQXRrDomOkIKGn18=')
+        # NOTE: you cannot rely on exact binary patterns of signatures
+        # produced by libsecp versus python ecdsa, etc. This is because nonces
+        # may differ.  We ran into this when switching from Bitcoin Core libsecp
+        # to Bitcoin ABC libsecp.  Amaury Sechet confirmed this to be true.
+        # So.. the below test is flawed.  Instead, we verify that both
+        # signatures pass. The hard-coded one as well as the generated one.
+        #self.assertEqual(sig1_b64, b'H/9jMOnj4MFbH3d7t4yCQ9i7DgZU/VZ278w3+ySv2F4yIsdqjsc5ng3kmN8OZAThgyfCZOQxZCWza9V5XzlVY0Y=')
+        #self.assertEqual(sig2_b64, b'G84dmJ8TKIDKMT9qBRhpX2sNmR0y5t+POcYnFFJCs66lJmAs3T8A6Sbpx7KA6yTQ9djQMabwQXRrDomOkIKGn18=')
+        #
+        # Hardcoded sigs were generated with an old version of Python ECDSA by Electrum team:
+        our_sig1 = base64.b64decode(b'H/9jMOnj4MFbH3d7t4yCQ9i7DgZU/VZ278w3+ySv2F4yIsdqjsc5ng3kmN8OZAThgyfCZOQxZCWza9V5XzlVY0Y=')
+        our_sig2 = base64.b64decode(b'G84dmJ8TKIDKMT9qBRhpX2sNmR0y5t+POcYnFFJCs66lJmAs3T8A6Sbpx7KA6yTQ9djQMabwQXRrDomOkIKGn18=')
 
+        # can it verify its own sigs?
         self.assertTrue(verify_message(addr1, sig1, msg1))
         self.assertTrue(verify_message(addr2, sig2, msg2))
+        # can it verify our hard-coded sigs?
+        self.assertTrue(verify_message(addr1, our_sig1, msg1))
+        self.assertTrue(verify_message(addr2, our_sig2, msg2))
 
         self.assertRaises(Exception, verify_message, addr1, b'wrong', msg1)
+        # test for bad sigs for a message
         self.assertFalse(verify_message(addr1, sig2, msg1))
+        self.assertFalse(verify_message(addr1, our_sig2, msg1))
+        self.assertFalse(verify_message(addr2, sig1, msg2))
+        self.assertFalse(verify_message(addr2, our_sig1, msg2))
 
     def test_aes_homomorphic(self):
         """Make sure AES is homomorphic."""
