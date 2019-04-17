77cad578fa62017fdf4b6cb5c76175eee8265881
Calin Culianu <calin.culianu@gmail.com>
2019-04-18 00:12:06 +0300

    Added srcdist building of .tar.gz and .zip via Docker
    To build, run:
    
    contrib/build-linux/srcdist_docker/build.sh master

---
+++ contrib/build-linux/srcdist_docker/_build.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+set -e
+
+PROJECT_ROOT="$(dirname "$(readlink -e "$0")")/../../.."
+CONTRIB="$PROJECT_ROOT/contrib"
+DISTDIR="$PROJECT_ROOT/dist"
+
+. "$CONTRIB"/base.sh
+
+rm -fvr "$DISTDIR"
+mkdir -p "$DISTDIR"
+
+python3 --version || fail "No python"
+
+pushd $PROJECT_ROOT
+
+python3 -m pip install --upgrade pip
+python3 -m pip install --upgrade setuptools
+python3 -m pip install --upgrade requests
+
+contrib/make_linux_sdist
+
+popd
---
+++ contrib/build-linux/srcdist_docker/build.sh
@@ -0,0 +1,85 @@
+#!/bin/bash
+
+here=$(dirname "$0")
+test -n "$here" -a -d "$here" || (echo "Cannot determine build dir. FIXME!" && exit 1)
+
+. "$here"/../../base.sh # functions we use below (fail, et al)
+
+if [ ! -z "$1" ]; then
+    REV="$1"
+else
+    fail "Please specify a release tag or branch to build (eg: master or 4.0.0, etc)"
+fi
+
+if [ ! -d 'contrib' ]; then
+    fail "Please run this script form the top-level Electron Cash git directory"
+fi
+
+pushd .
+
+docker_version=`docker --version`
+
+if [ "$?" != 0 ]; then
+    echo ''
+    echo "Please install docker by issuing the following commands (assuming you are on Ubuntu):"
+    echo ''
+    echo '$ curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -'
+    echo '$ sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"'
+    echo '$ sudo apt-get update'
+    echo '$ sudo apt-get install -y docker-ce'
+    echo ''
+    fail "Docker is required to build for Windows"
+fi
+
+set -e
+
+info "Using docker: $docker_version"
+
+SUDO=""  # on macOS (and others?) we don't do sudo for the docker commands ...
+if [ $(uname) = "Linux" ]; then
+    # .. on Linux we do
+    SUDO="sudo"
+fi
+
+
+info "Creating docker image ..."
+$SUDO docker build -t electroncash-srcdist-builder-img \
+    contrib/build-linux/srcdist_docker \
+    || fail "Failed to create docker image"
+
+# This is the place where we checkout and put the exact revision we want to work
+# on. Docker will run mapping this directory to /opt/electroncash
+# which inside wine will look lik c:\electroncash
+FRESH_CLONE=`pwd`/contrib/build-linux/fresh_clone2
+FRESH_CLONE_DIR=$FRESH_CLONE/$GIT_DIR_NAME
+
+(
+    $SUDO rm -fr $FRESH_CLONE && \
+        mkdir -p $FRESH_CLONE && \
+        cd $FRESH_CLONE  && \
+        git clone $GIT_REPO && \
+        cd $GIT_DIR_NAME && \
+        git checkout $REV
+) || fail "Could not create a fresh clone from git"
+
+(
+    $SUDO docker run -it \
+    --name electroncash-srcdist-builder-cont \
+    -v $FRESH_CLONE_DIR:/opt/electroncash \
+    --rm \
+    --workdir /opt/electroncash/contrib/build-linux/srcdist_docker \
+    electroncash-srcdist-builder-img \
+    ./_build.sh $REV
+) || fail "Build inside docker container failed"
+
+popd
+
+info "Copying built files out of working clone..."
+mkdir -p dist/
+cp -fpvR $FRESH_CLONE_DIR/dist/* dist/ || fail "Could not copy files"
+
+info "Removing $FRESH_CLONE ..."
+$SUDO rm -fr $FRESH_CLONE
+
+echo ""
+info "Done. Built SrdDist archives (.tar.gz, .zip) have been placed in dist/"
