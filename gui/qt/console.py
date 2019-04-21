68246d4e0b86356bb971636ae5b6dbb9bfaa3144
Calin Culianu <calin.culianu@gmail.com>
2019-04-21 18:49:13 +0300

    Fixups to console warning window
    - Adjusted the colors and layout a little bit
    - Went back to using deleteLater to kill the widget as seting parent to
    None caused a segmentation fault on macOS (!)
    - Got rid of the widget checking -R command-like option (relaxed
    warnings)
    - Misc other little tweaks

--- gui/qt/console.py
+++ gui/qt/console.py
@@ -13,36 +13,30 @@ from PyQt5 import QtWidgets
 
 from electroncash import util, get_config
 from electroncash.i18n import _
-from .util import ColorScheme
+from .util import ColorScheme, MONOSPACE_FONT
 
-if platform.system() == 'Windows':
-    MONOSPACE_FONT = 'Lucida Console'
-elif platform.system() == 'Darwin':
-    MONOSPACE_FONT = 'Monaco'
-else:
-    MONOSPACE_FONT = 'monospace'
 
 class ConsoleWarningOverlay(QtWidgets.QWidget):
     STYLESHEET = '''
     QLabel, QLabel link {
         color: rgb(0, 0, 0);
-        background-color: rgb(255, 218, 35, 192);
-        border: 3px solid;
-        border-color: rgb(255, 0, 0);
-        border-radius: 10px;
-        padding: 2px;
+        background-color: rgb(200, 220, 200, 215);
+        border: 2px solid;
+        border-color: rgb(16, 120, 16, 215);
+        border-radius: 16px;
+        padding: 16px;
         font: 16pt;
     }
     '''
 
     STYLESHEET_DARK = '''
     QLabel, QLabel link {
-        color: rgb(255, 255, 255);
-        background-color: rgb(201, 166, 28, 192);
+        color: rgb(200, 220, 200);
+        background-color: rgb(12, 3, 3, 215);
         border: 3px solid;
-        border-color: rgb(158, 0, 0);
-        border-radius: 10px;
-        padding: 2px;
+        border-color: rgb(3, 96, 3, 215);
+        border-radius: 16px;
+        padding: 16px;
         font: 16pt;
     }
     '''
@@ -54,15 +48,17 @@ class ConsoleWarningOverlay(QtWidgets.QWidget):
     def __init__(self, parent):
         super().__init__(parent)
 
-        warning_fmt = '<h1 align="center">{0}</h1><br>{1}<br><br>{2}<br><a href="{3}">{3}</a><br><br><p align="center">{4}</p>'
+        util.finalization_print_error(self)
+
+        warning_fmt = '<h1 align="center">{0}</h1><p align=center><font size=-1>{1} {2}</font></p><p align=center><font size=-1><a href="{3}">{3}</a></font></p><p align="center">{4}</p>'
         warning_text = warning_fmt.format(
-            _('WARNING!'),
+            _('WARNING'),
             _('Do not enter code here that you don\'t understand. Executing the wrong code could '
               'lead to your coins being irreversibly lost.'),
             _("If someone you do not trust wants you to enter something here, that person might "
               "be attempting a social engineering / phishing attack on you."),
             'https://en.wikipedia.org/wiki/Social_engineering_(security)',
-            _('Enter "{}" (without quotes) below to confirm.').format('<b>' + self.CONFIRM_TEXT + '</b>')
+            _("Type: '{}' below to proceed").format('<b>' + self.CONFIRM_TEXT + '</b>')
         )
 
         self.setStyleSheet(self.STYLESHEET_DARK if ColorScheme.dark_scheme else self.STYLESHEET)
@@ -72,10 +68,20 @@ class ConsoleWarningOverlay(QtWidgets.QWidget):
 
         warning_label = QtWidgets.QLabel(warning_text)
         warning_label.setSizePolicy(QtWidgets.QSizePolicy.Expanding,
-                                    QtWidgets.QSizePolicy.Expanding)
+                                    QtWidgets.QSizePolicy.MinimumExpanding)
         warning_label.setWordWrap(True)
         warning_label.setOpenExternalLinks(True)
-        layout.addWidget(warning_label)
+        layoutLbl = QtWidgets.QVBoxLayout()
+        layoutLbl.setContentsMargins(25,25,25,25)
+        layoutLbl.addWidget(warning_label)
+        layout.addLayout(layoutLbl, 1)
+
+        if not ColorScheme.dark_scheme:
+            drop_shadow_effect = QtWidgets.QGraphicsDropShadowEffect()
+            drop_shadow_effect.setBlurRadius(5.0)
+            drop_shadow_effect.setOffset(2.0, 2.0)
+            drop_shadow_effect.setColor(QtGui.QColor(63,63,63,100))
+            warning_label.setGraphicsEffect(drop_shadow_effect)
 
         hbox_layout = QtWidgets.QHBoxLayout()
         layout.addLayout(hbox_layout)
@@ -97,7 +103,7 @@ class ConsoleWarningOverlay(QtWidgets.QWidget):
         """
         Returns true if the value in the text input field matches the confirmation text
         """
-        return self.input_edit.text().upper() == self.CONFIRM_TEXT
+        return self.input_edit.text().strip().upper() == self.CONFIRM_TEXT
 
     @QtCore.pyqtSlot()
     def on_text_changed(self):
@@ -158,7 +164,7 @@ class ConsoleTextEdit(QtWidgets.QPlainTextEdit):
 class Console(QtWidgets.QWidget):
     CONFIG_DONTASKAGAIN_KEY = 'console_warning_dontaskagain'
 
-    def __init__(self, prompt='>> ', startup_message='', parent=None):
+    def __init__(self, wallet, prompt='>> ', startup_message='', parent=None):
         super().__init__(parent)
 
         self.prompt = prompt
@@ -181,31 +187,29 @@ class Console(QtWidgets.QWidget):
 
         self.warningOverlay = None
 
-        wallet_storage = parent.wallet.storage
+        wallet_storage = wallet.storage
         config_dontaskagain = wallet_storage.get(self.CONFIG_DONTASKAGAIN_KEY, False)
-        config_relaxwarn = get_config().cmdline_options["relaxwarn"]
 
-        # Don't show the warning if the user chose to have it not shown again or
-        # when the relaxwarn option is set.
-        if not config_dontaskagain and not config_relaxwarn:
+        # Don't show the warning if the user chose to have it not shown again
+        if not config_dontaskagain:
             self.warningOverlay = ConsoleWarningOverlay(self)
             self.warningOverlay.resize(self.size())
 
             fp = self.editor.focusPolicy()
             blur_effect = QtWidgets.QGraphicsBlurEffect()
-            blur_effect.setBlurRadius(7)
+            blur_effect.setBlurRadius(5)
             self.editor.setGraphicsEffect(blur_effect)
             self.editor.setFocusPolicy(QtCore.Qt.NoFocus)
             self.editor.setFocusProxy(self.warningOverlay)
             def on_acknowledged(dontaskagain: bool):
-                wallet_storage.put(self.CONFIG_DONTASKAGAIN_KEY, dontaskagain)
-                self.warningOverlay.setParent(None)
-                self.warningOverlay = None
+                wallet_storage.put(self.CONFIG_DONTASKAGAIN_KEY, dontaskagain or None)  # None deletes the key
                 self.editor.setGraphicsEffect(None)
                 self.editor.setFocusPolicy(fp)
                 self.editor.setFocusProxy(None)
                 # Focus the editor after confirming
                 self.editor.setFocus()
+                self.warningOverlay.deleteLater()
+                self.warningOverlay = None
             self.warningOverlay.acknowledged.connect(on_acknowledged)
 
     def resizeEvent(self, e):
