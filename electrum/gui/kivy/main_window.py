fd09033890f44f6279385d79309410299cacb9ca
SomberNight <somber.night@protonmail.com>
2019-05-06 21:10:52 +0200

    kivy: fix a race at startup
    on_history (fx) races with load_wallet

--- electrum/gui/kivy/main_window.py
+++ electrum/gui/kivy/main_window.py
@@ -180,7 +180,8 @@ class ElectrumWindow(App):
 
     def on_history(self, d):
         Logger.info("on_history")
-        self.wallet.clear_coin_price_cache()
+        if self.wallet:
+            self.wallet.clear_coin_price_cache()
         self._trigger_update_history()
 
     def on_fee_histogram(self, *args):
