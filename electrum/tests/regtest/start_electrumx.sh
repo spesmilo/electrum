15eecab978242eb0d414fb765e22095044ae37e4
ThomasV <thomasv@electrum.org>
2019-05-21 12:06:43 +0200

    update electrumx starting script

--- electrum/tests/regtest/start_electrumx.sh
+++ electrum/tests/regtest/start_electrumx.sh
@@ -2,6 +2,6 @@
 export HOME=~
 set -eux pipefail
 cd
-rm -rf electrumx_db
-mkdir electrumx_db
-COIN=BitcoinSegwit TCP_PORT=51001 RPC_PORT=8000 NET=regtest DAEMON_URL=http://doggman:donkey@127.0.0.1:18554 DB_DIRECTORY=~/electrumx_db electrumx_server > electrumx.log &
+rm -rf $HOME/electrumx_db
+mkdir $HOME/electrumx_db
+COIN=BitcoinSegwit SERVICES=tcp://:51001,rpc:// NET=regtest DAEMON_URL=http://doggman:donkey@127.0.0.1:18554 DB_DIRECTORY=$HOME/electrumx_db electrumx_server > $HOME/electrumx.log &
