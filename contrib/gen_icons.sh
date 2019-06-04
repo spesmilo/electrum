87e33c8a4d6e894eca11c47e5cd0500dceefa432
Axel Gembe <derago@gmail.com>
2019-06-04 21:56:37 +0700

    Icons: Use recreated clock icons by the Simple Ledger devs
    The clock icons were the last icons still stuck with low resolution.
    We wanted to keep the original look instead of going with a flat
    look like Electrum but the original vector files seem to have been
    lost. We decided to recreate the icon so that it closely matches the
    original when rendered at a low resoltion while giving more detail
    when rendered at a higher resolution.
    
    The Qt SVG engine seems to have an issue with these SVGs, rendering
    them somewhat too dark. Until we can figure out a fix for this
    problem we rasterize the icons.

---
+++ contrib/gen_icons.sh
@@ -0,0 +1,36 @@
+#!/bin/sh
+
+set -e
+
+RASTERIZE="clock1 clock2 clock3 clock4 clock5"
+RESOLUTIONS="16 24 32 48 64"
+
+require_command() {
+    cmd="$1"
+    msg="$2"
+    if [ -z "$msg" ]; then
+        msg="The '${cmd}' utility was not found. It is required by this script. Please install '${cmd}' using your package manager to proceed."
+    fi
+    if ! which $cmd > /dev/null 2>&1 ; then
+        echo "$msg"
+        exit 1
+    fi
+}
+
+require_command convert # Make sure user has imagemagick installed
+require_command pyrcc5
+
+for icon in $RASTERIZE ; do
+    echo "Generating $icon.ico"
+
+    for res in $RESOLUTIONS ; do
+    	convert -background none -resize ${res}x$res icons/$icon.svg icons/${icon}_$res.png
+    done
+
+    ICON_PNGS=$(for res in $RESOLUTIONS ; do echo icons/${icon}_$res.png ; done)
+    convert $ICON_PNGS icons/${icon}.ico
+    rm $ICON_PNGS
+done
+
+echo "Generating icons.py"
+pyrcc5 icons.qrc -o gui/qt/icons.py
