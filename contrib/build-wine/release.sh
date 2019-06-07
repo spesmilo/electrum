e9adc2f09fcdd43078405f0532164ceb79b03e87
Martin <martin@commerceblock.com>
2019-06-07 13:16:52 +0100

    Added github-release dependencies

--- contrib/build-wine/release.sh
+++ contrib/build-wine/release.sh
@@ -2,4 +2,7 @@
 
 git clone https://github.com/buildkite/github-release.git && \
 cd github-release && \
+go get github.com/google/go-github/github && \
+go get github.com/oleiade/reflections && \
+go get golang.org/x/oauth2  && \
 go run main.go \"latest\" ../dist/*.exe
