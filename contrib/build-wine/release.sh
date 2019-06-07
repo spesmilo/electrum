fd99213d0cd86d7ed67323bda0841ce15807e98f
Martin <martin@commerceblock.com>
2019-06-07 16:41:59 +0100

    Updated wine build and release scripts

--- contrib/build-wine/release.sh
+++ contrib/build-wine/release.sh
@@ -1,8 +1,4 @@
 #!/bin/bash
 
 git clone https://github.com/buildkite/github-release.git && \
-cd github-release && \
-go get github.com/google/go-github/github && \
-go get github.com/oleiade/reflections && \
-go get golang.org/x/oauth2  && \
-go run main.go \"latest\" ../dist/*.exe
+go run github-release/main.go \"latest\" dist/*.exe
