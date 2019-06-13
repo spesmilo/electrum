d07caaf601c7f6ac6bc2534d2dadbb90a23ea00f
SomberNight <somber.night@protonmail.com>
2019-06-13 17:03:12 +0200

    qt msgbox: when using rich text, set text format to "AutoText" instead
    "\n" newlines were ignored for WIF_HELP_TEXT InfoButtons

--- electrum/gui/qt/util.py
+++ electrum/gui/qt/util.py
@@ -258,7 +258,11 @@ def custom_message_box(*, icon, parent, title, text, buttons=QMessageBox.Ok,
     d.setDefaultButton(defaultButton)
     if rich_text:
         d.setTextInteractionFlags(Qt.TextSelectableByMouse | Qt.LinksAccessibleByMouse)
-        d.setTextFormat(Qt.RichText)
+        # set AutoText instead of RichText
+        # AutoText lets Qt figure out whether to render as rich text.
+        # e.g. if text is actually plain text and uses "\n" newlines;
+        #      and we set RichText here, newlines would be swallowed
+        d.setTextFormat(Qt.AutoText)
     else:
         d.setTextInteractionFlags(Qt.TextSelectableByMouse)
         d.setTextFormat(Qt.PlainText)
