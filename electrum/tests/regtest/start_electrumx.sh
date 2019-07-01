94fe28b57684d85a3f72f39ab6d16ef7b9a03b99
ThomasV <thomasv@electrum.org>
2019-07-01 23:10:20 +0200

    regtest: remove cost limit

--- electrum/tests/regtest/start_electrumx.sh
+++ electrum/tests/regtest/start_electrumx.sh
@@ -4,4 +4,4 @@ set -eux pipefail
 cd
 rm -rf $HOME/electrumx_db
 mkdir $HOME/electrumx_db
-COIN=BitcoinSegwit SERVICES=tcp://:51001,rpc:// NET=regtest DAEMON_URL=http://doggman:donkey@127.0.0.1:18554 DB_DIRECTORY=$HOME/electrumx_db electrumx_server > $HOME/electrumx.log &
+COST_SOFT_LIMIT=0 COST_HARD_LIMIT=0 COIN=BitcoinSegwit SERVICES=tcp://:51001,rpc:// NET=regtest DAEMON_URL=http://doggman:donkey@127.0.0.1:18554 DB_DIRECTORY=$HOME/electrumx_db electrumx_server > $HOME/electrumx.log &
