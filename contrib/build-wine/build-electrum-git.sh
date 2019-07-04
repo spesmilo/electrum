dcecf7db4b735d668b23af49f5c31c3aea58963d
Axel Gembe <derago@gmail.com>
2019-07-04 22:32:51 +0200

    Wine Build: Make it less noisy
    This suppresses the pip script location warnings, like we already
    do for AppImage. It also disables the Wine debugging messages by
    setting WINEDEBUG=-all.
    
    -----
    
    taken from Electron-Cash/Electron-Cash@d3685b038ef0dc3dc6a18345e51ff231c97623f5

--- contrib/build-wine/build-electrum-git.sh
+++ contrib/build-wine/build-electrum-git.sh
@@ -4,6 +4,7 @@ NAME_ROOT=electrum
 
 # These settings probably don't need any change
 export WINEPREFIX=/opt/wine64
+export WINEDEBUG=-all
 export PYTHONDONTWRITEBYTECODE=1
 export PYTHONHASHSEED=22
 
@@ -42,14 +43,14 @@ popd
 
 
 # Install frozen dependencies
-$PYTHON -m pip install -r "$CONTRIB"/deterministic-build/requirements.txt
+$PYTHON -m pip install --no-warn-script-location -r "$CONTRIB"/deterministic-build/requirements.txt
 
-$PYTHON -m pip install -r "$CONTRIB"/deterministic-build/requirements-hw.txt
+$PYTHON -m pip install --no-warn-script-location -r "$CONTRIB"/deterministic-build/requirements-hw.txt
 
 pushd $WINEPREFIX/drive_c/electrum
 # see https://github.com/pypa/pip/issues/2195 -- pip makes a copy of the entire directory
 info "Pip installing Electrum. This might take a long time if the project folder is large."
-$PYTHON -m pip install .
+$PYTHON -m pip install --no-warn-script-location .
 popd
 
 
