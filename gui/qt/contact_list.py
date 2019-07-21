e07530421343d7f92888cbb105e10775aea4ef41
Calin Culianu <calin.culianu@gmail.com>
2019-07-21 14:43:11 +0300

    CashAccounts: Fixed a wrapped C++ object deleted error in contact list
    My bad -- the context menu was holding a reference to an ephemeral list item.
    Fixed.

--- gui/qt/contact_list.py
+++ gui/qt/contact_list.py
@@ -221,7 +221,8 @@ class ContactList(PrintError, MyTreeWidget):
             if ca_info:
                 menu.addAction(_("View registration tx..."), lambda: self.parent.do_process_from_txid(txid=ca_info.txid, tx_desc=self.wallet.get_label(ca_info.txid)))
                 if typ in ('cashacct_W', 'cashacct'):
-                    menu.addAction(_("Details..."), lambda: cashacctqt.cash_account_detail_dialog(self.parent, i2c(item).name))
+                    _contact_d = i2c(item)
+                    menu.addAction(_("Details..."), lambda: cashacctqt.cash_account_detail_dialog(self.parent, _contact_d.name))
             menu.addSeparator()
 
         menu.addAction(self.icon_cashacct,
