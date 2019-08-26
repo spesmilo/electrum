de99f7b744400f8523405c4a4ee6f943263e146c
Calin Culianu <calin.culianu@gmail.com>
2019-08-26 05:00:28 +0300

    iOS: Nits to messaging in make_ios_project.sh
    Reworded and reformatted the end "operation complete" message slightly.

--- ios/make_ios_project.sh
+++ ios/make_ios_project.sh
@@ -251,7 +251,7 @@ fi
 # Clean up no-longer-needed electroncash/ dir that is outside of Xcode project
 rm -fr ${compact_name}/electroncash/*
 
-# Can add this back when it works uniformly without issues 
+# Can add this back when it works uniformly without issues
 # /usr/bin/env ruby update_project.rb
 
 echo ''
@@ -260,14 +260,12 @@ echo '*                                                                        *
 echo '*   Operation Complete. An Xcode project has been generated in "iOS/"    *'
 echo '*                                                                        *'
 echo '**************************************************************************'
-
-echo '  IMPORTANT!'
-echo '        Now you either need to manually add the library libxml2.tbd to the '
-echo '        project under "General -> Linked Frameworks and Libraries" or successfully '
-echo '        run the update_project.rb script to do it for you.'
-echo '        Either of those are needed to prevent build errors! '
 echo ''
-
+echo '  IMPORTANT!'
+echo '        Now you need to either manually add the library libxml2.tbd to the '
+echo '        project under "General -> Linked Frameworks and Libraries" *or* '
+echo '        run the ./update_project.rb script which will do it for you.'
+echo '        Either of the above are needed to prevent build errors! '
 echo ''
 echo '  Also note:'
 echo '        Modifications to files in iOS/ will be clobbered the next    '
