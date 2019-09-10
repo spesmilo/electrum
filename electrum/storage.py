8e4fe051d32481a5d0cb130f9b8f5452ec3898a4
ThomasV <thomasv@electrum.org>
2019-09-10 09:25:42 +0200

    add comment in storage._write

--- electrum/storage.py
+++ electrum/storage.py
@@ -106,6 +106,7 @@ class WalletStorage(Logger):
             os.fsync(f.fileno())
 
         mode = os.stat(self.path).st_mode if self.file_exists() else stat.S_IREAD | stat.S_IWRITE
+        # assert that wallet file does not exist, to prevent wallet corruption (see issue #5082)
         if not self.file_exists():
             assert not os.path.exists(self.path)
         os.replace(temp_path, self.path)
