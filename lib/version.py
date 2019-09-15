ad0f6040bcc76234cb370b810282a87e33991659
Calin Culianu <calin.culianu@gmail.com>
2019-09-15 14:25:33 +0300

    Bumped version to 4.0.11 for development

--- lib/version.py
+++ lib/version.py
@@ -1,4 +1,4 @@
-PACKAGE_VERSION  = '4.0.10'  # version of the client package
+PACKAGE_VERSION  = '4.0.11'  # version of the client package
 PROTOCOL_VERSION = '1.4'     # protocol version requested
 
 # The hash of the mnemonic seed must begin with this
