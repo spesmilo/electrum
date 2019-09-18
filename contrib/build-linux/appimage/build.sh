f4d29a88b2c2e1b48066bf4937e7ac27df19301d
nkostoulas <nkostoulas@gmail.com>
2019-09-18 13:34:47 +0100

    some fixes for the linux build

--- contrib/build-linux/appimage/build.sh
+++ contrib/build-linux/appimage/build.sh
@@ -146,7 +146,7 @@ cp "/usr/lib/x86_64-linux-gnu/libzbar.so.0" "$APPDIR/usr/lib/libzbar.so.0"
 
 info "desktop integration."
 cp "$PROJECT_ROOT/electrum.desktop" "$APPDIR/electrum.desktop"
-cp "$PROJECT_ROOT/electrum/gui/icons/electrum.png" "$APPDIR/electrum.png"
+cp "$PROJECT_ROOT/icons/electrum.png" "$APPDIR/electrum.png"
 
 
 # add launcher
