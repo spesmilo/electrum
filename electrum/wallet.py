6bc123ac5a62112aa2b68e8cc44f9ff90e4712cc
Tom Trevethan <tptrevethan@googlemail.com>
2019-10-04 11:16:42 +0100

    fixed contract bug

--- electrum/wallet.py
+++ electrum/wallet.py
@@ -634,7 +634,7 @@ class Abstract_Wallet(AddressSynchronizer):
 
         if constants.net.CONTRACTINTX:
             asset = tx.outputs()[0].asset
-            contr = self.contracts[-1]
+            contr = self.contracts[0]
             op_return_script = '6a20' + "".join(reversed([contr[i:i+2] for i in range(0, len(contr), 2)]))
             tx.add_outputs([TxOutput(TYPE_SCRIPT,op_return_script,0,1,asset,1)])
 
