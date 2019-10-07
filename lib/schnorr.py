9f0d412c890460fe999915f1322e69135fd381e7
Mark Lundeberg <36528214+markblundeberg@users.noreply.github.com>
2019-10-07 11:25:54 -0700

    schnorr.py: add additional note about blind signature forgery.
    The math behind this is a bit complex but it definitely applies in our
    case.

--- lib/schnorr.py
+++ lib/schnorr.py
@@ -260,8 +260,8 @@ class BlindSigner:
 
     Security note: If we were to sign two distinct requests for the same R,
     then our private key could be recovered. Thus, you can only call .sign()
-    once. If you need a new blind signature then you must create a new
-    object.
+    once (and this class enforces this restriction in a thread-safe manner).
+    If you need a new blind signature then you must create a new instance.
 
     Security note 2: If an adversary knows that this private key is related
     to another key (say, they are related by multiplication or addition of a
@@ -269,6 +269,21 @@ class BlindSigner:
     signature *from the other key*! For example, all keys in a BIP32 "xpub"
     are related, and so you should seriously avoid using this function with
     standard BIP32 or any other public key derivation method.
+
+    Security note 3: If a blind signer allows multiple blind signature
+    requests to be serviced in parallel (i.e., have multiple `.get_R`'s issued
+    at the same time, having not yet received the parameters for `.sign`),
+    then an adversary can perform work and submit carefully designed requests
+    that allow an additional signature to be created. E.g., with 511 parallel
+    requests, 512 signatures could be produced with ~2^35 work of precomputation
+    on the part of the adversary.
+    See:
+    - Schnorr 2001 "Security of Blind Discrete Log Signatures against Interactive Attacks"
+      https://www.math.uni-frankfurt.de/~dmst/research/papers/schnorr.blind_sigs_attack.2001.pdf
+    - Wagner 2002 "A Generalized Birthday Problem"
+      https://www.iacr.org/archive/crypto2002/24420288/24420288.pdf
+    - A possible solution that should make it so at least 2^70 work is needed
+      to get an additional signature: https://eprint.iacr.org/2019/877
     """
     order = ecdsa.SECP256k1.generator.order()
 
