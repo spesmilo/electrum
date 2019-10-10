spesmilo/electrum

commit 919e1b9bde4b63652832fdd1a18df438c3e33cb8

Optimize is_used and is_empty.

lib/wallet.py
@@ -1042,14 +1042,11 @@ class Abstract_Wallet(PrintError):
         return not self.is_watching_only() and hasattr(self.keystore, 'get_private_key')
 
     def is_used(self, address):
-        assert isinstance(address, Address)
-        c, u, x = self.get_addr_balance(address)
-        return c + u + x == 0 and self.get_address_history(address)
+        return self.get_address_history(address) or not self.is_empty(address)
 
     def is_empty(self, address):
         assert isinstance(address, Address)
-        c, u, x = self.get_addr_balance(address)
-        return c+u+x == 0
+        return any(self.get_addr_balance(address))
 
     def address_is_old(self, address, age_limit=2):
         age = -1

commit 59a78610beeb5a4fae93ebe8232cc8a91d5fc013

bugfix: inverted definition of wallet.is_empty
Bug was introduced in 919e1b9bde which was probably an insignificant
optimization. Root of all evil, etc etc. :-)

Note that there would have been a noticeable bug in commands.py's
`listaddresses` if funded=True, but likely nobody ever used this.

lib/wallet.py
@@ -1733,11 +1733,11 @@ class Abstract_Wallet(PrintError, SPVDelegate):
         return not self.is_watching_only() and hasattr(self.keystore, 'get_private_key')
 
     def is_used(self, address):
-        return self.get_address_history(address) and not self.is_empty(address)
+        return self.get_address_history(address) and self.is_empty(address)
 
     def is_empty(self, address):
         assert isinstance(address, Address)
-        return any(self.get_addr_balance(address))
+        return not any(self.get_addr_balance(address))
 
     def address_is_old(self, address, age_limit=2):
         age = -1