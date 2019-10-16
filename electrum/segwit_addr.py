bbb0e132e69ca593a4b2c400247bd75f8f0dc732
Janus <ysangkok@gmail.com>
2019-10-16 22:32:10 +0000

    segwit_addr: Use normal comparison for tuple literal

--- electrum/segwit_addr.py
+++ electrum/segwit_addr.py
@@ -1,3 +1,4 @@
+
 # Copyright (c) 2017 Pieter Wuille
 #
 # Permission is hereby granted, free of charge, to any person obtaining a copy
@@ -118,5 +119,5 @@ def decode(hrp, addr):
 def encode(hrp, witver, witprog):
     """Encode a segwit address."""
     ret = bech32_encode(hrp, [witver] + convertbits(witprog, 8, 5))
-    assert decode(hrp, ret) is not (None, None)
+    assert decode(hrp, ret) != (None, None)
     return ret
