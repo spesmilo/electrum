51b3c64dd617e3e225c423dd1c56fed055963f3f
marpme <marpme@users.noreply.github.com>
2019-11-16 16:57:35 +0100

    Remove some bitcoin exclusive details

--- electrum/constants.py
+++ electrum/constants.py
@@ -72,18 +72,18 @@ class BitcoinMainnet(AbstractNet):
 
     XPRV_HEADERS = {
         'standard':    0x0221312b,  # xprv
-        'p2wpkh-p2sh': 0x049d7878,  # yprv
-        'p2wsh-p2sh':  0x0295b005,  # Yprv
-        'p2wpkh':      0x04b2430c,  # zprv
-        'p2wsh':       0x02aa7a99,  # Zprv
+        # 'p2wpkh-p2sh': 0x049d7878,  # yprv
+        # 'p2wsh-p2sh':  0x0295b005,  # Yprv
+        # 'p2wpkh':      0x04b2430c,  # zprv
+        # 'p2wsh':       0x02aa7a99,  # Zprv
     }
     XPRV_HEADERS_INV = inv_dict(XPRV_HEADERS)
     XPUB_HEADERS = {
         'standard':    0x022d2533,  # xpub
-        'p2wpkh-p2sh': 0x049d7cb2,  # ypub
-        'p2wsh-p2sh':  0x0295b43f,  # Ypub
-        'p2wpkh':      0x04b24746,  # zpub
-        'p2wsh':       0x02aa7ed3,  # Zpub
+        # 'p2wpkh-p2sh': 0x049d7cb2,  # ypub
+        # 'p2wsh-p2sh':  0x0295b43f,  # Ypub
+        # 'p2wpkh':      0x04b24746,  # zpub
+        # 'p2wsh':       0x02aa7ed3,  # Zpub
     }
     XPUB_HEADERS_INV = inv_dict(XPUB_HEADERS)
     BIP44_COIN_TYPE = 77
@@ -108,26 +108,26 @@ class BitcoinTestnet(AbstractNet):
 
     XPRV_HEADERS = {
         'standard':    0x04358394,  # tprv
-        'p2wpkh-p2sh': 0x044a4e28,  # uprv
-        'p2wsh-p2sh':  0x024285b5,  # Uprv
-        'p2wpkh':      0x045f18bc,  # vprv
-        'p2wsh':       0x02575048,  # Vprv
+        # 'p2wpkh-p2sh': 0x044a4e28,  # uprv
+        # 'p2wsh-p2sh':  0x024285b5,  # Uprv
+        # 'p2wpkh':      0x045f18bc,  # vprv
+        # 'p2wsh':       0x02575048,  # Vprv
     }
     XPRV_HEADERS_INV = inv_dict(XPRV_HEADERS)
     XPUB_HEADERS = {
         'standard':    0x043587cf,  # tpub
-        'p2wpkh-p2sh': 0x044a5262,  # upub
-        'p2wsh-p2sh':  0x024289ef,  # Upub
-        'p2wpkh':      0x045f1cf6,  # vpub
-        'p2wsh':       0x02575483,  # Vpub
+        # 'p2wpkh-p2sh': 0x044a5262,  # upub
+        # 'p2wsh-p2sh':  0x024289ef,  # Upub
+        # 'p2wpkh':      0x045f1cf6,  # vpub
+        # 'p2wsh':       0x02575483,  # Vpub
     }
     XPUB_HEADERS_INV = inv_dict(XPUB_HEADERS)
-    BIP44_COIN_TYPE = 1
-    LN_REALM_BYTE = 1
-    LN_DNS_SEEDS = [
-        'test.nodes.lightning.directory.',
-        'lseed.bitcoinstats.com.',
-    ]
+    BIP44_COIN_TYPE = 77
+    # LN_REALM_BYTE = 1
+    # LN_DNS_SEEDS = [
+    #     'test.nodes.lightning.directory.',
+    #     'lseed.bitcoinstats.com.',
+    # ]
 
 
 class BitcoinRegtest(BitcoinTestnet):
