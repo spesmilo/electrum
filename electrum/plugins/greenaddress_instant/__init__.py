8c30ae78be8cbd0a4ef9ceab3d9c3951a6266762
Riccardo Casatta <riccardo@casatta.it>
2019-11-27 17:37:31 +0100

    remove GreenAddress instant plugin
    GreenAddress instant feature has been removed from the service, thus
    there is no reason anymore to keep the plugin

--- electrum/plugins/greenaddress_instant/__init__.py
+++
@@ -1,5 +0,0 @@
-from electrum.i18n import _
-
-fullname = 'GreenAddress instant'
-description = _("Allows validating if your transactions have instant confirmations by GreenAddress")
-available_for = ['qt']
