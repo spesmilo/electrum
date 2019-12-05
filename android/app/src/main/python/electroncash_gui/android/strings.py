bb62a0753297e327c4759a4f2fabd5844024a5f3
andrew#128 <andrew-128-github@protonmail.com>
2019-12-05 16:28:11 +0000

    Fix order of the strings and add a comment to maintain this.

--- android/app/src/main/python/electroncash_gui/android/strings.py
+++ android/app/src/main/python/electroncash_gui/android/strings.py
@@ -3,43 +3,43 @@
 # see https://medium.com/@jsaito/making-a-case-for-letter-case-19d09f653c98
 
 # If you change anything here, you need to rebuild the Android strings files by running the
-# Gradle task `generateStrings`.
+# Gradle task `generateStrings`. Please keep the strings in alphabetical order.
 
 def _(s):
     return s
 
 _("Block explorer")
-_("Copyright © 2017-2019 Electron Cash LLC and the Electron Cash developers.")
 _("Cannot specify private keys and addresses in the same wallet.")
 _("Change password")
 _("Close wallet")
 _("Confirm password")
-_("Current password")
 _("Console")
+_("Copyright © 2017-2019 Electron Cash LLC and the Electron Cash developers.")
+_("Current password")
 _("Delete wallet")
 _("Derivation invalid")
 _("Disconnect")
-_("ID")
 _("Enter password")
 _("For support, please visit us on <a href='https://github.com/Electron-Cash/Electron-Cash/issues'>"
   "GitHub</a> or on <a href='https://t.me/electroncashwallet'>Telegram</a>.")
+_("ID")
 _("Import addresses or private keys")
 _("Invalid address")
 _("Made with <a href='https://chaquo.com/chaquopy'>Chaquopy</a>, the Python SDK for Android.")
 _("New password")
 _("New wallet")
-_("No wallet")
 _("No wallet is open.")
+_("No wallet")
 _("Not a valid address or private key: '%s'")
 _("Passphrase")
 _("Press the menu button above to open or create one.")
+_("Rename wallet")
 _("Request")
 _("Restore from seed")
 _("Show seed")
 _("Size")
 _("Transaction not found")
 _("Use a master key")
+_("Wallet names cannot contain the '/' character. Please enter a different wallet name to proceed.")
 _("Wallet seed")
 _("You don't have any contacts.")
-_("Rename wallet")
-_("Wallet names cannot contain the '/' character. Please enter a different wallet name to proceed.")
