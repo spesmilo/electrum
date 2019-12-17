bc407a546fbacb5a34bd3af528fb8361a09e56cb
Lawrence Deacon <lawrence.deacon@gmail.com>
2019-12-17 13:07:42 +0000

    wallet title

--- electrum/constants.py
+++ electrum/constants.py
@@ -44,7 +44,7 @@ class OceanMainnet:
     FIXEDFEE = 50000
     SHOWFX = False
     WALLETPATH = "dgldwallet"
-    WALLETTITLE = "DGLD"
+    WALLETTITLE = "DGLD Wallet"
     CONTRACTINTX = True
     BASIC_HEADER_SIZE = 172
     MIN_HEADER_SIZE = 176
