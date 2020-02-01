5673449a1f55fd7488cd98f03e0364ec588afe88
Calin Culianu <calin.culianu@gmail.com>
2020-02-01 21:59:14 +0200

    Fix #1774: Tolerate spurious 'error' messages from server.
    Fulcrum sends a spurious 'error' message to the client right before
    terminating the connection due to idle timeout.  Normally this can't
    happen if the client is behaving, however in some circumstances, such as
    on mobile where it may go dormant, or on desktop systems that are
    thrashing due to low memory and never get to send 'server.ping', this spurious
    message was leading to an exception being thrown in Electron Cash.
    
    This corner case happens exceptionally rarely, but it will be addressed
    in Fulcrum soon: See https://github.com/cculianu/Fulcrum/issues/20
    
    In the meantime, we made the network 'interface' class more
    resilient/tolerant of the situation.
    
    Also, we added additional guards against malformed server messages in
    interface.py.
    
    This closes #1774, and fixes the related #1794.

--- lib/interface.py
+++ lib/interface.py
@@ -381,6 +381,26 @@ class Interface(util.PrintError):
                 self.print_error("<--", response)
             wire_id = response.get('id', None)
             if wire_id is None:  # Notification
+                if not isinstance(response.get('method'), str):  # defend against funny/out-of-spec JSON
+                    if response.get('error'):
+                        # Fulcrum servers versions 1.0.1 and earlier sometimes
+                        # would send spurious 'error' messages with id=null and
+                        # no 'method'. This would only happen on idle timeout
+                        # of the client.  We will tolerate this and simply
+                        # discard the message in that case.
+                        #
+                        # Electron Cash:
+                        #   https://github.com/Electron-Cash/Electron-Cash/issues/1774
+                        # Fulcrum:
+                        #   https://github.com/cculianu/Fulcrum/issues/20f
+                        self.print_error("Ignoring spurious error message from server:", response.get('error'))
+                        continue
+                    else:
+                        # Malforned notification -- signal bad server
+                        self.print_error("Server sent us a notification message without a 'method':", response)
+                        responses.append((None, None))  # Signal
+                        break
+                # At this point the notification has a 'method' defined, so we know it's good.
                 responses.append((None, response))
             else:
                 request = self.unanswered_requests.pop(wire_id, None)
@@ -388,7 +408,7 @@ class Interface(util.PrintError):
                     responses.append((request, response))
                 else:
                     self.print_error("unknown wire ID", wire_id)
-                    responses.append((None, None)) # Signal
+                    responses.append((None, None))  # Signal
                     break
 
         return responses
