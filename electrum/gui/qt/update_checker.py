5d4f8f316480776eb78d98ea9ac9c3b2a05ef01d
SomberNight <somber.night@protonmail.com>
2020-03-09 22:01:51 +0100

    qt update checker: subclass QDialog instead of QWidget
    this way "minimise to taskbar" and "fullscreen" buttons are not shown

--- electrum/gui/qt/update_checker.py
+++ electrum/gui/qt/update_checker.py
@@ -8,7 +8,7 @@ from distutils.version import StrictVersion
 
 from PyQt5.QtCore import Qt, QThread, pyqtSignal
 from PyQt5.QtWidgets import (QWidget, QVBoxLayout, QLabel, QProgressBar,
-                             QHBoxLayout, QPushButton)
+                             QHBoxLayout, QPushButton, QDialog)
 
 from electrum import version
 from electrum import constants
@@ -18,7 +18,7 @@ from electrum.util import make_aiohttp_session
 from electrum.logging import Logger
 
 
-class UpdateCheck(QWidget, Logger):
+class UpdateCheck(QDialog, Logger):
     url = "https://electrum.org/version"
     download_url = "https://electrum.org/#download"
 
@@ -28,7 +28,7 @@ class UpdateCheck(QWidget, Logger):
 
     def __init__(self, main_window, latest_version=None):
         self.main_window = main_window
-        QWidget.__init__(self)
+        QDialog.__init__(self)
         self.setWindowTitle('Electrum - ' + _('Update Check'))
         self.content = QVBoxLayout()
         self.content.setContentsMargins(*[10]*4)
