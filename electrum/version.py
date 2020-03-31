e2ed89a5899b91b2f49107fa915d9d85a4a84571
sidhujag <jsidhu@blockchainfoundry.co>
2020-03-31 12:23:22 -0700

    update verison

--- electrum/version.py
+++ electrum/version.py
@@ -1,4 +1,4 @@
-ELECTRUM_VERSION = '4.0.0a0' # version of the client package
+ELECTRUM_VERSION = '4.0.0' # version of the client package
 APK_VERSION = '4.0.0.0'      # read by buildozer.spec
 
 PROTOCOL_VERSION = '1.4.1'     # protocol version requested
