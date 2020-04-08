15b8d741e531dd7d8d8695debab6fd52bcddd192
SomberNight <somber.night@protonmail.com>
2020-04-08 18:54:11 +0200

    wizard hww: move devmgr.scan_devices() away from GUI thread

--- electrumsys/base_wizard.py
+++ electrumsys/base_wizard.py
@@ -280,7 +280,8 @@ class BaseWizard(Logger):
 
         # scan devices
         try:
-            scanned_devices = devmgr.scan_devices()
+            scanned_devices = self.run_task_without_blocking_gui(task=devmgr.scan_devices,
+                                                                 msg=_("Scanning devices..."))
         except BaseException as e:
             self.logger.info('error scanning devices: {}'.format(repr(e)))
             debug_msg = '  {}:\n    {}'.format(_('Error scanning devices'), e)
