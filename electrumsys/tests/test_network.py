0511b6961b55f0d31d482446f2a9cb12990d61c3
SomberNight <somber.night@protonmail.com>
2020-04-15 17:39:39 +0200

    fix tests: follow-up prev few commits

--- electrumsys/tests/test_network.py
+++ electrumsys/tests/test_network.py
@@ -5,7 +5,7 @@ import unittest
 from electrumsys import constants
 from electrumsys.simple_config import SimpleConfig
 from electrumsys import blockchain
-from electrumsys.interface import Interface
+from electrumsys.interface import Interface, ServerAddr
 from electrumsys.crypto import sha256
 from electrumsys.util import bh2u
 
@@ -24,7 +24,7 @@ class MockInterface(Interface):
         self.config = config
         network = MockNetwork()
         network.config = config
-        super().__init__(network, 'mock-server:50000:t', None)
+        super().__init__(network=network, server=ServerAddr.from_str('mock-server:50000:t'), proxy=None)
         self.q = asyncio.Queue()
         self.blockchain = blockchain.Blockchain(config=self.config, forkpoint=0,
                                                 parent=None, forkpoint_hash=constants.net.GENESIS, prev_hash=None)
