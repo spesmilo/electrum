b59c3294b29c8899480a69b5c79bc37dd034b11e
SomberNight <somber.night@protonmail.com>
2020-04-26 05:29:32 +0200

    fix #6115: qt wallet>information was broken for imported wallets

--- electrum/gui/qt/main_window.py
+++ electrum/gui/qt/main_window.py
@@ -2277,6 +2277,8 @@ class ElectrumWindow(QMainWindow, MessageBoxMixin, Logger):
             grid.addWidget(lightning_b, 5, 2)
         vbox.addLayout(grid)
 
+        labels_clayout = None
+
         if self.wallet.is_deterministic():
             mpk_text = ShowQRTextEdit()
             mpk_text.setMaximumHeight(150)
@@ -2286,8 +2288,6 @@ class ElectrumWindow(QMainWindow, MessageBoxMixin, Logger):
                 mpk_text.setText(mpk_list[index])
                 mpk_text.repaint()  # macOS hack for #4777
 
-            # declare this value such that the hooks can later figure out what to do
-            labels_clayout = None
             # only show the combobox in case multiple accounts are available
             if len(mpk_list) > 1:
                 # only show the combobox if multiple master keys are defined
