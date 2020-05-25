6372b1d13c991a39cef91bca43e5187ebc1436e0
Konrad Jawulski <konrad.jawulski@minebest.com>
2020-05-25 14:12:54 +0200

    fixed doubling --regtest command

--- electrum/commands.py
+++ electrum/commands.py
@@ -1159,10 +1159,9 @@ def add_global_options(parser):
     group.add_argument("-D", "--dir", dest="electrum_path", help="electrum directory")
     group.add_argument("-P", "--portable", action="store_true", dest="portable", default=False, help="Use local 'electrum_data' directory")
     group.add_argument("--testnet", action="store_true", dest="testnet", default=False, help="Use Testnet")
-    group.add_argument("--regtest", action="store_true", dest="regtest", default=False, help="Use Regtest")
+    group.add_argument("--regtest", action="store_true", dest="regtest", default=False, help="Use BTCV Regtest")
     group.add_argument("--simnet", action="store_true", dest="simnet", default=False, help="Use Simnet")
     group.add_argument("-o", "--offline", action="store_true", dest="offline", default=False, help="Run offline")
-    group.add_argument("--regtest", action="store_true", dest="btcv_regtest", default=False, help="Use BTCV Regtest")
 
 def add_wallet_option(parser):
     parser.add_argument("-w", "--wallet", dest="wallet_path", help="wallet path")
