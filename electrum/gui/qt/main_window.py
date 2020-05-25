a80c5345bd00838ea1f1e1221715718329292367
alex v <alex@encrypt-s.com>
2020-05-25 10:50:07 +0200

    update button label

--- electrum/gui/qt/main_window.py
+++ electrum/gui/qt/main_window.py
@@ -987,7 +987,7 @@ class ElectrumWindow(QMainWindow, MessageBoxMixin, Logger):
 
         self.clear_invoice_button = QPushButton(_('Clear'))
         self.clear_invoice_button.clicked.connect(self.clear_receive_tab)
-        self.create_invoice_button = QPushButton(_('On-chain'))
+        self.create_invoice_button = QPushButton(_('Create Address'))
         self.create_invoice_button.setIcon(read_QIcon("bitcoin.png"))
         self.create_invoice_button.clicked.connect(lambda: self.create_invoice(False))
         self.receive_buttons = buttons = QHBoxLayout()
