f43108937bfb0d70f5786a6251e866da9017430b
alex v <alex@encrypt-s.com>
2020-06-07 15:34:46 +0200

    bump version

--- electrum/version.py
+++ electrum/version.py
@@ -1,5 +1,5 @@
-ELECTRUM_VERSION = '4.0.1' # version of the client package
-APK_VERSION = '4.0.1'      # read by buildozer.spec
+ELECTRUM_VERSION = '5.0.0' # version of the client package
+APK_VERSION = '5.0.0'      # read by buildozer.spec
 
 PROTOCOL_VERSION = '1.4'     # protocol version requested
 
