24a007840f0172ccc978fbb9a6b921f0ea616e00
SomberNight <somber.night@protonmail.com>
2020-06-11 20:19:28 +0200

    sdist build: use modern pip
    the one in apt refused to install certain package versions (that were pinned by hash!!)
    and installed different versions instead... e.g.:
    
    Collecting wheel==0.34.2 (from -r /opt/electrum/contrib/build-linux/sdist/../../../contrib/deterministic-build/requirements.txt (line 112))
      Downloading https://files.pythonhosted.org/packages/75/28/521c6dc7fef23a68368efefdcd682f5b3d1d58c2b90b06dc1d0b805b51ae/wheel-0.34.2.tar.gz (58kB)
        100% |████████████████████████████████| 61kB 3.8MB/s
      Requested wheel==0.34.2 from https://files.pythonhosted.org/packages/75/28/521c6dc7fef23a68368efefdcd682f5b3d1d58c2b90b06dc1d0b805b51ae/wheel-0.34.2.tar.gz#sha256=8788e9155fe14f54164c1b9eb0a319d98ef02c160725587ad60f14ddc57b6f96 (from -r /opt/electrum/contrib/build-linux/sdist/../../../contrib/deterministic-build/requirements.txt (line 112)), but installing version 0.30.0

--- contrib/build-linux/sdist/build.sh
+++ contrib/build-linux/sdist/build.sh
@@ -9,6 +9,15 @@ DISTDIR="$PROJECT_ROOT/dist"
 
 . "$CONTRIB"/build_tools_util.sh
 
+python3 --version || fail "python interpreter not found"
+
+# upgrade to modern pip so that it knows the flags we need.
+# we will then install a pinned version of pip as part of requirements-sdist-build
+python3 -m pip install --upgrade pip
+
+info "Installing pinned requirements."
+python3 -m pip install --no-dependencies --no-warn-script-location -r "$CONTRIB"/deterministic-build/requirements-sdist-build.txt
+
 
 "$CONTRIB"/make_packages || fail "make_packages failed"
 
