spesmilo/electrum

commit adc97af58cefbad32c5a98f755aec35fb650360c

clear up requirements re pycryptodomex

setup.py
@@ -53,8 +53,13 @@ if platform.system() in ['Linux', 'FreeBSD', 'DragonFly']:
 extras_require = {
     'hardware': requirements_hw,
     'gui': ['pyqt5'],
+    'crypto': ['pycryptodomex>=3.7'],
+    'tests': ['pycryptodomex>=3.7', 'cryptography>=2.1'],
 }
-extras_require['full'] = [pkg for sublist in list(extras_require.values()) for pkg in sublist]
+# 'full' extra that tries to grab everything an enduser would need (except for libsecp256k1...)
+extras_require['full'] = [pkg for sublist in ['hardware', 'gui', 'crypto'] for pkg in sublist]
+# legacy. keep 'fast' extra working
+extras_require['fast'] = extras_require['crypto']
 
 
 setup(

.travis.yml
@@ -30,7 +30,7 @@ jobs:
         - sudo apt-get -qq update
         - sudo apt-get install -yq bitcoind
         - sudo apt-get -y install libsecp256k1-0
-        - pip install -r contrib/requirements/requirements.txt
+        - pip install .[tests]
         - pip install electrumx
       before_script:
           - electrum/tests/regtest/start_bitcoind.sh

tox.ini
@@ -9,4 +9,4 @@ commands=
 	coverage run --source=electrum '--omit=electrum/gui/*,electrum/plugins/*,electrum/scripts/*,electrum/tests/*' -m py.test -v
 	coverage report
 extras=
-	fast
+	tests

commit 0b6ce657b152448ed7640d824385791d13938a22

setup.py: fix 'full' extra
got broken in adc97af58cefbad32c5a98f755aec35fb650360c

setup.py
@@ -57,7 +57,9 @@ extras_require = {
     'tests': ['pycryptodomex>=3.7', 'cryptography>=2.1'],
 }
 # 'full' extra that tries to grab everything an enduser would need (except for libsecp256k1...)
-extras_require['full'] = [pkg for sublist in ['hardware', 'gui', 'crypto'] for pkg in sublist]
+extras_require['full'] = [pkg for sublist in
+                          (extras_require['hardware'], extras_require['gui'], extras_require['crypto'])
+                          for pkg in sublist]
 # legacy. keep 'fast' extra working
 extras_require['fast'] = extras_require['crypto']
 