cb4c8abe1c121ee8f7c39de6b4b1824821542b7e
ThomasV <thomasv@electrum.org>
2020-06-18 14:15:05 +0200

    submarine swaps: disable merging of transaction in history
    This is too complicated and ugly because it relies on side
    effects. What we should do instead is collapse transactions
    in children nodes of QTreeView (see #6237)

--- electrum/lnworker.py
+++ electrum/lnworker.py
@@ -634,10 +634,10 @@ class LNWallet(LNWorker):
             swap = self.swap_manager.get_swap(payment_hash)
             if swap:
                 if swap.is_reverse:
-                    item['txid'] = swap.spending_txid
+                    #item['txid'] = swap.spending_txid
                     item['label'] = 'Reverse swap' + ' ' + self.config.format_amount_and_units(swap.lightning_amount)
                 else:
-                    item['txid'] = swap.funding_txid
+                    #item['txid'] = swap.funding_txid
                     item['label'] = 'Normal swap' + ' ' + self.config.format_amount_and_units(swap.onchain_amount)
             # done
             out[payment_hash] = item
@@ -677,29 +677,6 @@ class LNWallet(LNWorker):
                 'fee_msat': None,
             }
             out[closing_txid] = item
-        # add submarine swaps
-        settled_payments = self.get_settled_payments()
-        current_height = self.network.get_local_height()
-        for payment_hash_hex, swap in self.swap_manager.swaps.items():
-            txid = swap.spending_txid if swap.is_reverse else swap.funding_txid
-            if txid is None:
-                continue
-            if payment_hash_hex in settled_payments:
-                plist = settled_payments[payment_hash_hex]
-                info = self.get_payment_info(bytes.fromhex(payment_hash_hex))
-                amount_msat, fee_msat, timestamp = self.get_payment_value(info, plist)
-            else:
-                amount_msat = 0
-            label = 'Reverse swap' if swap.is_reverse else 'Normal swap'
-            delta = current_height - swap.locktime
-            if delta < 0:
-                label += f' (refundable in {-delta} blocks)'
-            out[txid] = {
-                'txid': txid,
-                'amount_msat': amount_msat,
-                'type': 'swap',
-                'label': label
-            }
         return out
 
     def get_history(self):
