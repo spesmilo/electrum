bbb6599c2a389ea3adbe62e77b18f80985130649
Richard <36215881+hoganri@users.noreply.github.com>
2020-06-25 12:49:27 -0400

    Adds mempool.space testnet explorer
    Didn't realize they had testnet as well - added

--- electrum/util.py
+++ electrum/util.py
@@ -758,6 +758,8 @@ testnet_block_explorers = {
                        {'tx': 'tx/', 'addr': 'address/'}),
     'Blockstream.info': ('https://blockstream.info/testnet/',
                         {'tx': 'tx/', 'addr': 'address/'}),
+    'mempool.space': ('https://mempool.space/testnet/',
+                        {'tx': 'tx/', 'addr': 'address/'}),    
     'smartbit.com.au': ('https://testnet.smartbit.com.au/',
                        {'tx': 'tx/', 'addr': 'address/'}),
     'system default': ('blockchain://000000000933ea01ad0ee984209779baaec3ced90fa3f408719526f8d77f4943/',
