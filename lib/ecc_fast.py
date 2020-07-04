231535887a081fe3b877345e325d3866606b313c
Calin Culianu <calin.culianu@gmail.com>
2020-07-04 20:35:27 +0300

    Fix ecc_fast: ecdsa sometimes uses mpz from gmpy2
    Sometimes the ints are not ints, but rather the 'mpz' type from gmpy2.
    We added a workaround for this to support whatever ecdsa is doing under
    the hood.

--- lib/ecc_fast.py
+++ lib/ecc_fast.py
@@ -41,12 +41,12 @@ def _prepare_monkey_patching_of_python_ecdsa_internals_with_libsecp256k1():
         if self == point_at_infinity or other == 0:
             return point_at_infinity
         pubkey = create_string_buffer(64)
-        public_pair_bytes = b'\4' + self.x().to_bytes(32, byteorder="big") + self.y().to_bytes(32, byteorder="big")
+        public_pair_bytes = b'\4' + int(self.x()).to_bytes(32, byteorder="big") + int(self.y()).to_bytes(32, byteorder="big")
         r = secp256k1.secp256k1.secp256k1_ec_pubkey_parse(
             secp256k1.secp256k1.ctx, pubkey, public_pair_bytes, len(public_pair_bytes))
         if not r:
             return False
-        r = secp256k1.secp256k1.secp256k1_ec_pubkey_tweak_mul(secp256k1.secp256k1.ctx, pubkey, other.to_bytes(32, byteorder="big"))
+        r = secp256k1.secp256k1.secp256k1_ec_pubkey_tweak_mul(secp256k1.secp256k1.ctx, pubkey, int(other).to_bytes(32, byteorder="big"))
         if not r:
             return point_at_infinity
 
@@ -63,37 +63,39 @@ def _prepare_monkey_patching_of_python_ecdsa_internals_with_libsecp256k1():
         if self.public_key.curve != curve_secp256k1:
             # this operation is not on the secp256k1 curve; use original implementation
             return _patched_functions.orig_sign(self, hash, random_k)
+        # might not be int but might rather be gmpy2 'mpz' type
+        maybe_mpz = type(hash)
         secret_exponent = self.secret_multiplier
         nonce_function = None
         sig = create_string_buffer(64)
-        sig_hash_bytes = hash.to_bytes(32, byteorder="big")
+        sig_hash_bytes = int(hash).to_bytes(32, byteorder="big")
         secp256k1.secp256k1.secp256k1_ecdsa_sign(
-            secp256k1.secp256k1.ctx, sig, sig_hash_bytes, secret_exponent.to_bytes(32, byteorder="big"), nonce_function, None)
+            secp256k1.secp256k1.ctx, sig, sig_hash_bytes, int(secret_exponent).to_bytes(32, byteorder="big"), nonce_function, None)
         compact_signature = create_string_buffer(64)
         secp256k1.secp256k1.secp256k1_ecdsa_signature_serialize_compact(secp256k1.secp256k1.ctx, compact_signature, sig)
         r = int.from_bytes(compact_signature[:32], byteorder="big")
         s = int.from_bytes(compact_signature[32:], byteorder="big")
-        return ecdsa.ecdsa.Signature(r, s)
+        return ecdsa.ecdsa.Signature(maybe_mpz(r), maybe_mpz(s))
 
     def verify(self: ecdsa.ecdsa.Public_key, hash: int, signature: ecdsa.ecdsa.Signature):
         if self.curve != curve_secp256k1:
             # this operation is not on the secp256k1 curve; use original implementation
             return _patched_functions.orig_verify(self, hash, signature)
         sig = create_string_buffer(64)
-        input64 = signature.r.to_bytes(32, byteorder="big") + signature.s.to_bytes(32, byteorder="big")
+        input64 = int(signature.r).to_bytes(32, byteorder="big") + int(signature.s).to_bytes(32, byteorder="big")
         r = secp256k1.secp256k1.secp256k1_ecdsa_signature_parse_compact(secp256k1.secp256k1.ctx, sig, input64)
         if not r:
             return False
         r = secp256k1.secp256k1.secp256k1_ecdsa_signature_normalize(secp256k1.secp256k1.ctx, sig, sig)
 
-        public_pair_bytes = b'\4' + self.point.x().to_bytes(32, byteorder="big") + self.point.y().to_bytes(32, byteorder="big")
+        public_pair_bytes = b'\4' + int(self.point.x()).to_bytes(32, byteorder="big") + int(self.point.y()).to_bytes(32, byteorder="big")
         pubkey = create_string_buffer(64)
         r = secp256k1.secp256k1.secp256k1_ec_pubkey_parse(
             secp256k1.secp256k1.ctx, pubkey, public_pair_bytes, len(public_pair_bytes))
         if not r:
             return False
 
-        return 1 == secp256k1.secp256k1.secp256k1_ecdsa_verify(secp256k1.secp256k1.ctx, sig, hash.to_bytes(32, byteorder="big"), pubkey)
+        return 1 == secp256k1.secp256k1.secp256k1_ecdsa_verify(secp256k1.secp256k1.ctx, sig, int(hash).to_bytes(32, byteorder="big"), pubkey)
 
     # save new functions so that we can (re-)do patching
     _patched_functions.fast_sign   = sign
