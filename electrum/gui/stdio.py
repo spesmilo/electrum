843eb2788292a4a6a63b66ac074075e5ed445083
Henrik Grimler <henrik@grimler.se>
2020-09-09 10:56:57 +0200

    gui: update stdio and text after db and storage separation
    e1ce3aace7e3143da24115890d9bae78a9f5bcaf updated the qt and kivy guis, but not stdio or text one.

--- electrum/gui/stdio.py
+++ electrum/gui/stdio.py
@@ -5,6 +5,7 @@ import logging
 
 from electrum import util
 from electrum import WalletStorage, Wallet
+from electrum.wallet_db import WalletDB
 from electrum.util import format_satoshis
 from electrum.bitcoin import is_address, COIN
 from electrum.transaction import PartialTxOutput
@@ -30,6 +31,8 @@ class ElectrumGui:
             password = getpass.getpass('Password:', stream=None)
             storage.decrypt(password)
 
+        db = WalletDB(storage.read(), manual_upgrades=False)
+
         self.done = 0
         self.last_balance = ""
 
@@ -40,7 +43,7 @@ class ElectrumGui:
         self.str_amount = ""
         self.str_fee = ""
 
-        self.wallet = Wallet(storage, config=config)
+        self.wallet = Wallet(db, storage, config=config)
         self.wallet.start_network(self.network)
         self.contacts = self.wallet.contacts
 
