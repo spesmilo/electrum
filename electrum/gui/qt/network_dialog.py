7286f87766c616a3cf5811aa1326e70e873d357c
bitromortac <bitromortac@protonmail.com>
2020-09-29 07:01:59 +0200

    network-dialog: include protocol in server field
    In this way it is now possible again to use plain server connections
    without reverting it automatically to tls connections.

--- electrum/gui/qt/network_dialog.py
+++ electrum/gui/qt/network_dialog.py
@@ -327,7 +327,7 @@ class NetworkChoiceLayout(object):
         server = net_params.server
         auto_connect = net_params.auto_connect
         if not self.server_e.hasFocus():
-            self.server_e.setText(server.net_addr_str())
+            self.server_e.setText(str(server))
         self.autoconnect_cb.setChecked(auto_connect)
 
         height_str = "%d "%(self.network.get_local_height()) + _('blocks')
