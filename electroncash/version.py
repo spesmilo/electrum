63840442fda2d637acc6af4a2a9dda790438ca12
Calin Culianu <calin.culianu@gmail.com>
2020-11-18 12:11:45 +0200

    Bumped version to 4.2.1

--- electroncash/version.py
+++ electroncash/version.py
@@ -1,4 +1,4 @@
-PACKAGE_VERSION  = '4.2.0'   # version of the client package
+PACKAGE_VERSION  = '4.2.1'   # version of the client package
 PROTOCOL_VERSION = '1.4'     # protocol version requested
 
 # The hash of the Electrum mnemonic seed must begin with this
