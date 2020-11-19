ad7455885368afc0184dcd37f89cc42a9e679f85
Calin Culianu <calin.culianu@gmail.com>
2020-11-19 02:11:34 +0200

    Fix rare None dereference
    Closes #1344

--- electroncash/synchronizer.py
+++ electroncash/synchronizer.py
@@ -154,7 +154,8 @@ class Synchronizer(ThreadJob):
         tx_fees = dict(filter(lambda x:x[1] is not None, tx_fees))
         # Note if the server hasn't been patched to sort the items properly
         if hist != sorted(hist, key=lambda x:x[1]):
-            self.network.interface.print_error("serving improperly sorted address histories")
+            which = self.network.interface or self
+            which.print_error("serving improperly sorted address histories")
         # Check that txids are unique
         if len(hashes) != len(result):
             self.print_error("error: server history has non-unique txids: {}"
