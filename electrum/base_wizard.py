spesmilo/electrum

commit 77e0d3745e990e97ddbac5550e399f3e695af788

fix #4326

electrum/gui/kivy/uix/dialogs/installwizard.py
@@ -874,16 +874,18 @@ class RestoreSeedDialog(WizardDialog):
         self.ids.text_input_seed.text = ''
         self.message = _('Please type your seed phrase using the virtual keyboard.')
         self.title = _('Enter Seed')
-        self.ext = False
-        self.bip39 = False
+        self.opt_ext = kwargs['opt_ext']
+        self.opt_bip39 = kwargs['opt_bip39']
+        self.is_ext = False
+        self.is_bip39 = False
 
     def options_dialog(self):
         from .seed_options import SeedOptionsDialog
         def callback(ext, bip39):
-            self.ext = ext
-            self.bip39 = bip39
+            self.is_ext = ext
+            self.is_bip39 = bip39
             self.update_next_button()
-        d = SeedOptionsDialog(self.ext, self.bip39, callback)
+        d = SeedOptionsDialog(self.opt_ext, self.opt_bip39, self.is_ext, self.is_bip39, callback)
         d.open()
 
     def get_suggestions(self, prefix):
@@ -892,7 +894,7 @@ class RestoreSeedDialog(WizardDialog):
                 yield w
 
     def update_next_button(self):
-        self.ids.next.disabled = False if self.bip39 else not bool(self._test(self.get_text()))
+        self.ids.next.disabled = False if self.is_bip39 else not bool(self._test(self.get_text()))
 
     def on_text(self, dt):
         self.update_next_button()
@@ -980,7 +982,7 @@ class RestoreSeedDialog(WizardDialog):
             tis.focus = False
 
     def get_params(self, b):
-        return (self.get_text(), self.bip39, self.ext)
+        return (self.get_text(), self.is_bip39, self.is_ext)
 
 
 class ConfirmSeedDialog(RestoreSeedDialog):
@@ -1094,6 +1096,8 @@ class InstallWizard(BaseWizard, Widget):
         ConfirmSeedDialog(self, **kwargs).open()
 
     def restore_seed_dialog(self, **kwargs):
+        kwargs['opt_bip39'] = self.opt_bip39
+        kwargs['opt_ext'] = self.opt_ext
         RestoreSeedDialog(self, **kwargs).open()
 
     def confirm_dialog(self, **kwargs):

electrum/gui/kivy/uix/dialogs/seed_options.py
@@ -6,6 +6,10 @@ from kivy.lang import Builder
 Builder.load_string('''
 <SeedOptionsDialog@Popup>
     id: popup
+    opt_bip39: False
+    opt_ext: False
+    is_bip39: False
+    is_ext: False
     title: _('Seed Options')
     size_hint: 0.8, 0.8
     pos_hint: {'top':0.9}
@@ -22,16 +26,24 @@ Builder.load_string('''
             size_hint: 1, 0.2
             Label:
                 text: _('Extend Seed')
+                opacity: 1 if root.opt_ext else 0
             CheckBox:
                 id:ext
+                disabled: not root.opt_ext
+                opacity: 1 if root.opt_ext else 0
+                active: root.is_ext
         BoxLayout:
             orientation: 'horizontal'
             size_hint: 1, 0.2
             Label:
                 text: _('BIP39')
                 id:bip39_label
+                opacity: 1 if root.opt_bip39 else 0
             CheckBox:
                 id:bip39
+                disabled: not root.opt_bip39
+                opacity: 1 if root.opt_bip39 else 0
+                active: root.is_bip39
         Widget:
             size_hint: 1, 0.1
         BoxLayout:
@@ -53,13 +65,10 @@ Builder.load_string('''
 
 
 class SeedOptionsDialog(Factory.Popup):
-    def __init__(self, is_ext, is_bip39, callback):
+    def __init__(self, opt_ext, opt_bip39, is_ext, is_bip39, callback):
         Factory.Popup.__init__(self)
-        self.ids.ext.active = is_ext
-        if is_bip39 is None:
-            self.ids.bip39.opacity = 0
-            self.ids.bip39_label.opacity = 0
-            self.ids.bip39.disabled = True
-        else:
-            self.ids.bip39.active = is_bip39
+        self.opt_ext = opt_ext
+        self.opt_bip39 = opt_bip39
+        self.is_ext = is_ext
+        self.is_bip39 = is_bip39
         self.callback = callback

commit 620a6aaf990f7dce07c3cd7ae87c4c1c9f70d505

kivy: fix confirm_seed_dialog (broken in 77e0d3745e990e97ddbac5550e399f3e695af788)

electrum/base_wizard.py
@@ -694,6 +694,7 @@ class BaseWizard(Logger):
         self.seed_type = seed_type
         seed = mnemonic.Mnemonic('en').make_seed(seed_type=self.seed_type)
         self.opt_bip39 = False
+        self.opt_ext = True
         f = lambda x: self.request_passphrase(seed, x)
         self.show_seed_dialog(run_next=f, seed_text=seed)
 

electrum/gui/kivy/uix/dialogs/installwizard.py
@@ -835,11 +835,12 @@ class ChoiceLineDialog(WizardChoiceDialog):
 class ShowSeedDialog(WizardDialog):
     seed_text = StringProperty('')
     message = _("If you forget your PIN or lose your device, your seed phrase will be the only way to recover your funds.")
-    ext = False
 
     def __init__(self, wizard, **kwargs):
         super(ShowSeedDialog, self).__init__(wizard, **kwargs)
         self.seed_text = kwargs['seed_text']
+        self.opt_ext = True
+        self.is_ext = False
 
     def on_parent(self, instance, value):
         if value:
@@ -848,12 +849,12 @@ class ShowSeedDialog(WizardDialog):
     def options_dialog(self):
         from .seed_options import SeedOptionsDialog
         def callback(ext, _):
-            self.ext = ext
-        d = SeedOptionsDialog(self.ext, None, callback)
+            self.is_ext = ext
+        d = SeedOptionsDialog(self.opt_ext, False, self.is_ext, False, callback)
         d.open()
 
     def get_params(self, b):
-        return (self.ext,)
+        return (self.is_ext,)
 
 
 class WordButton(Button):
@@ -1099,6 +1100,8 @@ class InstallWizard(BaseWizard, Widget):
     def confirm_seed_dialog(self, **kwargs):
         kwargs['title'] = _('Confirm Seed')
         kwargs['message'] = _('Please retype your seed phrase, to confirm that you properly saved it')
+        kwargs['opt_bip39'] = self.opt_bip39
+        kwargs['opt_ext'] = self.opt_ext
         ConfirmSeedDialog(self, **kwargs).open()
 
     def restore_seed_dialog(self, **kwargs):