e70dcb70428390d37d410a29d8e2568b2ebe9bfe
Axel Gembe <derago@gmail.com>
2021-02-09 16:59:28 +0700

    Build: Update Python to version 3.8.7
    The old Python version 3.6 was not getting binary updates for Windows
    anymore, so updating is needed.
    
    The Linux reproducible buildinfo patch has been updated from Ubuntu.
    
    Python does not have the `--with-threads` option anymore, so that has
    been removed.
    
    Dependencies have been updated and the `pyscard` install step on Windows
    has been removed.
    
    `pyblake2` and `websocket-client` unused dependencies have been removed.
    
    The `requirements-build-wine` dependencies have been included in the
    `freeze_packages` scripts.

--- contrib/build-linux/appimage/scripts/common.sh
+++ contrib/build-linux/appimage/scripts/common.sh
@@ -1,6 +1,6 @@
 set -e
 
-PYTHON="${APPDIR}/usr/bin/python3.6"
+PYTHON="${APPDIR}/usr/bin/python3.8"
 
 export LD_LIBRARY_PATH="${APPDIR}/usr/lib/:${APPDIR}/usr/lib/x86_64-linux-gnu${LD_LIBRARY_PATH+:$LD_LIBRARY_PATH}"
 export PATH="${APPDIR}/usr/bin:${PATH}"
