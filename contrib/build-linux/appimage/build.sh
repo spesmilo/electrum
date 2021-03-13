eaffced6ddee8c40bd6cbff3bdde5462beff7db7
SomberNight <somber.night@protonmail.com>
2021-03-13 15:04:22 +0100

    binaries: bump python version

--- contrib/build-linux/appimage/build.sh
+++ contrib/build-linux/appimage/build.sh
@@ -16,7 +16,7 @@ export GCC_STRIP_BINARIES="1"
 # pinned versions
 # note: compiling python 3.8.x requires at least glibc 2.27,
 #       which is first available on ubuntu 18.04
-PYTHON_VERSION=3.7.9
+PYTHON_VERSION=3.7.10
 PKG2APPIMAGE_COMMIT="eb8f3acdd9f11ab19b78f5cb15daa772367daf15"
 SQUASHFSKIT_COMMIT="ae0d656efa2d0df2fcac795b6823b44462f19386"
 
@@ -41,7 +41,7 @@ download_if_not_exist "$CACHEDIR/appimagetool" "https://github.com/AppImage/AppI
 verify_hash "$CACHEDIR/appimagetool" "d918b4df547b388ef253f3c9e7f6529ca81a885395c31f619d9aaf7030499a13"
 
 download_if_not_exist "$CACHEDIR/Python-$PYTHON_VERSION.tar.xz" "https://www.python.org/ftp/python/$PYTHON_VERSION/Python-$PYTHON_VERSION.tar.xz"
-verify_hash "$CACHEDIR/Python-$PYTHON_VERSION.tar.xz" "91923007b05005b5f9bd46f3b9172248aea5abc1543e8a636d59e629c3331b01"
+verify_hash "$CACHEDIR/Python-$PYTHON_VERSION.tar.xz" "f8d82e7572c86ec9d55c8627aae5040124fd2203af400c383c821b980306ee6b"
 
 
 
