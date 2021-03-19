65e16fabd2f3ef03d728f4046c27315b0939f351
SomberNight <somber.night@protonmail.com>
2021-03-19 21:21:09 +0100

    follow-up d8352f1a0acbbaa4c4b8a995414ea2eb3fd69dba
    message-induced exceptions should not propagate out

--- electrum/lnonion.py
+++ electrum/lnonion.py
@@ -33,7 +33,8 @@ from .crypto import sha256, hmac_oneshot, chacha20_encrypt
 from .util import bh2u, profiler, xor_bytes, bfh
 from .lnutil import (get_ecdh, PaymentFailure, NUM_MAX_HOPS_IN_PAYMENT_PATH,
                      NUM_MAX_EDGES_IN_PAYMENT_PATH, ShortChannelID, OnionFailureCodeMetaFlag)
-from .lnmsg import OnionWireSerializer, read_bigsize_int, write_bigsize_int, UnknownMsgType
+from .lnmsg import OnionWireSerializer, read_bigsize_int, write_bigsize_int
+from . import lnmsg
 
 if TYPE_CHECKING:
     from .lnrouter import LNPaymentRoute
@@ -443,7 +444,7 @@ class OnionRoutingFailure(Exception):
     def decode_data(self) -> Optional[Dict[str, Any]]:
         try:
             message_type, payload = OnionWireSerializer.decode_msg(self.to_bytes())
-        except UnknownMsgType:
+        except lnmsg.FailedToParseMsg:
             payload = None
         return payload
 
