1e3373bd2f7aaeec6507db22975cec91ff265527
SomberNight <somber.night@protonmail.com>
2021-03-27 01:44:18 +0100

    cosigner_pool: fix #7134

--- electrum/plugins/cosigner_pool/qt.py
+++ electrum/plugins/cosigner_pool/qt.py
@@ -34,7 +34,7 @@ import certifi
 
 from electrum import util, keystore, ecc, crypto
 from electrum import transaction
-from electrum.transaction import Transaction, PartialTransaction, tx_from_any
+from electrum.transaction import Transaction, PartialTransaction, tx_from_any, SerializationError
 from electrum.bip32 import BIP32Node
 from electrum.plugin import BasePlugin, hook
 from electrum.i18n import _
@@ -248,5 +248,9 @@ class Plugin(BasePlugin):
             return
 
         self.listener.clear(keyhash)
-        tx = tx_from_any(message)
+        try:
+            tx = tx_from_any(message)
+        except SerializationError as e:
+            window.show_error(_("Electrum was unable to deserialize the transaction:") + "\n" + str(e))
+            return
         show_transaction(tx, parent=window, prompt_if_unsaved=True)
