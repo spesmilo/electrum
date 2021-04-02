c3d25be8e54786e8df8431f7db46a02ca5a31c91
gruve-p <jackielove4u@hotmail.com>
2021-04-02 18:26:47 +0200

    Fixes after merging upstream

--- contrib/release.sh
+++ contrib/release.sh
@@ -5,8 +5,8 @@
 # 3. cd .. && git push
 #!/bin/bash
 
-ELECTRUM_DIR=/opt/electrum
-WWW_DIR=/opt/electrum-web
+ELECTRUM_DIR=/opt/electrum-grs
+#WWW_DIR=/opt/electrum-web
 
 # Note:
 # uploadserver and website are set in /etc/hosts
@@ -16,7 +16,7 @@ cd $ELECTRUM_DIR
 # rm -f .buildozer
 
 
-VERSION=`python3 -c "import electrum; print(electrum.version.ELECTRUM_VERSION)"`
+VERSION=`python3 -c "import electrum_grs; print(electrum_grs.version.ELECTRUM_VERSION)"`
 echo "VERSION: $VERSION"
 REV=`git describe --tags`
 echo "REV: $REV"
@@ -32,54 +32,54 @@ fi
 set -ex
 
 # create tarball
-target=Electrum-$VERSION.tar.gz
+target=Electrum-grs-$VERSION.tar.gz
 if test -f dist/$target; then
     echo "file exists: $target"
 else
    pushd .
-   sudo docker build -t electrum-sdist-builder-img contrib/build-linux/sdist
+   sudo docker build -t electrum-grs-sdist-builder-img contrib/build-linux/sdist
    FRESH_CLONE=contrib/build-linux/sdist/fresh_clone && \
        sudo rm -rf $FRESH_CLONE && \
        umask 0022 && \
        mkdir -p $FRESH_CLONE && \
        cd $FRESH_CLONE  && \
-       git clone https://github.com/spesmilo/electrum.git &&\
-       cd electrum
+       git clone https://github.com/Groestlcoin/electrum-grs.git &&\
+       cd electrum-grs
    #git checkout $REV
    sudo docker run -it \
-	--name electrum-sdist-builder-cont \
-	-v $PWD:/opt/electrum \
+	--name electrum-grs-sdist-builder-cont \
+	-v $PWD:/opt/electrum-grs \
 	--rm \
-	--workdir /opt/electrum/contrib/build-linux/sdist \
+	--workdir /opt/electrum-grs/contrib/build-linux/sdist \
 	electrum-sdist-builder-img \
 	./build.sh
    popd
-   cp /opt/electrum/contrib/build-linux/sdist/fresh_clone/electrum/dist/$target dist/
+   cp /opt/electrum-grs/contrib/build-linux/sdist/fresh_clone/electrum/dist/$target dist/
 fi
 
 # appimage
 if [ $REV != $VERSION ]; then
-    target=electrum-${REV:0:-2}-x86_64.AppImage
+    target=electrum-grs-${REV:0:-2}-x86_64.AppImage
 else
-    target=electrum-$REV-x86_64.AppImage
+    target=electrum-grs-$REV-x86_64.AppImage
 fi
 
 if test -f dist/$target; then
     echo "file exists: $target"
 else
-    sudo docker build -t electrum-appimage-builder-img contrib/build-linux/appimage
+    sudo docker build -t electrum-grs-appimage-builder-img contrib/build-linux/appimage
     sudo docker run -it \
-         --name electrum-appimage-builder-cont \
-	 -v $PWD:/opt/electrum \
+         --name electrum-grs-appimage-builder-cont \
+	 -v $PWD:/opt/electrum-grs \
          --rm \
-	 --workdir /opt/electrum/contrib/build-linux/appimage \
+	 --workdir /opt/electrum-grs/contrib/build-linux/appimage \
          electrum-appimage-builder-img \
 	 ./build.sh
 fi
 
 
 # windows
-target=electrum-$REV.exe
+target=electrum-grs-$REV.exe
 if test -f dist/$target; then
     echo "file exists: $target"
 else
@@ -88,41 +88,41 @@ else
         sudo rm -rf $FRESH_CLONE && \
         mkdir -p $FRESH_CLONE && \
         cd $FRESH_CLONE  && \
-        git clone https://github.com/spesmilo/electrum.git && \
-        cd electrum
+        git clone https://github.com/groestlcoin/electrum-grs.git && \
+        cd electrum-grs
     #git checkout $REV
     sudo docker run -it \
-        --name electrum-wine-builder-cont \
-        -v $PWD:/opt/wine64/drive_c/electrum \
+        --name electrum-grs-wine-builder-cont \
+        -v $PWD:/opt/wine64/drive_c/electrum-grs \
         --rm \
-        --workdir /opt/wine64/drive_c/electrum/contrib/build-wine \
-        electrum-wine-builder-img \
+        --workdir /opt/wine64/drive_c/electrum-grs/contrib/build-wine \
+        electrum-grs-wine-builder-img \
         ./build.sh
     # do this in the fresh clone directorry!
-    cd contrib/build-wine/
-    ./sign.sh
-    cp ./signed/*.exe /opt/electrum/dist/
-    popd
+    #cd contrib/build-wine/
+    #./sign.sh
+    #cp ./signed/*.exe /opt/electrum-grs/dist/
+    #popd
 fi
 
 # android
-target1=Electrum-$VERSION.0-armeabi-v7a-release.apk
-target2=Electrum-$VERSION.0-arm64-v8a-release.apk
+target1=ElectrumGRS-$VERSION.0-armeabi-v7a-release.apk
+target2=ElectrumGRS-$VERSION.0-arm64-v8a-release.apk
 
 if test -f dist/$target1; then
     echo "file exists: $target"
 else
     ./contrib/make_packages
-    sudo docker build -t electrum-android-builder-img contrib/android
+    sudo docker build -t electrum-grs-android-builder-img contrib/android
 
     mkdir --parents $PWD/.buildozer/.gradle
     sudo docker run -it --rm \
-         --name electrum-android-builder-cont \
-         -v $PWD:/home/user/wspace/electrum \
+         --name electrum-grs-android-builder-cont \
+         -v $PWD:/home/user/wspace/electrum-grs \
          -v $PWD/.buildozer/.gradle:/home/user/.gradle \
          -v ~/.keystore:/home/user/.keystore \
-         --workdir /home/user/wspace/electrum \
-         electrum-android-builder-img \
+         --workdir /home/user/wspace/electrum-grs \
+         electrum-grs-android-builder-img \
          ./contrib/android/make_apk release
 
     cp bin/$target1 dist/
@@ -131,51 +131,51 @@ else
 fi
 
 # wait for dmg before signing
-if test -f dist/electrum-$VERSION.dmg; then
-    if test -f dist/electrum-$VERSION.dmg.asc; then
-	echo "packages are already signed"
-    else
-	echo "signing packages"
-	./contrib/sign_packages
-    fi
-else
-    echo "dmg is missing, aborting"
-    exit 1
-fi
+#if test -f dist/electrum-grs-$VERSION.dmg; then
+#    if test -f dist/electrum-grs-$VERSION.dmg.asc; then
+#	echo "packages are already signed"
+#    else
+#	echo "signing packages"
+#	./contrib/sign_packages
+#    fi
+#else
+#    echo "dmg is missing, aborting"
+#    exit 1
+#fi
 
 
 
-echo "updating www repo"
-./contrib/make_download $WWW_DIR
-echo "signing the version file"
-sig=`./run_electrum -o signmessage $ELECTRUM_SIGNING_ADDRESS $VERSION -w $ELECTRUM_SIGNING_WALLET`
-echo "{ \"version\":\"$VERSION\", \"signatures\":{ \"$ELECTRUM_SIGNING_ADDRESS\":\"$sig\"}}" > $WWW_DIR/version
+#echo "updating www repo"
+#./contrib/make_download $WWW_DIR
+#echo "signing the version file"
+#sig=`./run_electrum_grs -o signmessage $ELECTRUM_SIGNING_ADDRESS $VERSION -w $ELECTRUM_SIGNING_WALLET`
+#echo "{ \"version\":\"$VERSION\", \"signatures\":{ \"$ELECTRUM_SIGNING_ADDRESS\":\"$sig\"}}" > $WWW_DIR/version
 
 
-if [ $REV != $VERSION ]; then
-    echo "versions differ, not uploading"
-    exit 1
-fi
+#if [ $REV != $VERSION ]; then
+#    echo "versions differ, not uploading"
+#    exit 1
+#fi
 
 # upload the files
-if test -f dist/uploaded; then
-    echo "files already uploaded"
-else
-    ./contrib/upload uploadserver
-    touch dist/uploaded
-fi
+#if test -f dist/uploaded; then
+#    echo "files already uploaded"
+#else
+#    ./contrib/upload uploadserver
+#    touch dist/uploaded
+#fi
 
 #exit 0
 
 # push changes to website
-pushd $WWW_DIR
-git diff
-git commit -a -m "version $VERSION"
-git push
-popd
+#pushd $WWW_DIR
+#git diff
+#git commit -a -m "version $VERSION"
+#git push
+#popd
 
 # update webserver:
-echo "to deploy, type:"
-echo "ssh root@website \"cd /var/www/new; git pull github master\""
+#echo "to deploy, type:"
+#echo "ssh root@website \"cd /var/www/new; git pull github master\""
 
 # clear cloudflare cache
