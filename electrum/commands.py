07eb7840121fa6b5188a21513dffe8ae52d3d341
James Lovejoy <jameslovejoy1@gmail.com>
2021-04-12 18:54:41 -0400

    Enable sending to scripts from payto

--- electrum/commands.py
+++ electrum/commands.py
@@ -622,7 +622,7 @@ class Commands:
         change_addr = self._resolver(change_addr, wallet)
         domain_addr = None if domain_addr is None else map(self._resolver, domain_addr, repeat(wallet))
         amount_sat = satoshis_or_max(amount)
-        outputs = [PartialTxOutput.from_address_and_value(destination, amount_sat)]
+        outputs = [PartialTxOutput(scriptpubkey=bitcoin.parse_output(destination), value=amount_sat)]
         tx = wallet.create_transaction(
             outputs,
             fee=tx_fee,
