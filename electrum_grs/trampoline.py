8d40855edfdbd7dbbf58df629336a7d50aaac1ec
gruve-p <jackielove4u@hotmail.com>
2021-05-31 20:28:13 +0200

    Add Trampoline node

--- electrum_grs/trampoline.py
+++ electrum_grs/trampoline.py
@@ -54,16 +54,11 @@ TRAMPOLINE_FEES = [
 # hardcoded list
 # TODO for some pubkeys, there are multiple network addresses we could try
 TRAMPOLINE_NODES_MAINNET = {
-    'grspay':                 LNPeerAddr(host='104.236.133.196',         port=9735, pubkey=bytes.fromhex('02c0ef8fad8a9d3176424826367a7f9470feb2ad86fa4634d9454190eda876d0ad')),
-    'lnmainnet':              LNPeerAddr(host='104.236.130.222',         port=9735, pubkey=bytes.fromhex('0221af9e2a67c90ffac37db331ab847439509465d599933d4b9b20352ae493c1c1')),
-    'lndhub':                 LNPeerAddr(host='104.236.14.225',          port=9735, pubkey=bytes.fromhex('02cfc4c212e4183ca8bd8bd21e18996603f69706553d5f4e1464fdc6cb612b433b')),
-    'swaps':                  LNPeerAddr(host='104.236.15.120',          port=9735, pubkey=bytes.fromhex('02f31ea82a5c07387abec0274b457db35355ef874e21bb2fdc4f4abb3f5d0d82ce')),
+    'eclair':                 LNPeerAddr(host='82.196.13.206',           port=9735, pubkey=bytes.fromhex('02576fe2dfc26879c751a38f69a1e6b6d6646fa3edf045d5534d8674a188c7da81')),
 }
 
 TRAMPOLINE_NODES_TESTNET = {
-    'grspay testnet': LNPeerAddr(host='95.179.140.39', port=9735, pubkey=bytes.fromhex('0384dee0ec597a7b8235ccf56c68ffa0af5dae72b3455aa3ecb81c4fc4eef9ef2c')),
-    'lntestnet': LNPeerAddr(host='45.32.236.128', port=9735, pubkey=bytes.fromhex('02435dea09ad875c36c88f680845277245e7e8bdd28b3bb20470e82e4de0c3cb09')),
-    'testnet swaps': LNPeerAddr(host='78.141.220.223', port=9735, pubkey=bytes.fromhex('025da2bb1df5afad0f31680414755800d13eb382423ad59e984d93c68c1ea78542')),
+    'eclair testnet': LNPeerAddr(host='108.61.99.169', port=9735, pubkey=bytes.fromhex('021fedfc02b43971339bf9052e2c639e182be6565435d1606761718352be666f15')),
 }
 
 def hardcoded_trampoline_nodes():
@@ -112,9 +107,9 @@ def create_trampoline_route(
         params = TRAMPOLINE_FEES[trampoline_fee_level]
     else:
         raise NoPathFound()
-    # temporary fix: until ACINQ uses a proper feature bit to detect
+    # temporary fix: until eclair uses a proper feature bit to detect
     # Phoenix, they might try to open channels when payments fail
-    if trampoline_node_id == TRAMPOLINE_NODES_MAINNET['grspay'].pubkey:
+    if trampoline_node_id == TRAMPOLINE_NODES_MAINNET['eclair'].pubkey:
         is_legacy = True
         use_two_trampolines = False
     # add optional second trampoline
