dce24ffea6fb720259b79b8514890db8abbd5e45
gruve-p <jackielove4u@hotmail.com>
2021-06-15 10:46:38 +0200

    Updates after merging with upstream

--- contrib/android/build_docker_image.sh
+++ contrib/android/build_docker_image.sh
@@ -6,5 +6,5 @@ CONTRIB_ANDROID="$(dirname "$(readlink -e "$0")")"
 CONTRIB="$CONTRIB_ANDROID"/..
 
 cp "$CONTRIB/deterministic-build/requirements-build-android.txt" "$CONTRIB_ANDROID/requirements-build-android.txt"
-sudo docker build -t electrum-android-builder-img "$CONTRIB_ANDROID"
+sudo docker build -t electrum-grs-android-builder-img "$CONTRIB_ANDROID"
 rm "$CONTRIB_ANDROID/requirements-build-android.txt"
