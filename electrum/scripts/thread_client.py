ad894b62d1e6609bc074403bbbc3ae6f62afe3b3
captain <aP7UUAN>
2021-06-18 08:39:27 +0300

    add thread clients, batch client

--- electrum/scripts/thread_client.py
+++ electrum/scripts/thread_client.py
@@ -5,18 +5,12 @@ import time
 import asyncio
 import os
 import queue
-from asyncio.exceptions import InvalidStateError
 
 from typing import Callable, List, Any
-
-import ipdb
 
 from electrum.network import Network
 from electrum.util import print_msg
 from electrum.simple_config import SimpleConfig
-
-
-logging.basicConfig(format='%(asctime)-15s %(levelname)-8s %(message)s', level=logging.INFO)
 
 
 class ElectrumClient:
@@ -51,7 +45,8 @@ class ElectrumBatchClient(ElectrumClient):
             yield lst[i:i + n]
 
     async def get_balances(self, script_hashes: List[str], *args, **kwargs) -> dict:
-        self.logger.info(f"start get_balances in thread #{kwargs.get('thread_numb', 0)}, count={len(script_hashes)}, batch_lim={self.batch_limit}")
+        self.logger.info(
+            f"start get_balances in thread #{kwargs.get('thread_numb', 0)}, count={len(script_hashes)}, batch_lim={self.batch_limit}")
 
         result = {}
         count = 0
@@ -66,12 +61,13 @@ class ElectrumBatchClient(ElectrumClient):
         except Exception as e:
             self.logger.error(e)
         finally:
-            self.logger.warning("FINISH")
-            self.logger.info(f"stop get_balances in thread #{kwargs.get('thread_numb', 0)}. count of processed addresses is {count}")
+            self.logger.info(
+                f"stop get_balances in thread #{kwargs.get('thread_numb', 0)}. count of processed addresses is {count}")
             return result
 
     async def get_listunspents(self, script_hashes: List[str], *args, **kwargs) -> dict:
-        self.logger.info(f"start get_listunspents in thread #{kwargs.get('thread_numb', 0)}, count={len(script_hashes)}, batch_lim={self.batch_limit} ---- {script_hashes[0]}")
+        self.logger.info(
+            f"start get_listunspents in thread #{kwargs.get('thread_numb', 0)}, count={len(script_hashes)}, batch_lim={self.batch_limit} ---- {script_hashes[0]}")
         count = 0
         result = {}
         try:
@@ -84,12 +80,13 @@ class ElectrumBatchClient(ElectrumClient):
         except Exception as e:
             self.logger.error(e)
         finally:
-            self.logger.warning("FINISH kwargs.get('thread_number')")
-            self.logger.info(f"stop get_listunspents in thread #{kwargs.get('thread_numb', 0)}. count of processed addresses is {count},")
+            self.logger.info(
+                f"stop get_listunspents in thread #{kwargs.get('thread_numb', 0)}. count of processed addresses is {count},")
             return result
 
     async def get_listmempools(self, script_hashes: List[str], *args, **kwargs) -> dict:
-        self.logger.info(f"start listmempools_for_scripthashes in thread #{kwargs.get('thread_numb', 0)}, count={len(script_hashes)}, batch_lim={self.batch_limit} ---- {script_hashes[0]}")
+        self.logger.info(
+            f"start listmempools_for_scripthashes in thread #{kwargs.get('thread_numb', 0)}, count={len(script_hashes)}, batch_lim={self.batch_limit} ---- {script_hashes[0]}")
         count = 0
         result = {}
         try:
@@ -102,8 +99,8 @@ class ElectrumBatchClient(ElectrumClient):
         except Exception as e:
             self.logger.error(e)
         finally:
-            self.logger.warning("FINISH kwargs.get('thread_number')")
-            self.logger.info(f"stop listmempools_for_scripthashes in thread #{kwargs.get('thread_numb', 0)}. count of processed addresses is {count},")
+            self.logger.info(
+                f"stop listmempools_for_scripthashes in thread #{kwargs.get('thread_numb', 0)}. count of processed addresses is {count},")
             return result
 
 
@@ -139,7 +136,6 @@ class ElectrumThreadClient(ElectrumBatchClient):
         for t in self.threads:
             t.join()
 
-        self.logger.warning("FUCK")
         return self.result
 
     def __enter__(self):
@@ -147,4 +143,7 @@ class ElectrumThreadClient(ElectrumBatchClient):
         return self
 
     def __exit__(self, exc_type, exc_val, exc_tb):
-        asyncio.run(self.network.stop())
+        a = asyncio.run_coroutine_threadsafe(self.network.stop(), self.loop)
+        while not a.done():
+            time.sleep(1)
+        return
