55feacf16c11b34fcd683c26ee11af9422349411
Santiago Chiabotto <santiagochiabotto@gmail.com>
2021-08-17 01:37:05 -0300

    Add exchange ratio for ARS (#2326)
    * Add exchange ratio for ARS
    
    * Update electroncash/exchange_rate.py
    
    Co-authored-by: Calin Culianu <calin.culianu@gmail.com>
    
    * Update electroncash/exchange_rate.py
    
    Co-authored-by: Calin Culianu <calin.culianu@gmail.com>
    
    Co-authored-by: Calin Culianu <calin.culianu@gmail.com>

--- electroncash/exchange_rate.py
+++ electroncash/exchange_rate.py
@@ -261,6 +261,12 @@ class CoinGecko(ExchangeBase):
         return dict([(dt.utcfromtimestamp(h[0]/1000).strftime('%Y-%m-%d'), h[1])
                      for h in history['prices']])
 
+class BitstampYadio(ExchangeBase):
+    def get_rates(self, ccy):
+        json_usd = self.get_json('www.bitstamp.net', '/api/v2/ticker/bchusd')
+        json_ars = self.get_json('api.yadio.io', '/exrates/ARS')
+        return {'ARS': PyDecimal(json_usd['last']) / PyDecimal(json_ars['ARS']['USD'])}
+
 
 def dictinvert(d):
     inv = {}
