b1c4bb8914d6de3c5dccd3bd644ea3187ee6311e
JeremyRand <244188+JeremyRand@users.noreply.github.com>
2021-09-13 16:20:54 +0000

    Add Cirrus CI (#7431)
    * Cirrus: Add Tox task
    
    * Cirrus: Add Locale task
    
    * Cirrus: Add Flake8 Task
    
    * Cirrus: Add Regtest task
    
    * Regtest: Flush stdout
    
    Allows viewing output sooner.
    
    * Regtest: Read process.stdout in text mode
    
    Improves ability to quickly see output.
    
    * Cirrus: Add Windows task
    
    * Cirrus: Add Android task
    
    * Cirrus: Add macOS task
    
    * Cirrus: Add AppImage task
    
    * Cirrus: Add tarball task
    
    * Cirrus: Add Submodules task
    
    * Android: remove superfluous cp/rm
    
    * Add .dockerignore
    
    Symlink to .gitignore.

--- electrum/tests/regtest.py
+++ electrum/tests/regtest.py
@@ -7,9 +7,10 @@ class TestLightning(unittest.TestCase):
 
     @staticmethod
     def run_shell(args, timeout=30):
-        process = subprocess.Popen(['electrum/tests/regtest/regtest.sh'] + args, stderr=subprocess.STDOUT, stdout=subprocess.PIPE)
-        for line in iter(process.stdout.readline, b''):
-            sys.stdout.write(line.decode(sys.stdout.encoding))
+        process = subprocess.Popen(['electrum/tests/regtest/regtest.sh'] + args, stderr=subprocess.STDOUT, stdout=subprocess.PIPE, universal_newlines=True)
+        for line in iter(process.stdout.readline, ''):
+            sys.stdout.write(line)
+            sys.stdout.flush()
         process.wait(timeout=timeout)
         process.stdout.close()
         assert process.returncode == 0
