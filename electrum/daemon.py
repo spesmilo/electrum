3eab9f33b38f9a732a76434c92c5df620e8211be
coval3nte <65908512+coval3nte@users.noreply.github.com>
2022-03-02 11:57:14 +0100

    fix rpcsock

--- electrum/daemon.py
+++ electrum/daemon.py
@@ -65,11 +65,11 @@ class DaemonNotRunning(Exception):
 def get_rpcsock_defaultpath(config: SimpleConfig):
     return os.path.join(config.path, 'daemon_rpc_socket')
 
-def get_rpcsock_default_type():
+def get_rpcsock_default_type(config: SimpleConfig):
     # Use unix domain sockets when available,
     # with the extra paranoia that in case windows "implements" them,
     # we want to test it before making it the default there.
-    if hasattr(socket, 'AF_UNIX') and sys.platform != 'win32':
+    if not config.get('rpchost') and hasattr(socket, 'AF_UNIX') and sys.platform != 'win32':
         return 'unix'
     return 'tcp'
 
@@ -251,7 +251,7 @@ class CommandsServer(AuthenticatedServer):
         self.fd = fd
         self.config = daemon.config
         sockettype = self.config.get('rpcsock', 'auto')
-        self.socktype = sockettype if sockettype != 'auto' else get_rpcsock_default_type()
+        self.socktype = sockettype if sockettype != 'auto' else get_rpcsock_default_type(self.config)
         self.sockpath = self.config.get('rpcsockpath', get_rpcsock_defaultpath(self.config))
         self.host = self.config.get('rpchost', '127.0.0.1')
         self.port = self.config.get('rpcport', 0)
