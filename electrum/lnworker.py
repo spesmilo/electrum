spesmilo/electrum

commit ba018c707f6639edb4a18d178a60cdef804934b5

Qt: add bolt11_fallback and bip21_lightning options to preferences

electrum/wallet.py
@@ -2348,7 +2348,8 @@ class Abstract_Wallet(AddressSynchronizer, ABC):
         amount_sat = amount_sat or 0
         exp_delay = exp_delay or 0
         timestamp = int(time.time())
-        lightning_invoice = self.lnworker.add_request(amount_sat, message, exp_delay) if lightning else None
+        fallback_address = address if self.config.get('bolt11_fallback', True) else None
+        lightning_invoice = self.lnworker.add_request(amount_sat, message, exp_delay, fallback_address) if lightning else None
         outputs = [ PartialTxOutput.from_address_and_value(address, amount_sat)] if address else []
         height = self.get_local_height()
         req = Invoice(

electrum/gui/qt/main_window.py
@@ -1268,11 +1268,11 @@ class ElectrumWindow(QMainWindow, MessageBoxMixin, Logger):
 
     def show_receive_request(self, req):
         addr = req.get_address() or ''
-        URI = req.get_bip21_URI() if addr else ''
-        lnaddr = req.lightning_invoice or ''
         can_receive_lightning = self.wallet.lnworker and req.get_amount_sat() <= self.wallet.lnworker.num_sats_can_receive()
-        if not can_receive_lightning:
-            lnaddr = ''
+        lnaddr = req.lightning_invoice if can_receive_lightning else None
+        bip21_lightning = lnaddr if self.config.get('bip21_lightning', False) else None
+        URI = req.get_bip21_URI(lightning=bip21_lightning)
+        lnaddr = lnaddr or ''
         icon_name = "lightning.png" if can_receive_lightning else "lightning_disconnected.png"
         self.receive_tabs.setTabIcon(2, read_QIcon(icon_name))
         # encode lightning invoices as uppercase so QR encoding can use
@@ -1673,7 +1673,7 @@ class ElectrumWindow(QMainWindow, MessageBoxMixin, Logger):
             choices = {}
             if can_pay_onchain:
                 msg = ''.join([
-                    _('Pay this invoice onchain'), '\n',
+                    _('Pay onchain'), '\n',
                     _('Funds will be sent to the invoice fallback address.')
                 ])
                 choices[0] = msg
@@ -1694,7 +1694,6 @@ class ElectrumWindow(QMainWindow, MessageBoxMixin, Logger):
             msg = _('You cannot pay that invoice using Lightning.')
             if self.wallet.lnworker.channels:
                 msg += '\n' + _('Your channels can send {}.').format(self.format_amount(num_sats_can_send) + self.base_unit())
-
             r = self.query_choice(msg, choices)
             if r is not None:
                 self.save_pending_invoice()
@@ -2162,6 +2161,10 @@ class ElectrumWindow(QMainWindow, MessageBoxMixin, Logger):
         amount = out.get('amount')
         label = out.get('label')
         message = out.get('message')
+        lightning = out.get('lightning')
+        if lightning:
+            self.set_ln_invoice(lightning)
+            return
         # use label as description (not BIP21 compliant)
         if label and not message:
             message = label

electrum/gui/qt/settings_dialog.py
@@ -97,6 +97,21 @@ class SettingsDialog(WindowModalDialog):
                 self.window.refresh_tabs()
         nz.valueChanged.connect(on_nz)
 
+        # invoices
+        bolt11_fallback_cb = QCheckBox(_('Add on-chain fallback to lightning invoices'))
+        bolt11_fallback_cb.setChecked(bool(self.config.get('bolt11_fallback', True)))
+        bolt11_fallback_cb.setToolTip(_('Add fallback addresses to BOLT11 lightning invoices.'))
+        def on_bolt11_fallback(x):
+            self.config.set_key('bolt11_fallback', bool(x))
+        bolt11_fallback_cb.stateChanged.connect(on_bolt11_fallback)
+
+        bip21_lightning_cb = QCheckBox(_('Add lightning invoice to bitcoin URIs'))
+        bip21_lightning_cb.setChecked(bool(self.config.get('bip21_lightning', False)))
+        bip21_lightning_cb.setToolTip(_('This may create larger qr codes.'))
+        def on_bip21_lightning(x):
+            self.config.set_key('bip21_lightning', bool(x))
+        bip21_lightning_cb.stateChanged.connect(on_bip21_lightning)
+
         use_rbf = bool(self.config.get('use_rbf', True))
         use_rbf_cb = QCheckBox(_('Use Replace-By-Fee'))
         use_rbf_cb.setChecked(use_rbf)
@@ -483,6 +498,9 @@ class SettingsDialog(WindowModalDialog):
         gui_widgets.append((nz_label, nz))
         gui_widgets.append((msat_cb, None))
         gui_widgets.append((thousandsep_cb, None))
+        invoices_widgets = []
+        invoices_widgets.append((bolt11_fallback_cb, None))
+        invoices_widgets.append((bip21_lightning_cb, None))
         tx_widgets = []
         tx_widgets.append((usechange_cb, None))
         tx_widgets.append((use_rbf_cb, None))
@@ -513,6 +531,7 @@ class SettingsDialog(WindowModalDialog):
 
         tabs_info = [
             (gui_widgets, _('Appearance')),
+            (invoices_widgets, _('Invoices')),
             (tx_widgets, _('Transactions')),
             (lightning_widgets, _('Lightning')),
             (fiat_widgets, _('Fiat')),

electrum/invoices.py
@@ -157,12 +157,21 @@ class Invoice(StoredObject):
             return None
         return int(amount_msat / 1000)
 
-    def get_bip21_URI(self):
+    def get_bip21_URI(self, lightning=None):
         from electrum.util import create_bip21_uri
         addr = self.get_address()
         amount = int(self.get_amount_sat())
         message = self.message
-        uri = create_bip21_uri(addr, amount, message)
+        extra = {}
+        if self.time and self.exp:
+            extra['time'] = str(self.time)
+            extra['exp'] = str(self.exp)
+        # only if we can receive
+        if lightning:
+            extra['lightning'] = lightning
+        if not addr and lightning:
+            return "bitcoin:?lightning="+lightning
+        uri = create_bip21_uri(addr, amount, message, extra_query_params=extra)
         return str(uri)
 
     @lightning_invoice.validator

electrum/util.py
@@ -942,6 +942,7 @@ def parse_URI(uri: str, on_pr: Callable = None, *, loop=None) -> dict:
     """Raises InvalidBitcoinURI on malformed URI."""
     from . import bitcoin
     from .bitcoin import COIN, TOTAL_COIN_SUPPLY_LIMIT_IN_BTC
+    from .lnaddr import lndecode
 
     if not isinstance(uri, str):
         raise InvalidBitcoinURI(f"expected string, not {repr(uri)}")
@@ -1004,6 +1005,17 @@ def parse_URI(uri: str, on_pr: Callable = None, *, loop=None) -> dict:
             out['sig'] = bh2u(bitcoin.base_decode(out['sig'], base=58))
         except Exception as e:
             raise InvalidBitcoinURI(f"failed to parse 'sig' field: {repr(e)}") from e
+    if 'lightning' in out:
+        try:
+            lnaddr = lndecode(out['lightning'])
+            amount_sat = out.get('amount')
+            if amount:
+                assert int(lnaddr.get_amount_sat()) == amount_sat
+            address = out.get('address')
+            if address:
+                assert lnaddr.get_fallback_address() == address
+        except Exception as e:
+            raise InvalidBitcoinURI(f"Inconsistent lightning field: {repr(e)}") from e
 
     r = out.get('r')
     sig = out.get('sig')

commit 9fe93524b770051da85023f104fc9186621efdb0

Index lightning requests with rhash instead of onchain address.
get_unused_addresses() has been broken since #7730, because
addresses are considered as permanently used if they are in
the list of keys of receive_requests. This is true even if
an address is used as fallback for a lightning payment. This
means that the number of lightning payments we can receive
is constrained by the gap limit.

If a payment succeeds off-chain, we want to be able to reuse
its fallback address in other requests (this does not reduce
privacy, because invoices already share the same public key).

This implies that we should not use the onchain address as key
for lightning-enabled requests in wallet.receive_requests. If
we did, paid invoices would be overwritten when the address is
reused. That is the reason for the wallet_db upgrade.

Related: a3faf85e3cce83f90d5a3af8e0fd48f06ce0cabd

electrum/lnworker.py
@@ -1919,10 +1919,10 @@ class LNWallet(LNWorker):
         if self.get_payment_status(payment_hash) == status:
             return
         self.set_payment_status(payment_hash, status)
-        req = self.wallet.get_request_by_rhash(payment_hash.hex())
+        key = payment_hash.hex()
+        req = self.wallet.get_request(key)
         if req is None:
             return
-        key = self.wallet.get_key_for_receive_request(req)
         util.trigger_callback('request_status', self.wallet, key, status)
 
     def set_payment_status(self, payment_hash: bytes, status: int) -> None:

electrum/wallet.py
@@ -968,7 +968,7 @@ class Abstract_Wallet(ABC, Logger):
 
     def clear_requests(self):
         self.receive_requests.clear()
-        self._requests_rhash_to_key.clear()
+        self._requests_addr_to_rhash.clear()
         self.save_db()
 
     def get_invoices(self):
@@ -1020,10 +1020,10 @@ class Abstract_Wallet(ABC, Logger):
         return invoices
 
     def _init_requests_rhash_index(self):
-        self._requests_rhash_to_key = {}
+        self._requests_addr_to_rhash = {}
         for key, req in self.receive_requests.items():
-            if req.is_lightning():
-                self._requests_rhash_to_key[req.rhash] = key
+            if req.is_lightning() and (addr:=req.get_address()):
+                self._requests_addr_to_rhash[addr] = req.rhash
 
     def _prepare_onchain_invoice_paid_detection(self):
         # scriptpubkey -> list(invoice_keys)
@@ -1335,7 +1335,7 @@ class Abstract_Wallet(ABC, Logger):
         return ''
 
     def _get_default_label_for_rhash(self, rhash: str) -> str:
-        req = self.get_request_by_rhash(rhash)
+        req = self.get_request(rhash)
         return req.message if req else ''
 
     def get_label_for_rhash(self, rhash: str) -> str:
@@ -2219,9 +2219,7 @@ class Abstract_Wallet(ABC, Logger):
 
     def get_unused_addresses(self) -> Sequence[str]:
         domain = self.get_receiving_addresses()
-        # TODO we should index receive_requests by id
-        # add lightning requests. (use as key)
-        in_use_by_request = set(self.receive_requests.keys())
+        in_use_by_request = set(req.get_address() for req in self.get_unpaid_requests())
         return [addr for addr in domain if not self.adb.is_used(addr)
                 and addr not in in_use_by_request]
 
@@ -2342,11 +2340,12 @@ class Abstract_Wallet(ABC, Logger):
         return self.check_expired_status(r, status)
 
     def get_request(self, key):
-        return self.receive_requests.get(key) or self.get_request_by_rhash(key)
+        return self.receive_requests.get(key) or self.get_request_by_address(key)
 
-    def get_request_by_rhash(self, rhash):
-        key = self._requests_rhash_to_key.get(rhash)
-        return self.receive_requests.get(key) if key else None
+    def get_request_by_address(self, addr):
+        rhash = self._requests_addr_to_rhash.get(addr)
+        if rhash:
+            return self.receive_requests.get(rhash)
 
     def get_formatted_request(self, key):
         x = self.get_request(key)
@@ -2484,16 +2483,15 @@ class Abstract_Wallet(ABC, Logger):
                     raise Exception(_('Address not in wallet.'))
             key = addr
         else:
-            addr = req.get_address()
-            key = req.rhash if addr is None else addr
+            key = req.rhash
         return key
 
     def add_payment_request(self, req: Invoice, *, write_to_disk: bool = True):
         key = self.get_key_for_receive_request(req, sanity_checks=True)
         message = req.message
         self.receive_requests[key] = req
-        if req.is_lightning():
-            self._requests_rhash_to_key[req.rhash] = key
+        if req.is_lightning() and (addr:=req.get_address()):
+            self._requests_addr_to_rhash[addr] = req.rhash
         if write_to_disk:
             self.save_db()
         return key
@@ -2503,8 +2501,8 @@ class Abstract_Wallet(ABC, Logger):
         req = self.receive_requests.pop(key, None)
         if req is None:
             return
-        if req.is_lightning():
-            self._requests_rhash_to_key.pop(req.rhash)
+        if req.is_lightning() and (addr:=req.get_address()):
+            self._requests_addr_to_rhash.pop(addr)
         if req.is_lightning() and self.lnworker:
             self.lnworker.delete_payment_info(req.rhash)
         self.save_db()

electrum/wallet_db.py
@@ -52,7 +52,7 @@ if TYPE_CHECKING:
 
 OLD_SEED_VERSION = 4        # electrum versions < 2.0
 NEW_SEED_VERSION = 11       # electrum versions >= 2.0
-FINAL_SEED_VERSION = 46     # electrum >= 2.7 will set this to prevent
+FINAL_SEED_VERSION = 47     # electrum >= 2.7 will set this to prevent
                             # old versions from overwriting new format
 
 
@@ -195,6 +195,7 @@ class WalletDB(JsonDB):
         self._convert_version_44()
         self._convert_version_45()
         self._convert_version_46()
+        self._convert_version_47()
         self.put('seed_version', FINAL_SEED_VERSION)  # just to be sure
 
         self._after_upgrade_tasks()
@@ -924,6 +925,22 @@ class WalletDB(JsonDB):
                 invoices[newkey] = item
                 del invoices[key]
         self.data['seed_version'] = 46
+
+    def _convert_version_47(self):
+        from .lnaddr import lndecode
+        if not self._is_upgrade_method_needed(46, 46):
+            return
+        # recalc keys of requests
+        requests = self.data.get('payment_requests', {})
+        for key, item in list(requests.items()):
+            lnaddr = item.get('lightning_invoice')
+            if lnaddr:
+                lnaddr = lndecode(lnaddr)
+                rhash = lnaddr.paymenthash.hex()
+                if key != rhash:
+                    requests[rhash] = item
+                    del requests[key]
+        self.data['seed_version'] = 47
 
     def _convert_imported(self):
         if not self._is_upgrade_method_needed(0, 13):

electrum/tests/test_lnpeer.py
@@ -105,7 +105,7 @@ class MockWallet:
     receive_requests = {}
     adb = MockADB()
 
-    def get_request_by_rhash(self, rhash):
+    def get_request(self, key):
         pass
 
     def get_key_for_receive_request(self, x):