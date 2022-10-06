28b5103d93f417c9de7e4691427375efeccc4404
Sander van Grieken <sander@outrightsolutions.nl>
2022-10-06 09:20:02 +0200

    remove qml_test plugin, remove plugin settings test

--- electrum/plugins/qml_test/__init__.py
+++
@@ -1,5 +0,0 @@
-from electrum.i18n import _
-
-fullname = 'QML Plugin Test'
-description = '%s\n%s' % (_("Plugin to test QML integration from plugins."), _("Note: Used for development"))
-available_for = ['qml']
