46ca3b16c563d59159d9d79af43bb9acec009e45
gruve-p <groestlcoin@gmail.com>
2022-11-06 17:17:37 +0100

    Fix windows build

--- contrib/build-wine/build-electrum-git.sh
+++ contrib/build-wine/build-electrum-git.sh
@@ -34,8 +34,7 @@ export MULTIDICT_NO_EXTENSIONS=1
 export FROZENLIST_NO_EXTENSIONS=1
 
 info "Installing requirements..."
-# TODO use "--no-binary :all:" (but we don't have a C compiler...)
-$WINE_PYTHON -m pip install --no-build-isolation --no-dependencies --no-binary :all: --no-warn-script-location \
+$WINE_PYTHON -m pip install --no-build-isolation --no-dependencies --no-binary :all: --only-binary groestlcoin_hash --no-warn-script-location \
     --cache-dir "$WINE_PIP_CACHE_DIR" -r "$CONTRIB"/deterministic-build/requirements.txt
 info "Installing dependencies specific to binaries..."
 # TODO use "--no-binary :all:" (but we don't have a C compiler...)
