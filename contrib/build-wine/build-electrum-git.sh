7efe34c1775985841edca32002ddc3f49460c6a8
Wellenreiter <wellenreiter01@t-online.de>
2022-11-14 11:33:27 +0100

    adaption to neoscrypt 1

--- contrib/build-wine/build-electrum-git.sh
+++ contrib/build-wine/build-electrum-git.sh
@@ -38,7 +38,6 @@ export FROZENLIST_NO_EXTENSIONS=1
 
 info "Installing requirements..."
 $WINE_PYTHON -m pip install --no-build-isolation --no-dependencies --no-binary :all: --no-warn-script-location \
-
     --cache-dir "$WINE_PIP_CACHE_DIR" -r "$CONTRIB"/deterministic-build/requirements.txt
 info "Installing dependencies specific to binaries..."
 # TODO use "--no-binary :all:" (but we don't have a C compiler...)
