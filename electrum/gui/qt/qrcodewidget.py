spesmilo/electrum

commit 2a31f80d0962197780a2f45c279c83236051e4de

QRCodeWidget: add margin of 1 block, remove fixedSize parameter

electrum/gui/qt/qrcodewidget.py
@@ -16,14 +16,10 @@ from .util import WindowModalDialog, WWLabel, getSaveFileName
 
 class QRCodeWidget(QWidget):
 
-    def __init__(self, data = None, fixedSize=False):
+    def __init__(self, data = None):
         QWidget.__init__(self)
         self.data = None
         self.qr = None
-        self.margin = 0
-        self.fixedSize=fixedSize
-        if fixedSize:
-            self.setFixedSize(fixedSize, fixedSize)
         self.setData(data)
 
 
@@ -37,9 +33,6 @@ class QRCodeWidget(QWidget):
                 border=0,
             )
             self.qr.add_data(self.data)
-            if not self.fixedSize:
-                k = len(self.qr.get_matrix())
-                self.setMinimumSize(k*5,k*5)
         else:
             self.qr = None
 
@@ -71,7 +64,7 @@ class QRCodeWidget(QWidget):
         qp.begin(self)
         r = qp.viewport()
         framesize = min(r.width(), r.height())
-        boxsize = int((framesize - 2*self.margin)/k)
+        boxsize = int(framesize/(k + 2))
         if boxsize < 2:
             qp.drawText(0, 20, 'Cannot draw QR code:')
             qp.drawText(0, 40, 'Boxsize too small')
@@ -115,6 +108,7 @@ class QRDialog(WindowModalDialog):
         vbox = QVBoxLayout()
 
         qrw = QRCodeWidget(data)
+        qrw.setMinimumSize(250, 250)
         qr_hbox = QHBoxLayout()
         qr_hbox.addWidget(qrw)
         qr_hbox.addStretch(1)

electrum/gui/qt/main_window.py
@@ -1222,9 +1222,12 @@ class ElectrumWindow(QMainWindow, MessageBoxMixin, Logger):
         for w in [self.receive_address_help, self.receive_lightning_help]:
             w.setFixedSize(fixedSize, fixedSize)
 
-        self.receive_address_qr = QRCodeWidget(fixedSize=fixedSize)
-        self.receive_URI_qr = QRCodeWidget(fixedSize=fixedSize)
-        self.receive_lightning_qr = QRCodeWidget(fixedSize=fixedSize)
+        self.receive_address_qr = QRCodeWidget()
+        self.receive_URI_qr = QRCodeWidget()
+        self.receive_lightning_qr = QRCodeWidget()
+
+        for w in [self.receive_address_qr, self.receive_URI_qr, self.receive_lightning_qr]:
+            w.setFixedSize(fixedSize, fixedSize)
 
         self.receive_lightning_e.textChanged.connect(self.update_receive_widgets)
 

commit 95a8df69b4d08fb0571586aac6ccfc00e2092996

qt wizard: (fix) during 2fa wallet creation, shared QR to be drawn
This was a regression from 2a31f80d0962197780a2f45c279c83236051e4de,
before which, when using the default `QRCodeWidget()` constructor,
there had been a min size set on the widget. I like that old behaviour:
reasonable size should be set by default, and if you want to set the
size manually, opt-in to that.

fixes https://github.com/spesmilo/electrum/issues/8071

electrum/gui/qt/qrcodewidget.py
@@ -18,13 +18,13 @@ from .util import WindowModalDialog, WWLabel, getSaveFileName
 
 class QRCodeWidget(QWidget):
 
-    def __init__(self, data = None):
+    def __init__(self, data=None, *, manual_size: bool = False):
         QWidget.__init__(self)
         self.data = None
         self.qr = None
         self._framesize = None  # type: Optional[int]
+        self._manual_size = manual_size
         self.setData(data)
-
 
     def setData(self, data):
         if self.data != data:
@@ -36,6 +36,9 @@ class QRCodeWidget(QWidget):
                 border=0,
             )
             self.qr.add_data(self.data)
+            if not self._manual_size:
+                k = len(self.qr.get_matrix())
+                self.setMinimumSize(k * 5, k * 5)
         else:
             self.qr = None
 
@@ -120,7 +123,7 @@ class QRDialog(WindowModalDialog):
 
         vbox = QVBoxLayout()
 
-        qrw = QRCodeWidget(data)
+        qrw = QRCodeWidget(data, manual_size=True)
         qrw.setMinimumSize(250, 250)
         vbox.addWidget(qrw, 1)
 

electrum/gui/qt/receive_tab.py
@@ -135,9 +135,9 @@ class ReceiveTab(QWidget, MessageBoxMixin, Logger):
         self.receive_lightning_help = FramedWidget()
         self.receive_lightning_help.setVisible(False)
         self.receive_lightning_help.setLayout(vbox)
-        self.receive_address_qr = QRCodeWidget()
-        self.receive_URI_qr = QRCodeWidget()
-        self.receive_lightning_qr = QRCodeWidget()
+        self.receive_address_qr = QRCodeWidget(manual_size=True)
+        self.receive_URI_qr = QRCodeWidget(manual_size=True)
+        self.receive_lightning_qr = QRCodeWidget(manual_size=True)
 
         for e in [self.receive_address_e, self.receive_URI_e, self.receive_lightning_e]:
             e.setFont(QFont(MONOSPACE_FONT))