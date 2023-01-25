e5dffd742724638c3920fe1e07075f65a047360e
/dev/fd0 <alicexbt@protonmail.com>
2023-01-25 17:49:14 +0530

    Check UTXOs and raise exception if none are larger than deniability budget

--- electrum/plugins/deniability/deniability.py
+++ electrum/plugins/deniability/deniability.py
@@ -1,7 +1,19 @@
 #!/usr/bin/python
 
-from electrum.plugin import BasePlugin
+from electrum.plugin import hook, BasePlugin
 
 class Deniability(BasePlugin):
     def __init__(self, parent, config, name):
         BasePlugin.__init__(self, parent, config, name)
+
+    @hook
+    def load_wallet(self, wallet, window):
+        budget = float(self.config.get('deniability_budget', 0))
+
+        self.wallet = window.wallet
+        utxos = []
+        for utxo in self.wallet.get_utxos():
+            utxos.append(float(self.config.format_amount(utxo.value_sats())))
+
+        if all(x < budget for x in utxos):
+            raise Exception("Error: None of the UTXOs are greater than the deniability budget.")
