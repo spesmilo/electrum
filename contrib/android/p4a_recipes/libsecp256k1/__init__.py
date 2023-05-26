c049b461bbd26c04c77448a01f506ca66257cbe1
SomberNight <somber.night@protonmail.com>
2023-05-26 13:22:35 +0000

    bump libsecp256k1 version

--- contrib/android/p4a_recipes/libsecp256k1/__init__.py
+++ contrib/android/p4a_recipes/libsecp256k1/__init__.py
@@ -6,9 +6,9 @@ assert LibSecp256k1Recipe.python_depends == []
 
 
 class LibSecp256k1RecipePinned(LibSecp256k1Recipe):
-    version = "346a053d4c442e08191f075c3932d03140579d47"
+    version = "acf5c55ae6a94e5ca847e07def40427547876101"
     url = "https://github.com/bitcoin-core/secp256k1/archive/{version}.zip"
-    sha512sum = "d6232bd8fb29395984b15633bee582e7588ade0ec1c7bea5b2cab766b1ff657672b804e078656e0ce4067071140b0552d12ce3c01866231b212f3c65908b85aa"
+    sha512sum = "6639f239de3c7abc1906088f2b0bf833b3c7b073bc25151fa908a64b5585dce59a073ed4eb0c0c3360c785a639ca4fce897e0288b94bbfa7f1d07f7ab610f1d6"
 
 
 recipe = LibSecp256k1RecipePinned()
