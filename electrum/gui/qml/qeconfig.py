5b4df75918bf4a894fdd8dcb2fccb560a4084682
SomberNight <somber.night@protonmail.com>
2023-06-23 16:23:12 +0000

    qml: fix lnurl-pay when config.BTC_AMOUNTS_ADD_THOUSANDS_SEP is True
    when paying an lnurl-pay that provides an amount interval,
    the amount field is editable by the user and it expects no spaces

--- electrum/gui/qml/qeconfig.py
+++ electrum/gui/qml/qeconfig.py
@@ -223,6 +223,15 @@ class QEConfig(AuthMixin, QObject):
             self.config.set_key('user_knows_press_and_hold', userKnowsPressAndHold)
             self.userKnowsPressAndHoldChanged.emit()
 
+    @pyqtSlot('qint64', result=str)
+    @pyqtSlot(QEAmount, result=str)
+    def formatSatsForEditing(self, satoshis):
+        if isinstance(satoshis, QEAmount):
+            satoshis = satoshis.satsInt
+        return self.config.format_amount(
+            satoshis,
+            add_thousands_sep=False,
+        )
 
     @pyqtSlot('qint64', result=str)
     @pyqtSlot('qint64', bool, result=str)
