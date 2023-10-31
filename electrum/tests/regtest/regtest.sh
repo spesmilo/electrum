fca2d55c6421d113ad10d1f69bb13f8d26011323
robertmin2 <129261636+robertmin2@users.noreply.github.com>
2023-10-31 18:31:53 +0100

    Update regtest.sh

--- electrum/tests/regtest/regtest.sh
+++ electrum/tests/regtest/regtest.sh
@@ -446,7 +446,7 @@ if [[ $1 == "unixsockets" ]]; then
     ./run_electrum --regtest daemon -d --rpcsock=unix # Start daemon with unix domain socket
     ./run_electrum --regtest stop # Errors if it can't connect
     # Test custom socket path
-    f=$(mktemp --dry-run)
+    f=$(mktemp -u /tmp/electrum_test.XXXX)
     ./run_electrum --regtest daemon -d --rpcsock=unix --rpcsockpath=$f
     [ -S $f ] # filename exists and is socket
     ./run_electrum --regtest stop
