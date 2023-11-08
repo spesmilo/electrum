98264f64adb6a4cc20e78227a67ca028be100f23
Sander van Grieken <sander@outrightsolutions.nl>
2023-11-08 14:04:22 +0100

    qml: introduce PasswordStrengthIndicator control, and add to PasswordDialog and WCWalletPassword

--- electrum/gui/qml/util.py
+++ electrum/gui/qml/util.py
@@ -1,18 +1,20 @@
+import math
+import re
 import sys
 import queue
 
 from functools import wraps
 from time import time
-from typing import Callable, Optional, NamedTuple
+from typing import Callable, Optional, NamedTuple, Tuple
 
 from PyQt6.QtCore import pyqtSignal, QThread
 
+from electrum.i18n import _
 from electrum.logging import Logger
 from electrum.util import EventListener, event_listener
 
 
 class QtEventListener(EventListener):
-
     qt_callback_signal = pyqtSignal(tuple)
 
     def register_callbacks(self):
@@ -56,6 +58,21 @@ def status_update_timer_interval(exp):
         interval = 1000 * 60 * 60
 
     return interval
+
+
+# TODO: copied from qt password_dialog.py, move to common code
+def check_password_strength(password: str) -> Tuple[int, str]:
+    """Check the strength of the password entered by the user and return back the same
+    :param password: password entered by user in New Password
+    :return: password strength Weak or Medium or Strong"""
+    password = password
+    n = math.log(len(set(password)))
+    num = re.search("[0-9]", password) is not None and re.match("^[0-9]*$", password) is None
+    caps = password != password.upper() and password != password.lower()
+    extra = re.match("^[a-zA-Z0-9]*$", password) is None
+    score = len(password)*(n + caps + num + extra)/20
+    password_strength = {0: _('Weak'), 1: _('Medium'), 2: _('Strong'), 3: _('Very Strong')}
+    return min(3, int(score)), password_strength[min(3, int(score))]
 
 
 # TODO: copied from desktop client, this could be moved to a set of common code.
