6b8f444775f5c82fd83b880db6a6ffe5ce9ceef7
gruve-p <jackielove4u@hotmail.com>
2023-12-03 21:05:02 +0100

    fix

--- contrib/osx/make_osx.sh
+++ contrib/osx/make_osx.sh
@@ -225,7 +225,7 @@ python3 -m pip install --no-build-isolation --no-dependencies \
     --no-warn-script-location . > /dev/null || fail "Could not build $PACKAGE"
 # pyinstaller needs to be able to "import electrum", for which we need libsecp256k1:
 # (or could try "pip install -e" instead)
-cp "$PROJECT_ROOT/electrum_grs"/libsecp256k1.*.dylib "$VENV_DIR/lib/python$PY_VER_MAJOR/site-packages/electrum-grs/"
+cp "$PROJECT_ROOT/electrum_grs"/libsecp256k1.*.dylib "$VENV_DIR/lib/python$PY_VER_MAJOR/site-packages/electrum_grs/"
 
 # strip debug symbols of some compiled libs
 # - hidapi (hid.cpython-39-darwin.so) in particular is not reproducible without this
