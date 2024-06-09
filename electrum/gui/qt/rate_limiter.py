8b6eb24b81ba6379b39cbb1283572737f79f58c8
longxiangqiao <longxiangqiao@qq.com>
2024-06-09 18:58:10 +0800

    chore: fix some comments
    Signed-off-by: longxiangqiao <longxiangqiao@qq.com>

--- electrum/gui/qt/rate_limiter.py
+++ electrum/gui/qt/rate_limiter.py
@@ -98,7 +98,7 @@ class RateLimiter(Logger):
                 self.timer.start(int(diff*1e3))
                 #self.logger.debug("deferring")
         else:
-            # We had a timer active, which means as future call will occur. So return early and let that call happenin the future.
+            # We had a timer active, which means as future call will occur. So return early and let that call happen in the future.
             # Note that a side-effect of this aborted invocation was to update self.saved_args.
             pass
             #self.logger.debug("ignoring (already scheduled)")
