2471efb1d76cb3446bb952dbefd65adef2fc654e
Sander van Grieken <sander@outrightsolutions.nl>
2024-10-10 10:05:38 +0200

    qml: show option for single server in ServerConfig

--- electrum/gui/qt/wizard/server_connect.py
+++ electrum/gui/qt/wizard/server_connect.py
@@ -109,3 +109,4 @@ class WCServerConfig(WizardComponent):
     def apply(self):
         self.wizard_data['autoconnect'] = self.sw.server_e.text().strip() == ''
         self.wizard_data['server'] = self.sw.server_e.text()
+        self.wizard_data['one_server'] = False
