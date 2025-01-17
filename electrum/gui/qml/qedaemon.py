spesmilo/electrum

commit ee42e09387b64ba8c5f40128d082ee1c0ac21b77

anchor channels: unlock wallet on startup if the wallet has channels

electrum/gui/qml/qedaemon.py
@@ -251,7 +251,8 @@ class QEDaemon(AuthMixin, QObject):
         assert wallet is not None
         self._current_wallet = QEWallet.getInstanceFor(wallet)
         self.availableWallets.updateWallet(self._path)
-        self._current_wallet.password = password if password else None
+        if wallet.requires_unlock():
+            wallet.unlock(password)
         self._loading = False
         self.loadingChanged.emit()
         self.walletLoaded.emit(self._name, self._path)

electrum/daemon.py
@@ -495,6 +495,8 @@ class Daemon(Logger):
         if wallet := self._wallets.get(wallet_key):
             return wallet
         wallet = self._load_wallet(path, password, upgrade=upgrade, config=self.config)
+        if wallet.requires_unlock():
+            wallet.unlock(password)
         wallet.start_network(self.network)
         self.add_wallet(wallet)
         self.update_recently_opened_wallets(path)

electrum/gui/qml/qewallet.py
@@ -731,11 +731,15 @@ class QEWallet(AuthMixin, QObject, QtEventListener):
         try:
             self._logger.info('setting new password')
             self.wallet.update_password(current_password, password, encrypt_storage=True)
-            self.password = password
+            self.wallet.unlock(password)
             return True
         except InvalidPassword as e:
             self._logger.exception(repr(e))
             return False
+
+    @property
+    def password(self):
+        return self.wallet.get_unlocked_password()
 
     @pyqtSlot(str)
     def importAddresses(self, addresslist):

electrum/lnworker.py
@@ -1291,6 +1291,8 @@ class LNWallet(LNWorker):
             zeroconf: bool = False,
             opening_fee: int = None,
             password=None):
+        if self.config.ENABLE_ANCHOR_CHANNELS:
+            self.wallet.unlock(password)
         coins = self.wallet.get_spendable_coins(None)
         node_id = peer.pubkey
         funding_tx = self.mktx_for_open_channel(

electrum/wallet.py
@@ -502,6 +502,12 @@ class Abstract_Wallet(ABC, Logger, EventListener):
     def has_lightning(self) -> bool:
         return bool(self.lnworker)
 
+    def has_channels(self):
+        return self.lnworker is not None and len(self.lnworker._channels) > 0
+
+    def requires_unlock(self):
+        return self.config.ENABLE_ANCHOR_CHANNELS and self.has_channels()
+
     def can_have_lightning(self) -> bool:
         # we want static_remotekey to be a wallet address
         return self.txin_type == 'p2wpkh'
@@ -2977,8 +2983,8 @@ class Abstract_Wallet(ABC, Logger, EventListener):
         if self.storage and self.storage.file_exists():
             self.db.write_and_force_consolidation()
         # if wallet was previously unlocked, update password in memory
-        if self._password_in_memory is not None:
-            self._password_in_memory = new_pw
+        if self.requires_unlock():
+            self.unlock(new_pw)
 
     @abstractmethod
     def _update_password_for_keystore(self, old_pw: Optional[str], new_pw: Optional[str]) -> None:

commit 010b153ab26676ae345cbeca0cddb48d09bc2f3c

qml: fix regression caused by ee42e09387b64ba8c5f40128d082ee1c0ac21b77
in qml, we need the password in-memory as the auth wrapper (@auth_protect) does not
pass the password to the wrapped fn.

electrum/gui/qml/qedaemon.py
@@ -251,8 +251,8 @@ class QEDaemon(AuthMixin, QObject):
         assert wallet is not None
         self._current_wallet = QEWallet.getInstanceFor(wallet)
         self.availableWallets.updateWallet(self._path)
-        if wallet.requires_unlock():
-            wallet.unlock(password or None)
+        wallet.unlock(password or None)  # not conditional on wallet.requires_unlock in qml, as
+        # the auth wrapper doesn't pass the entered password, but instead we rely on the password in memory
         self._loading = False
         self.loadingChanged.emit()
         self.walletLoaded.emit(self._name, self._path)