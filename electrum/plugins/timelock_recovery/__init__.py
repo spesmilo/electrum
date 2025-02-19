8b1d7bef1f26da8770b473e36bd5c606a0b2f4ed
Oren <orenz0@protonmail.com>
2025-02-19 16:20:30 +0200

    fix description

--- electrum/plugins/timelock_recovery/__init__.py
+++ electrum/plugins/timelock_recovery/__init__.py
@@ -2,8 +2,10 @@ from electrum.i18n import _
 
 fullname = _('Timelock Recovery Utility')
 description = ''.join(["<br/>",
-    "<b>"+_("Do you have something to hide ?")+"</b>", '<br/>', '<br/>',
-    _("This plug-in allows you to create a visually encrypted backup of your wallet seeds, or of custom alphanumeric secrets."), '<br/>'])
+    _("This plug-in allows you to create Timelock Recovery Plans for your wallet."),
+    "<br/>",
+    'See: <a href="https://timelockrecovery.com">timelockrecovery.com</a>',
+])
 available_for = ['qt']
 
 
