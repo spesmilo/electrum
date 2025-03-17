a9f80482513982de6e8b2785926963d4596d2428
f321x <f321x@tutamail.com>
2025-03-17 17:29:38 +0100

    use manifest.json instead of loading init file for plugin registration

--- electrum/plugins/digitalbitbox/__init__.py
+++
@@ -1,6 +0,0 @@
-from electrum.i18n import _
-
-fullname = 'Digital Bitbox'
-description = _('Provides support for Digital Bitbox hardware wallet')
-registers_keystore = ('hardware', 'digitalbitbox', _("Digital Bitbox wallet"))
-available_for = ['qt', 'cmdline']
--- electrum/plugins/revealer/__init__.py
+++ electrum/plugins/revealer/__init__.py
@@ -1,9 +1,2 @@
-from electrum.i18n import _
-
-fullname = _('Revealer Backup Utility')
-description = ''.join(["<br/>",
-    "<b>"+_("Do you have something to hide ?")+"</b>", '<br/>', '<br/>',
-    _("This plug-in allows you to create a visually encrypted backup of your wallet seeds, or of custom alphanumeric secrets."), '<br/>'])
-available_for = ['qt']
 
 
