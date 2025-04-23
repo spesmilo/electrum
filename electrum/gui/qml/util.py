c89e8f6f03cab90d10f200f55b8a0701677a6811
Sander van Grieken <sander@outrightsolutions.nl>
2025-04-23 15:24:02 +0200

    qt,qml: move TaskThread to common_qt

--- electrum/gui/qml/util.py
+++ electrum/gui/qml/util.py
@@ -1,16 +1,13 @@
 import math
 import re
-import sys
-import queue
 
 from functools import wraps
 from time import time
-from typing import Callable, Optional, NamedTuple, Tuple
+from typing import Tuple
 
-from PyQt6.QtCore import pyqtSignal, QThread
+from PyQt6.QtCore import pyqtSignal
 
 from electrum.i18n import _
-from electrum.logging import Logger
 from electrum.util import EventListener, event_listener
 
 
@@ -73,76 +70,3 @@ def check_password_strength(password: str) -> Tuple[int, str]:
     score = len(password)*(n + caps + num + extra)/20
     password_strength = {0: _('Weak'), 1: _('Medium'), 2: _('Strong'), 3: _('Very Strong')}
     return min(3, int(score)), password_strength[min(3, int(score))]
-
-
-# TODO: copied from desktop client, this could be moved to a set of common code.
-class TaskThread(QThread, Logger):
-    """Thread that runs background tasks.  Callbacks are guaranteed
-    to happen in the context of its parent."""
-
-    class Task(NamedTuple):
-        task: Callable
-        cb_success: Optional[Callable]
-        cb_done: Optional[Callable]
-        cb_error: Optional[Callable]
-        cancel: Optional[Callable] = None
-
-    doneSig = pyqtSignal(object, object, object)
-
-    def __init__(self, parent, on_error=None):
-        QThread.__init__(self, parent)
-        Logger.__init__(self)
-        self.on_error = on_error
-        self.tasks = queue.Queue()
-        self._cur_task = None  # type: Optional[TaskThread.Task]
-        self._stopping = False
-        self.doneSig.connect(self.on_done)
-        self.start()
-
-    def add(self, task, on_success=None, on_done=None, on_error=None, *, cancel=None):
-        if self._stopping:
-            self.logger.warning(f"stopping or already stopped but tried to add new task.")
-            return
-        on_error = on_error or self.on_error
-        task_ = TaskThread.Task(task, on_success, on_done, on_error, cancel=cancel)
-        self.tasks.put(task_)
-
-    def run(self):
-        while True:
-            if self._stopping:
-                break
-            task = self.tasks.get()  # type: TaskThread.Task
-            self._cur_task = task
-            if not task or self._stopping:
-                break
-            try:
-                result = task.task()
-                self.doneSig.emit(result, task.cb_done, task.cb_success)
-            except BaseException:
-                self.doneSig.emit(sys.exc_info(), task.cb_done, task.cb_error)
-
-    def on_done(self, result, cb_done, cb_result):
-        # This runs in the parent's thread.
-        if cb_done:
-            cb_done()
-        if cb_result:
-            cb_result(result)
-
-    def stop(self):
-        self._stopping = True
-        # try to cancel currently running task now.
-        # if the task does not implement "cancel", we will have to wait until it finishes.
-        task = self._cur_task
-        if task and task.cancel:
-            task.cancel()
-        # cancel the remaining tasks in the queue
-        while True:
-            try:
-                task = self.tasks.get_nowait()
-            except queue.Empty:
-                break
-            if task and task.cancel:
-                task.cancel()
-        self.tasks.put(None)  # in case the thread is still waiting on the queue
-        self.exit()
-        self.wait()
--- electrum/gui/qt/util.py
+++ electrum/gui/qt/util.py
@@ -12,7 +12,7 @@ from typing import (NamedTuple, Callable, Optional, TYPE_CHECKING, List, Any, Se
 from PyQt6 import QtCore
 from PyQt6.QtGui import (QFont, QColor, QCursor, QPixmap, QImage,
                          QPalette, QIcon, QFontMetrics, QPainter, QContextMenuEvent, QMovie)
-from PyQt6.QtCore import (Qt, pyqtSignal, QCoreApplication, QThread, QSize, QRect, QPoint, QObject)
+from PyQt6.QtCore import (Qt, pyqtSignal, QCoreApplication, QSize, QRect, QPoint, QObject)
 from PyQt6.QtWidgets import (QPushButton, QLabel, QMessageBox, QHBoxLayout, QVBoxLayout, QLineEdit,
                              QStyle, QDialog, QGroupBox, QButtonGroup, QRadioButton,
                              QFileDialog, QWidget, QToolButton, QPlainTextEdit, QApplication, QToolTip,
@@ -24,8 +24,9 @@ from electrum.util import (FileImportFailed, FileExportFailed, resource_path, Ev
                            get_logger, UserCancelled, UserFacingException)
 from electrum.invoices import (PR_UNPAID, PR_PAID, PR_EXPIRED, PR_INFLIGHT, PR_UNKNOWN, PR_FAILED, PR_ROUTING,
                                PR_UNCONFIRMED, PR_BROADCASTING, PR_BROADCAST)
-from electrum.logging import Logger
 from electrum.qrreader import MissingQrDetectionLib, QrCodeResult
+
+from electrum.gui.common_qt.util import TaskThread
 
 if TYPE_CHECKING:
     from .main_window import ElectrumWindow
@@ -1076,78 +1077,6 @@ class PasswordLineEdit(QLineEdit):
         # This is really just a best-effort thing...
         self.setText(len(self.text()) * " ")
         super().clear()
-
-
-class TaskThread(QThread, Logger):
-    '''Thread that runs background tasks.  Callbacks are guaranteed
-    to happen in the context of its parent.'''
-
-    class Task(NamedTuple):
-        task: Callable
-        cb_success: Optional[Callable]
-        cb_done: Optional[Callable]
-        cb_error: Optional[Callable]
-        cancel: Optional[Callable] = None
-
-    doneSig = pyqtSignal(object, object, object)
-
-    def __init__(self, parent, on_error=None):
-        QThread.__init__(self, parent)
-        Logger.__init__(self)
-        self.on_error = on_error
-        self.tasks = queue.Queue()
-        self._cur_task = None  # type: Optional[TaskThread.Task]
-        self._stopping = False
-        self.doneSig.connect(self.on_done)
-        self.start()
-
-    def add(self, task, on_success=None, on_done=None, on_error=None, *, cancel=None):
-        if self._stopping:
-            self.logger.warning(f"stopping or already stopped but tried to add new task.")
-            return
-        on_error = on_error or self.on_error
-        task_ = TaskThread.Task(task, on_success, on_done, on_error, cancel=cancel)
-        self.tasks.put(task_)
-
-    def run(self):
-        while True:
-            if self._stopping:
-                break
-            task = self.tasks.get()  # type: TaskThread.Task
-            self._cur_task = task
-            if not task or self._stopping:
-                break
-            try:
-                result = task.task()
-                self.doneSig.emit(result, task.cb_done, task.cb_success)
-            except BaseException:
-                self.doneSig.emit(sys.exc_info(), task.cb_done, task.cb_error)
-
-    def on_done(self, result, cb_done, cb_result):
-        # This runs in the parent's thread.
-        if cb_done:
-            cb_done()
-        if cb_result:
-            cb_result(result)
-
-    def stop(self):
-        self._stopping = True
-        # try to cancel currently running task now.
-        # if the task does not implement "cancel", we will have to wait until it finishes.
-        task = self._cur_task
-        if task and task.cancel:
-            task.cancel()
-        # cancel the remaining tasks in the queue
-        while True:
-            try:
-                task = self.tasks.get_nowait()
-            except queue.Empty:
-                break
-            if task and task.cancel:
-                task.cancel()
-        self.tasks.put(None)  # in case the thread is still waiting on the queue
-        self.exit()
-        self.wait()
 
 
 class ColorSchemeItem:
