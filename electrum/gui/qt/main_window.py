28d300ba4f0438303629644939c01ab65dd945f0
kratm2 <moreno.kratter@students.bfh.ch>
2025-06-03 12:24:21 +0200

    gui: main_window integration

--- electrum/gui/qt/main_window.py
+++ electrum/gui/qt/main_window.py
@@ -75,6 +75,7 @@ from electrum.lntransport import extract_nodeid, ConnStringFormatError
 from electrum.lnaddr import lndecode
 from electrum.submarine_swaps import SwapServerTransport, NostrTransport
 from electrum.fee_policy import FeePolicy
+from electrum.silent_payment import is_silent_payment_address
 
 from .rate_limiter import rate_limited
 from .exception_window import Exception_Hook
@@ -1432,6 +1433,11 @@ class ElectrumWindow(QMainWindow, MessageBoxMixin, Logger, QtEventListener):
         self.send_tab.broadcast_transaction(tx, invoice=invoice)
 
     @protected
+    def prompt_and_get_pw(self, password):
+        # Note: the caller is responsible to distinguish between: user cancelled and pw is None (both return None)
+        return password
+
+    @protected
     def sign_tx(
         self,
         tx: PartialTransaction,
@@ -1619,7 +1625,7 @@ class ElectrumWindow(QMainWindow, MessageBoxMixin, Logger, QtEventListener):
         self.send_tab.payto_contacts(labels)
 
     def set_contact(self, label, address):
-        if not (is_address(address) or is_valid_email(address)):  # email = lightning address
+        if not (is_address(address) or is_silent_payment_address(address) or is_valid_email(address)):  # email = lightning address
             self.show_error(_('Invalid Address'))
             self.contact_list.update()  # Displays original unchanged value
             return False
@@ -1651,6 +1657,7 @@ class ElectrumWindow(QMainWindow, MessageBoxMixin, Logger, QtEventListener):
             grid.addWidget(QLabel(_("Address") + ':'), 2, 0)
             grid.addWidget(QLabel(invoice.get_address()), 2, 1)
         else:
+            # Fixme: fails with multiline address and script (concat None)
             outputs_str = '\n'.join(map(lambda x: x.address + ' : ' + self.format_amount(x.value)+ self.base_unit(), invoice.outputs))
             grid.addWidget(QLabel(_("Outputs") + ':'), 2, 0)
             grid.addWidget(QLabel(outputs_str), 2, 1)
@@ -2941,6 +2948,9 @@ class ElectrumWindow(QMainWindow, MessageBoxMixin, Logger, QtEventListener):
             win.show_error(e)
             return False
         else:
+            for output in tx.outputs():
+                if output.is_silent_payment():
+                    self.wallet.save_silent_payment_address(output.address, output.sp_addr.encoded, write_to_disk=False)
             self.wallet.save_db()
             # need to update at least: history_list, utxo_list, address_list
             self.need_update.set()
