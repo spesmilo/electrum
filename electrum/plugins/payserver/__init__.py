b8761ca006d05fd5e2ad051530e6a214eba3d21c
ThomasV <thomasv@electrum.org>
2025-06-10 20:39:14 +0200

    payserver plugin: fix config var names

--- electrum/plugins/payserver/__init__.py
+++ electrum/plugins/payserver/__init__.py
@@ -1,5 +1,5 @@
 from electrum.simple_config import ConfigVar, SimpleConfig
 
-SimpleConfig.PAYSERVER_PORT = ConfigVar('payserver_port', default=8080, type_=int, plugin=__name__)
-SimpleConfig.PAYSERVER_ROOT = ConfigVar('payserver_root', default='/r', type_=str, plugin=__name__)
-SimpleConfig.PAYSERVER_ALLOW_CREATE_INVOICE = ConfigVar('payserver_allow_create_invoice', default=False, type_=bool, plugin=__name__)
+SimpleConfig.PAYSERVER_PORT = ConfigVar('plugins.payserver.port', default=8080, type_=int, plugin=__name__)
+SimpleConfig.PAYSERVER_ROOT = ConfigVar('plugins.payserver.root', default='/r', type_=str, plugin=__name__)
+SimpleConfig.PAYSERVER_ALLOW_CREATE_INVOICE = ConfigVar('plugins.payserver.allow_create_invoice', default=False, type_=bool, plugin=__name__)
