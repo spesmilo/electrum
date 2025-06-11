8398a8ae2ebef71e4105d502afe39c7271fd1a91
SomberNight <somber.night@protonmail.com>
2025-06-11 16:51:52 +0000

    mac build: don't compile C extensions for "propcache"
    makes build not reproducible
    
    random temporary paths leak into a compiled .so:
    dist_ghost43/Electrum.app/Contents/Resources/propcache/_helpers_c.cpython-312-darwin.so

--- contrib/osx/make_osx.sh
+++ contrib/osx/make_osx.sh
@@ -172,6 +172,7 @@ cp -f "$DLL_TARGET_DIR/libusb-1.0.dylib" "$PROJECT_ROOT/electrum/" || fail "Coul
 
 # opt out of compiling C extensions
 export YARL_NO_EXTENSIONS=1
+export PROPCACHE_NO_EXTENSIONS=1
 
 export ELECTRUM_ECC_DONT_COMPILE=1
 
