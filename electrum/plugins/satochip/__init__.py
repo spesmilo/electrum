424b6d9055a581d19bf3a5cb9ef4f680f0240d47
Toporin <satochip.wallet@gmail.com>
2025-06-20 11:36:44 +0200

    use manifest.json instead of loading init file for plugin registration

--- electrum/plugins/satochip/__init__.py
+++
@@ -1,6 +0,0 @@
-
-fullname = 'Satochip Wallet'
-description = 'Provides support for Satochip hardware wallet'
-requires = [('satochip', 'github.com/Toporin/pysatochip')]
-registers_keystore = ('hardware', 'satochip', "Satochip wallet")
-available_for = ['qt']
