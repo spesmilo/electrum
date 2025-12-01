fe5241b43019a31df0eb8e2addd5369176451a10
Sander van Grieken <sander@outrightsolutions.nl>
2025-12-01 15:06:05 +0100

    payment_identifier: properly lock amount once resolved to invoice, consistently use invoice_from_payment_identifier()

--- electrum/gui/qt/send_tab.py
+++ electrum/gui/qt/send_tab.py
@@ -588,9 +588,7 @@ class SendTab(QWidget, MessageBoxMixin, Logger):
         if pi.error:
             self.show_error(pi.error)
             return
-        invoice = pi.bolt11
-        if not invoice and pi.bolt12_offer:
-            invoice = Invoice.from_bolt12_invoice_tlv(pi.bolt12_invoice_tlv)
+        invoice = invoice_from_payment_identifier(pi, self.wallet)
         self.pending_invoice = invoice
         self.logger.debug(f'after finalize invoice: {invoice!r}')
         self.do_pay_invoice(invoice)
