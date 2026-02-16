c44d060321be6e752d43a259a020840409ec979f
Sander van Grieken <sander@outrightsolutions.nl>
2026-02-16 14:57:30 +0100

    qml: add workarounds for issue assigning custom types to QObject properties
    - on the python side, for pyqtProperty's with a setter, the pyqtProperty should be declared as QVariant type
    - on the qml side, properties should be declared 'var', not the custom type.

--- electrum/gui/qml/qetxdetails.py
+++ electrum/gui/qml/qetxdetails.py
@@ -1,6 +1,6 @@
 from typing import Optional
 
-from PyQt6.QtCore import pyqtProperty, pyqtSignal, pyqtSlot, QObject
+from PyQt6.QtCore import pyqtProperty, pyqtSignal, pyqtSlot, QObject, QVariant
 
 from electrum.i18n import _
 from electrum.logging import get_logger
@@ -102,12 +102,13 @@ class QETxDetails(QObject, QtEventListener):
         self.update()
 
     walletChanged = pyqtSignal()
-    @pyqtProperty(QEWallet, notify=walletChanged)
+    @pyqtProperty(QVariant, notify=walletChanged)
     def wallet(self):
         return self._wallet
 
     @wallet.setter
     def wallet(self, wallet: QEWallet):
+        assert isinstance(wallet, QEWallet)
         if self._wallet != wallet:
             self._wallet = wallet
             self.walletChanged.emit()
@@ -165,7 +166,7 @@ class QETxDetails(QObject, QtEventListener):
     def warning(self):
         return self._sighash_danger.get_long_message()
 
-    @pyqtProperty(QEAmount, notify=detailsChanged)
+    @pyqtProperty(QVariant, notify=detailsChanged)
     def amount(self):
         return self._amount
 
