04d581800d7879e03a0cbab3d1c166ff782a74ed
Sander van Grieken <sander@outrightsolutions.nl>
2026-02-20 10:13:49 +0100

    android: update for rebase p4a, update qt to 6.10, ndk to 28
    p4a rebased on spesmilo/electrum_202602, 375a05de21b538d704174b1efeb3fc85d151f94e

--- contrib/android/p4a_recipes/pyqt6/__init__.py
+++ contrib/android/p4a_recipes/pyqt6/__init__.py
@@ -6,13 +6,13 @@ from pythonforandroid.util import load_source
 util = load_source('util', os.path.join(os.path.dirname(os.path.dirname(__file__)), 'util.py'))
 
 
-assert PyQt6Recipe._version == "6.4.2"
-assert PyQt6Recipe.depends == ['qt6', 'pyjnius', 'setuptools', 'pyqt6sip', 'hostpython3', 'pyqt_builder']
+assert PyQt6Recipe._version == "6.10.1"
+assert PyQt6Recipe.depends == ['qt6', 'pyjnius', 'setuptools', 'pyqt6sip', 'hostpython3', 'python3'], PyQt6Recipe.depends
 assert PyQt6Recipe.python_depends == []
 
 
 class PyQt6RecipePinned(util.InheritedRecipeMixin, PyQt6Recipe):
-    sha512sum = "51e5f0d028ee7984876da1653cb135d61e2c402f18b939a92477888cc7c86d3bc2889477403dee6b3d9f66519ee3236d344323493b4c2c2e658e1637b10e53bf"
+    sha512sum = "af9bb54b20fd177cf1dac5fe8fb0ff289e1e7e42716d09093d49dd99a7d8065c6b6f34784ed19e21e7e07ba0d550b270cb6be7273f7180e2bf886160fc773d01"
 
 
 recipe = PyQt6RecipePinned()
