660f2ead20153ed97771a55a6c91a02af4dbe5bb
f321x <f@f321x.com>
2026-03-05 16:57:54 +0100

    android: update Qt6 to 6.10.2, PyQt6 to 6.10.2

--- contrib/android/p4a_recipes/pyqt6/__init__.py
+++ contrib/android/p4a_recipes/pyqt6/__init__.py
@@ -6,13 +6,13 @@ from pythonforandroid.util import load_source
 util = load_source('util', os.path.join(os.path.dirname(os.path.dirname(__file__)), 'util.py'))
 
 
-assert PyQt6Recipe._version == "6.10.1"
+assert PyQt6Recipe._version == "6.10.2"
 assert PyQt6Recipe.depends == ['qt6', 'pyjnius', 'setuptools', 'pyqt6sip', 'hostpython3', 'python3'], PyQt6Recipe.depends
 assert PyQt6Recipe.python_depends == []
 
 
 class PyQt6RecipePinned(util.InheritedRecipeMixin, PyQt6Recipe):
-    sha512sum = "af9bb54b20fd177cf1dac5fe8fb0ff289e1e7e42716d09093d49dd99a7d8065c6b6f34784ed19e21e7e07ba0d550b270cb6be7273f7180e2bf886160fc773d01"
+    sha512sum = "d58515d181530fdd71edc3edfa0b647a3aeeb56cbc33f4d7fd0d40a7a99d52298ac5bb4438b5dadea5439759e52cc459e601f1fab5d9afdd61f2a492d0bae1ef"
 
 
 recipe = PyQt6RecipePinned()
--- contrib/android/p4a_recipes/qt6/__init__.py
+++ contrib/android/p4a_recipes/qt6/__init__.py
@@ -5,13 +5,13 @@ from pythonforandroid.util import load_source
 
 util = load_source('util', os.path.join(os.path.dirname(os.path.dirname(__file__)), 'util.py'))
 
-assert Qt6Recipe._version == "6.10.1"
+assert Qt6Recipe._version == "6.10.2"
 assert Qt6Recipe.depends == ['python3', 'hostqt6']
 assert Qt6Recipe.python_depends == []
 
 
 class Qt6RecipePinned(util.InheritedRecipeMixin, Qt6Recipe):
-    sha512sum = "62e8a8fcdef84187bff43e6185a1ba983e3db4d927ec01cd0ff5247d12eb7fd116a8f67323b3e44ba23f2e1792ade8c54e033cf28f34ec42a776ec204b9c2d8d"
+    sha512sum = "bf1a1d42d57b4d2e77f7227f4cbe01e847fd65035461b89481063b32f25a57be6e5a07889acc4af65ca9ff9d27b7fe63bd2fe60b8aa7fa19d554394d799fbaa1"
 
 
 recipe = Qt6RecipePinned()
