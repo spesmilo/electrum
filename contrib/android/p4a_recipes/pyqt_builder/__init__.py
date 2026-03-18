c8f5798d4ea0fa91149ebf5b602bdc883498bf6d
Sander van Grieken <sander@outrightsolutions.nl>
2026-03-18 23:46:48 +0100

    android: build pyqt_builder and sip ourselves, hash pin all hostpython_prerequisites

--- contrib/android/p4a_recipes/pyqt_builder/__init__.py
+++ contrib/android/p4a_recipes/pyqt_builder/__init__.py
@@ -1,12 +1,14 @@
 from pythonforandroid.recipes.pyqt_builder import PyQtBuilderRecipe
+from pythonforandroid.util import HashPinnedDependency
 
 
 assert PyQtBuilderRecipe._version == "1.19.1"
-assert PyQtBuilderRecipe.depends == ["sip", "packaging", "python3"]
+assert PyQtBuilderRecipe.depends == ["packaging", "sip", "python3"], PyQtBuilderRecipe.depends
 assert PyQtBuilderRecipe.python_depends == []
 
 
 class PyQtBuilderRecipePinned(PyQtBuilderRecipe):
     sha512sum = "2308c51f93c37b1d13f312e4f2475d26b22d374ef284925fead9eab4aa89b994770431aca45170ac2154b4813fff151798f113f56d4cbf6c6e544fb463104a6d"
+
 
 recipe = PyQtBuilderRecipePinned()
