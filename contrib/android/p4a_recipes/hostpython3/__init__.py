9d5b4a7cd933b0e2a132194def62d4daa39e618d
Sander van Grieken <sander@outrightsolutions.nl>
2026-03-23 18:11:50 +0100

    android: use plain 'build' dependency (using 'venv') instead of 'build[virtualenv]', remove setuptools as its use is now pinned via hostpython_prerequisites where applicable, update depends asserts in pyqt6sip, sip, pyqt_builder

--- contrib/android/p4a_recipes/hostpython3/__init__.py
+++ contrib/android/p4a_recipes/hostpython3/__init__.py
@@ -17,28 +17,18 @@ class HostPython3RecipePinned(util.InheritedRecipeMixin, HostPython3Recipe):
 
     # this property overrides the default hostpython dependencies for PyProjectRecipe recipies
     pyproject_base_dependencies = [
-        HashPinnedDependency(package="build[virtualenv]==1.4.0",
+        HashPinnedDependency(package="build==1.4.0",
                              hashes=['sha256:6a07c1b8eb6f2b311b96fcbdbce5dab5fe637ffda0fd83c9cac622e927501596']),
         HashPinnedDependency(package="pip==24.0",
                              hashes=['sha256:ba0d021a166865d2265246961bec0152ff124de910c5cc39f1156ce3fa7c69dc']),
         HashPinnedDependency(package="setuptools==80.9.0",
                              hashes=['sha256:062d34222ad13e0cc312a4c02d73f059e86a4acbfbdea8f8f76b28c99f306922']),
 
-        # pin deptree build[virtualenv]==1.4.0
+        # pin deptree build==1.4.0
         HashPinnedDependency(package="packaging==26.0",
                              hashes=['sha256:b36f1fef9334a5588b4166f8bcd26a14e521f2b55e6b9de3aaa80d3ff7a37529']),
         HashPinnedDependency(package="pyproject_hooks==1.2.0",
                              hashes=['sha256:9e5c6bfa8dcc30091c74b0cf803c81fdd29d94f01992a7707bc97babb1141913']),
-        HashPinnedDependency(package="virtualenv==21.2.0",
-                             hashes=['sha256:1bd755b504931164a5a496d217c014d098426cddc79363ad66ac78125f9d908f']),
-        HashPinnedDependency(package="distlib==0.4.0",
-                             hashes=['sha256:9659f7d87e46584a30b5780e43ac7a2143098441670ff0a49d5f9034c54a6c16']),
-        HashPinnedDependency(package="filelock==3.25.2",
-                             hashes=['sha256:ca8afb0da15f229774c9ad1b455ed96e85a81373065fb10446672f64444ddf70']),
-        HashPinnedDependency(package="platformdirs==4.9.4",
-                             hashes=['sha256:68a9a4619a666ea6439f2ff250c12a853cd1cbd5158d258bd824a7df6be2f868']),
-        HashPinnedDependency(package="python_discovery==1.1.3",
-                             hashes=['sha256:90e795f0121bc84572e737c9aa9966311b9fde44ffb88a5953b3ec9b31c6945e']),
     ]
 
 
