7b7d7028bdd207ac049ce164352a825c18134c64
Sander van Grieken <sander@outrightsolutions.nl>
2026-03-24 09:12:40 +0100

    android: hash-pin hostpython prerequisites for pyqt6sip and sip

--- contrib/android/p4a_recipes/pyqt6sip/__init__.py
+++ contrib/android/p4a_recipes/pyqt6sip/__init__.py
@@ -1,7 +1,7 @@
 import os
 
 from pythonforandroid.recipes.pyqt6sip import PyQt6SipRecipe
-from pythonforandroid.util import load_source
+from pythonforandroid.util import load_source, HashPinnedDependency
 
 util = load_source('util', os.path.join(os.path.dirname(os.path.dirname(__file__)), 'util.py'))
 
@@ -13,6 +13,13 @@ assert PyQt6SipRecipe.python_depends == []
 
 class PyQt6SipRecipePinned(util.InheritedRecipeMixin, PyQt6SipRecipe):
     sha512sum = "555b061eec3db6a66388fae07de21f58d756f6f12b13e4ede729c3348d2c8997ac5a59d3006ee45c3a09b5cde673f579265fa254bc583a4ba721748cf8f3a617"
+
+    hostpython_prerequisites = [
+        HashPinnedDependency(package="setuptools==80.9.0",
+                             hashes=['sha256:062d34222ad13e0cc312a4c02d73f059e86a4acbfbdea8f8f76b28c99f306922']),
+        HashPinnedDependency(package="packaging==26.0",
+                             hashes=['sha256:b36f1fef9334a5588b4166f8bcd26a14e521f2b55e6b9de3aaa80d3ff7a37529']),
+    ]
 
 
 recipe = PyQt6SipRecipePinned()
--- contrib/android/p4a_recipes/sip/__init__.py
+++ contrib/android/p4a_recipes/sip/__init__.py
@@ -1,5 +1,5 @@
 from pythonforandroid.recipes.sip import SipRecipe
-
+from pythonforandroid.util import HashPinnedDependency
 
 assert SipRecipe._version == "6.15.1"
 assert SipRecipe.depends == ["python3"], SipRecipe.depends
@@ -8,6 +8,11 @@ assert SipRecipe.python_depends == []
 
 class SipRecipePinned(SipRecipe):
     sha512sum = "30a312419ba82c0221c0cf03c3fb3ad7d45bb8fe633d1d7477025a7986b0a7f7b7b781a8d9cd6bcdb78f3b872231fd1eed123a761b497861822f2e35093f574d"
+
+    hostpython_prerequisites = [
+        HashPinnedDependency(package="setuptools==80.9.0",
+                             hashes=['sha256:062d34222ad13e0cc312a4c02d73f059e86a4acbfbdea8f8f76b28c99f306922']),
+    ]
 
 
 recipe = SipRecipePinned()
