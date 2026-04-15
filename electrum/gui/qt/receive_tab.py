9057d8b40d64d573bef04cf6134fe32f39ddff3c
Sander van Grieken <sander@outrightsolutions.nl>
2026-04-15 16:13:20 +0200

    wallet: kwarg all parameters of wallet.create_request() add amount_msat parameter and make mutually exclusive with amount_sat

--- electrum/gui/qt/receive_tab.py
+++ electrum/gui/qt/receive_tab.py
@@ -315,7 +315,7 @@ class ReceiveTab(QWidget, MessageBoxMixin, Logger):
 
         # generate even if we cannot receive
         try:
-            key = self.wallet.create_request(amount_sat, message, expiry, address)
+            key = self.wallet.create_request(amount_sat=amount_sat, message=message, exp_delay=expiry, address=address)
         except InvoiceError as e:
             self.show_error(_('Error creating payment request') + ':\n' + str(e))
             return
