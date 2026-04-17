958cc28272f662732d5b158b3ac4ebacfbfbce76
Sander van Grieken <sander@outrightsolutions.nl>
2026-04-17 12:09:52 +0200

    qml: in QEAmount, keep msat and sat synchronized, and be more strict with parameter combinations to constructor.
    in QEConfig, add formatMilliSatsForEditing, update formatMilliSats passing Decimal to config.format_amount/_and_units

--- electrum/gui/qml/qetypes.py
+++ electrum/gui/qml/qetypes.py
@@ -1,3 +1,5 @@
+from decimal import Decimal
+
 from PyQt6.QtCore import pyqtProperty, pyqtSignal, pyqtSlot, QObject
 
 from electrum.logging import get_logger
@@ -7,7 +9,6 @@ from electrum.i18n import _
 class QEAmount(QObject):
     """Container for bitcoin amounts that can be passed around more
        easily between python, QML-property and QML-javascript contexts.
-       Note: millisat and sat amounts are not synchronized!
 
        QML type 'int' in property definitions is 32 bit signed, so will overflow easily
        on (milli)satoshi amounts! 'int' in QML-javascript seems to be larger than 32 bit, and
@@ -20,32 +21,50 @@ class QEAmount(QObject):
 
     _logger = get_logger(__name__)
 
-    def __init__(self, *, amount_sat: int = 0, amount_msat: int = 0, is_max: bool = False, from_invoice=None, parent=None):
+    valueChanged = pyqtSignal()
+
+    def __init__(self, *, amount_sat: int = None, amount_msat: int = None, is_max: bool = False, from_invoice=None, parent=None):
         super().__init__(parent)
-        self._amount_sat = int(amount_sat) if amount_sat is not None else None
-        self._amount_msat = int(amount_msat) if amount_msat is not None else None
+
+        self._amount_msat = None
+        if amount_sat is not None:
+            assert isinstance(amount_sat, int)
+            self._amount_msat = self._sat_to_msat(amount_sat)
+        if amount_msat is not None:
+            assert isinstance(amount_msat, int)
+            if amount_sat is not None:
+                assert amount_sat == self._msat_to_sat(amount_msat)  # if both defined, assert conversion is as expected
+            self._amount_msat = amount_msat
+        if is_max:
+            assert amount_sat is None and amount_msat is None
+
         self._is_max = is_max
         if from_invoice:
+            assert amount_sat is None and amount_msat is None, 'cannot combine from_invoice and amount_(m)sat'
             inv_amt = from_invoice.get_amount_msat()
             if inv_amt == '!':
                 self._is_max = True
             elif inv_amt is not None:
                 self._amount_msat = int(inv_amt)
-                self._amount_sat = int(from_invoice.get_amount_sat())
 
-    valueChanged = pyqtSignal()
+    def _sat_to_msat(self, amount_sat: int | None) -> int | None:
+        return amount_sat * 1000 if amount_sat is not None else None
+
+    def _msat_to_sat(self, amount_msat: int | None) -> int | None:
+        return int(Decimal(amount_msat) / 1000) if amount_msat is not None else None
 
     @pyqtProperty('qint64', notify=valueChanged)
     def satsInt(self):
-        if self._amount_sat is None:  # should normally be defined when accessing this property
-            self._logger.warning('amount_sat is undefined, returning 0')
+        if self._amount_msat is None:  # should normally be defined when accessing this property
+            self._logger.warning('amount_msat is undefined, returning 0')
             return 0
-        return self._amount_sat
+        return self._msat_to_sat(self._amount_msat)
 
     @satsInt.setter
     def satsInt(self, sats):
-        if self._amount_sat != sats:
-            self._amount_sat = sats
+        msats = self._sat_to_msat(sats)
+        if self._amount_msat != msats:
+            self._amount_msat = msats
             self.valueChanged.emit()
 
     @pyqtProperty('qint64', notify=valueChanged)
@@ -63,7 +82,7 @@ class QEAmount(QObject):
 
     @pyqtProperty(str, notify=valueChanged)
     def satsStr(self):
-        return str(self._amount_sat)
+        return str(self._msat_to_sat(self._amount_msat))
 
     @pyqtProperty(str, notify=valueChanged)
     def msatsStr(self):
@@ -81,11 +100,14 @@ class QEAmount(QObject):
 
     @pyqtProperty(bool, notify=valueChanged)
     def isEmpty(self):
-        return not(self._is_max or self._amount_sat or self._amount_msat)
+        return not (self._is_max or self._amount_msat)
+
+    @pyqtProperty(bool, notify=valueChanged)
+    def hasMsatPrecision(self):
+        return not (self._amount_msat == self._sat_to_msat(self._msat_to_sat(self._amount_msat)))
 
     @pyqtSlot()
     def clear(self):
-        self._amount_sat = 0
         self._amount_msat = 0
         self._is_max = False
         self.valueChanged.emit()
@@ -95,17 +117,22 @@ class QEAmount(QObject):
         if not amount:
             self._logger.warning('copyFrom with None argument. assuming 0')  # TODO
             amount = QEAmount()
-        self.satsInt = amount.satsInt
-        self.msatsInt = amount.msatsInt
-        self.isMax = amount.isMax
+
+        changed = False
+        if self._amount_msat != amount._amount_msat:
+            self._amount_msat = amount._amount_msat
+            changed = True
+        if self._is_max != amount._is_max:
+            self._is_max = amount._is_max
+            changed = True
+        if changed:
+            self.valueChanged.emit()
 
     def __eq__(self, other):
         if isinstance(other, QEAmount):
-            return self._amount_sat == other._amount_sat and self._amount_msat == other._amount_msat and self._is_max == other._is_max
+            return self._amount_msat == other._amount_msat and self._is_max == other._is_max
         elif isinstance(other, int):
-            return self._amount_sat == other
-        elif isinstance(other, str):
-            return self.satsStr == other
+            return self._amount_msat == other
 
         return False
 
@@ -113,10 +140,10 @@ class QEAmount(QObject):
         s = _('Amount')
         if self._is_max:
             return '%s(MAX)' % s
-        return '%s(sats=%d, msats=%d)' % (s, self._amount_sat, self._amount_msat)
+        return '%s(sats=%s, msats=%s)' % (s, str(self._msat_to_sat(self._amount_msat)), str(self._amount_msat))
 
     def __repr__(self):
-        return f"<QEAmount max={self._is_max} sats={self._amount_sat} msats={self._amount_msat} empty={self.isEmpty}>"
+        return f"<QEAmount max={self._is_max} sats={self._msat_to_sat(self._amount_msat)} msats={self._amount_msat} empty={self.isEmpty}>"
 
 
 class QEBytes(QObject):
