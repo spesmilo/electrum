58c94a6d751430667d57f76dc807e540eba19bb0
Sander van Grieken <sander@outrightsolutions.nl>
2026-04-19 13:44:24 +0200

    qml: only use QEAmount container instead of javascript/qml (q(u))int(64)
    Using javascript or QML `int` type is a long-standing issue, as there is not enough range to express millisats or even sats.
    A short summary of the issues:
    - integers in javascript context have a max range of +/- 2^54-1, which allows 21M BTC expressed in sats, but not in msats.
    - QML `int` properties have a max range of +/- 2^31-1, which only allows 21 BTC expressed in sats, 21 mBTC expressed in msats
    - `int` parameters declared in the `pyqtProperty` and `pyqtSlot` decorators have a max range of 2^31-1,
    although this is somewhat alleviated in the QML->Python direction by using `q(u)int64`. Returning a `q(u)int64` does not
    work around the `int` limitation
    
    In most of the QML code, `QEAmount` is already used for storing and passing around BTC values.
    The only exception is where amounts are compared (e.g. invoice amount < available balance etc),
    so the `<`, `>`, `<=`, `>=` and `!=` operators, and where these operators are implied, like `Math.min()` and `Math.max()`
    
    This commit delegates these operators to python scope.

--- electrum/gui/qml/qetypes.py
+++ electrum/gui/qml/qetypes.py
@@ -23,7 +23,7 @@ class QEAmount(QObject):
 
     valueChanged = pyqtSignal()
 
-    def __init__(self, *, amount_sat: int = None, amount_msat: int = None, is_max: bool = False, from_invoice=None, parent=None):
+    def __init__(self, parent=None, *, amount_sat: int = None, amount_msat: int = None, is_max: bool = False, from_invoice=None):
         super().__init__(parent)
 
         self._amount_msat = None
@@ -54,57 +54,63 @@ class QEAmount(QObject):
         return int(Decimal(amount_msat) / 1000) if amount_msat is not None else None
 
     @pyqtProperty('qint64', notify=valueChanged)
-    def satsInt(self):
+    def satsInt(self) -> int:
         if self._amount_msat is None:  # should normally be defined when accessing this property
             self._logger.warning('amount_msat is undefined, returning 0')
             return 0
         return self._msat_to_sat(self._amount_msat)
 
     @satsInt.setter
-    def satsInt(self, sats):
+    def satsInt(self, sats: int):
+        assert sats is None or isinstance(sats, int), 'sats must be int or None'
         msats = self._sat_to_msat(sats)
         if self._amount_msat != msats:
             self._amount_msat = msats
             self.valueChanged.emit()
 
     @pyqtProperty('qint64', notify=valueChanged)
-    def msatsInt(self):
+    def msatsInt(self) -> int:
         if self._amount_msat is None:  # should normally be defined when accessing this property
             self._logger.warning('amount_msat is undefined, returning 0')
             return 0
         return self._amount_msat
 
     @msatsInt.setter
-    def msatsInt(self, msats):
+    def msatsInt(self, msats: int):
+        assert msats is None or isinstance(msats, int), 'msats must be int or None'
         if self._amount_msat != msats:
             self._amount_msat = msats
             self.valueChanged.emit()
 
     @pyqtProperty(str, notify=valueChanged)
-    def satsStr(self):
+    def satsStr(self) -> str:
         return str(self._msat_to_sat(self._amount_msat))
 
     @pyqtProperty(str, notify=valueChanged)
-    def msatsStr(self):
+    def msatsStr(self) -> str:
         return str(self._amount_msat)
 
     @pyqtProperty(bool, notify=valueChanged)
-    def isMax(self):
+    def isMax(self) -> bool:
         return self._is_max
 
     @isMax.setter
-    def isMax(self, ismax):
+    def isMax(self, ismax: bool):
         if self._is_max != ismax:
             self._is_max = ismax
             self.valueChanged.emit()
 
     @pyqtProperty(bool, notify=valueChanged)
-    def isEmpty(self):
+    def isEmpty(self) -> bool:
         return not (self._is_max or self._amount_msat)
 
     @pyqtProperty(bool, notify=valueChanged)
-    def hasMsatPrecision(self):
+    def hasMsatPrecision(self) -> bool:
         return not (self._amount_msat == self._sat_to_msat(self._msat_to_sat(self._amount_msat)))
+
+    @pyqtProperty(bool, notify=valueChanged)
+    def positive(self) -> bool:
+        return self.isEmpty or self.isMax or self.msatsInt >= 0
 
     @pyqtSlot()
     def clear(self):
@@ -113,7 +119,7 @@ class QEAmount(QObject):
         self.valueChanged.emit()
 
     @pyqtSlot('QVariant')
-    def copyFrom(self, amount):
+    def copyFrom(self, amount: 'QEAmount|None'):
         if not amount:
             self._logger.warning('copyFrom with None argument. assuming 0')  # TODO
             amount = QEAmount()
@@ -128,21 +134,87 @@ class QEAmount(QObject):
         if changed:
             self.valueChanged.emit()
 
-    def __eq__(self, other):
-        if isinstance(other, QEAmount):
-            return self._amount_msat == other._amount_msat and self._is_max == other._is_max
-        elif isinstance(other, int):
-            return self._amount_msat == other
+    @pyqtSlot('QVariant', result=bool)
+    def lt(self, other: 'QEAmount|None') -> bool:
+        if other is None:
+            other = QEAmount()
+        assert isinstance(other, QEAmount)
+        assert not (self.isMax or other.isMax), "'lt/lte' operator undefined for MAX amounts"
+        if self.isEmpty and not other.isEmpty:
+            return True
+        return self.msatsInt < other.msatsInt
 
-        return False
+    @pyqtSlot('QVariant', result=bool)
+    def lte(self, other: 'QEAmount|None') -> bool:
+        return self.lt(other) or self == other
 
-    def __str__(self):
+    @pyqtSlot('QVariant', result=bool)
+    def gt(self, other: 'QEAmount|None') -> bool:
+        if other is None:
+            other = QEAmount()
+        assert isinstance(other, QEAmount)
+        assert not (self.isMax or other.isMax), "'gt/gte' operator undefined for MAX amounts"
+        if self.isEmpty and not other.isEmpty:
+            return False
+        return self.msatsInt > other.msatsInt
+
+    @pyqtSlot('QVariant', result=bool)
+    def gte(self, other: 'QEAmount|None') -> bool:
+        return self.gt(other) or self == other
+
+    @pyqtSlot('QVariant', result=bool)
+    def eq(self, other: 'QEAmount|None') -> bool:
+        return self == other
+
+    @pyqtSlot('QVariant', 'QVariant', result='QVariant')
+    def max(self, one: 'QEAmount|None', two: 'QEAmount|None'):
+        if one is None:
+            one = QEAmount()
+        if two is None:
+            two = QEAmount()
+        assert isinstance(one, QEAmount)
+        assert isinstance(two, QEAmount)
+
+        # TODO: as gt/lt is undefined for operands being isMax, we can either
+        # - raise (let the GUI avoid comparisons against MAX)
+        # - define MAX as always being larger than any value
+        if one.isMax:
+            return one
+        if two.isMax:
+            return two
+
+        return one if one.gt(two) else two
+
+    @pyqtSlot('QVariant', 'QVariant', result='QVariant')
+    def min(self, one: 'QEAmount|None', two: 'QEAmount|None'):
+        if one is None:
+            one = QEAmount()
+        if two is None:
+            two = QEAmount()
+        assert isinstance(one, QEAmount)
+        assert isinstance(two, QEAmount)
+        # TODO: as gt/lt is undefined for operands being isMax, we can either
+        # - raise (let the GUI avoid comparisons against MAX)
+        # - define MAX as always being larger than any value
+        if one.isMax:
+            return two
+        if two.isMax:
+            return one
+        return one if one.lt(two) else two
+
+    def __eq__(self, other: 'QEAmount') -> bool:
+        assert True if other is None else isinstance(other, QEAmount)
+        if other is None:
+            return False
+        return self._amount_msat == other._amount_msat and self._is_max == other._is_max
+
+    def __str__(self) -> str:
         s = _('Amount')
         if self._is_max:
             return '%s(MAX)' % s
         return '%s(sats=%s, msats=%s)' % (s, str(self._msat_to_sat(self._amount_msat)), str(self._amount_msat))
 
-    def __repr__(self):
+    def __repr__(self) -> str:
         return f"<QEAmount max={self._is_max} sats={self._msat_to_sat(self._amount_msat)} msats={self._amount_msat} empty={self.isEmpty}>"
 
 
