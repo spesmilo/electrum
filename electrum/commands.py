f321x/electrum-fork

bolt12/lnworker: implement creating offer
Implement initial LNWallet method `create_offer`
to create bolt12 offer for sharing with external parties.
Add CLI command `add_offer` to use this method.

electrum/commands.py
@@ -1386,6 +1386,37 @@ class Commands(Logger):
         return wallet.export_request(req)
 
     @command('wnl')
+    async def add_lightning_offer(
+            self,
+            amount: Optional[Decimal] = None,
+            description: Optional[str] = None,
+            relative_expiry: Optional[int] = None,
+            issuer_name: Optional[str] = None,
+            allow_unblined: bool = False,
+            wallet: Abstract_Wallet = None
+    ):
+        """Create a bolt12 offer.
+
+        arg:decimal:amount:Requested amount (in btc)
+        arg:str:description:Description of the request
+        arg:int:relative_expiry:Time in seconds.
+        arg:str:issuer_name:Issuer name string
+        arg:bool:allow_unblinded:Allow revealing node id for unblinded offers
+        """
+        amount_msat = satoshis(amount) * 1000 if amount else None
+        offer = wallet.lnworker.create_offer(
+            amount_msat=amount_msat,
+            description=description,
+            relative_expiry=relative_expiry,
+            issuer_name=issuer_name,
+            allow_unblinded=allow_unblined,
+        )
+
+        return {
+            'offer': offer.encode(as_bech32=True)
+        }
+
+    @command('wnl')
     async def add_hold_invoice(
             self,
             payment_hash: str,

tests/test_commands.py
@@ -21,7 +21,9 @@ from electrum.transaction import Transaction, TxOutput, tx_from_any
 from electrum.util import UserFacingException, NotEnoughFunds
 from electrum.crypto import sha256
 from electrum.bolt11 import decode_bolt11_invoice
+from electrum.bolt12 import BOLT12Offer
 from electrum.daemon import Daemon
+from electrum.bitcoin import COIN
 from electrum import json_db
 
 from . import ElectrumTestCase
@@ -784,3 +786,28 @@ class TestCommandsTestnet(ElectrumTestCase):
         result = await cmds.add_peer(connection_string=connection_string, wallet=w)
         assert called == 2
         self.assertTrue(result)
+
+    @mock.patch.object(wallet.Abstract_Wallet, 'save_db')
+    async def test_add_lightning_offer(self, *mock_args):
+        wallet: Abstract_Wallet = restore_wallet_from_text__for_unittest(
+            'disagree rug lemon bean unaware square alone beach tennis exhibit fix mimic',
+            path='if_this_exists_mocking_failed_648151893',
+            config=self.config)['wallet']
+        cmds = Commands(config=self.config)
+
+        bolt12_offer = (await cmds.add_lightning_offer(
+            amount=Decimal(0.001),
+            description="test cli",
+            relative_expiry=None,
+            issuer_name="me",
+            allow_unblined=True,
+            wallet=wallet,
+        ))['offer']
+        decoded_offer = BOLT12Offer.decode(bolt12_offer)
+        self.assertEqual(len(decoded_offer.offer_metadata), 40)
+        self.assertEqual(decoded_offer.offer_amount, 100000000)
+        self.assertEqual(decoded_offer.offer_description, "test cli")
+        self.assertEqual(decoded_offer.offer_absolute_expiry, None)
+        self.assertEqual(decoded_offer.offer_issuer, "me")
+        self.assertEqual(decoded_offer.offer_issuer_id, wallet.lnworker.node_keypair.pubkey)
+        self.assertEqual(decoded_offer.offer_paths, None)

spesmilo/electrum

electrum/commands.py
@@ -1389,6 +1389,37 @@ class Commands(Logger):
         return wallet.export_request(req)
 
     @command('wnl')
+    async def add_lightning_offer(
+            self,
+            amount: Optional[Decimal] = None,
+            description: Optional[str] = None,
+            relative_expiry: Optional[int] = None,
+            issuer_name: Optional[str] = None,
+            allow_unblinded: bool = False,
+            wallet: Abstract_Wallet = None
+    ):
+        """Create a bolt12 offer.
+
+        arg:decimal:amount:Requested amount (in btc)
+        arg:str:description:Description of the request
+        arg:int:relative_expiry:Time in seconds.
+        arg:str:issuer_name:Issuer name string
+        arg:bool:allow_unblinded:Allow revealing node id for unblinded offers
+        """
+        amount_msat = satoshis(amount) * 1000 if amount else None
+        offer = wallet.lnworker.create_offer(
+            amount_msat=amount_msat,
+            description=description,
+            relative_expiry=relative_expiry,
+            issuer_name=issuer_name,
+            allow_unblinded=allow_unblinded,
+        )
+
+        return {
+            'offer': offer.encode(as_bech32=True)
+        }
+
+    @command('wnl')
     async def add_hold_invoice(
             self,
             payment_hash: str,

tests/test_commands.py
@@ -23,6 +23,7 @@ from electrum.transaction import Transaction, TxOutput, tx_from_any
 from electrum.util import UserFacingException, NotEnoughFunds
 from electrum.crypto import sha256
 from electrum.bolt11 import decode_bolt11_invoice
+from electrum.bolt12 import BOLT12Offer
 from electrum.daemon import Daemon
 from electrum import json_db
 
@@ -914,3 +915,28 @@ class TestCommandsTestnet(ElectrumTestCase):
 
         # set back to Testnet
         constants.net = prev_net
+
+    @mock.patch.object(wallet.Abstract_Wallet, 'save_db')
+    async def test_add_lightning_offer(self, *mock_args):
+        wallet: Abstract_Wallet = restore_wallet_from_text__for_unittest(
+            'disagree rug lemon bean unaware square alone beach tennis exhibit fix mimic',
+            path='if_this_exists_mocking_failed_648151893',
+            config=self.config)['wallet']
+        cmds = Commands(config=self.config)
+
+        bolt12_offer = (await cmds.add_lightning_offer(
+            amount=Decimal(0.001),
+            description="test cli",
+            relative_expiry=None,
+            issuer_name="me",
+            allow_unblinded=True,
+            wallet=wallet,
+        ))['offer']
+        decoded_offer = BOLT12Offer.decode(bolt12_offer)
+        self.assertEqual(len(decoded_offer.offer_metadata), 40)
+        self.assertEqual(decoded_offer.offer_amount, 100000000)
+        self.assertEqual(decoded_offer.offer_description, "test cli")
+        self.assertEqual(decoded_offer.offer_absolute_expiry, None)
+        self.assertEqual(decoded_offer.offer_issuer, "me")
+        self.assertEqual(decoded_offer.offer_issuer_id, wallet.lnworker.node_keypair.pubkey)
+        self.assertEqual(decoded_offer.offer_paths, None)