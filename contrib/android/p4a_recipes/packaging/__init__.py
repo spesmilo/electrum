6c5b7bf9f3e82ca763136d2d8a31b1f80d8815c9
SomberNight <somber.night@protonmail.com>
2026-07-04 16:14:29 +0000

    android build: build p4a recipes with --no-isolation
    the core change is https://github.com/spesmilo/python-for-android/commit/8c0fcc9ef2e559918ca96ecde6e09fe521bb1427
    the rest here just follow that

--- contrib/android/p4a_recipes/packaging/__init__.py
+++ contrib/android/p4a_recipes/packaging/__init__.py
@@ -1,4 +1,5 @@
 from pythonforandroid.recipes.packaging import PackagingRecipe
+from pythonforandroid.util import HashPinnedDependency
 
 
 assert PackagingRecipe._version == "26.0"
@@ -8,6 +9,10 @@ assert PackagingRecipe.python_depends == []
 
 class PackagingRecipePinned(PackagingRecipe):
     sha512sum = "27a066a7d65ba76189212973b6a0d162f3d361848b1b0c34a82865cf180b3284a837cc34206c297f002a73feae414e25a26c5960bb884a74ea337f582585f1d2"
+    hostpython_prerequisites = [
+        HashPinnedDependency(package="flit-core==3.12.0",
+                             hashes=['sha256:e7a0304069ea895172e3c7bb703292e992c5d1555dd1233ab7b5621b5b69e62c']),
+    ]
 
 
 recipe = PackagingRecipePinned()
--- contrib/android/p4a_recipes/sip/__init__.py
+++ contrib/android/p4a_recipes/sip/__init__.py
@@ -2,7 +2,7 @@ from pythonforandroid.recipes.sip import SipRecipe
 from pythonforandroid.util import HashPinnedDependency
 
 assert SipRecipe._version == "6.15.1"
-assert SipRecipe.depends == ["python3"], SipRecipe.depends
+assert SipRecipe.depends == ["python3", "packaging"], SipRecipe.depends
 assert SipRecipe.python_depends == []
 
 
@@ -12,6 +12,11 @@ class SipRecipePinned(SipRecipe):
     hostpython_prerequisites = [
         HashPinnedDependency(package="setuptools==80.9.0",
                              hashes=['sha256:062d34222ad13e0cc312a4c02d73f059e86a4acbfbdea8f8f76b28c99f306922']),
+        HashPinnedDependency(package="setuptools-scm==8.3.1",
+                             hashes=['sha256:332ca0d43791b818b841213e76b1971b7711a960761c5bea5fc5cdb5196fbce3']),
+        HashPinnedDependency(package="packaging==26.0",  # pulled in by setuptools-scm
+                             hashes=['sha256:b36f1fef9334a5588b4166f8bcd26a14e521f2b55e6b9de3aaa80d3ff7a37529']),
+
     ]
 
 
