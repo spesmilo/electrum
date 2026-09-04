f321x/electrum-fork

bolt12/lnworker: implement creating offer
Implement initial LNWallet method `create_offer`
to create bolt12 offer for sharing with external parties.
Add CLI command `add_offer` to use this method.

electrum/commands.py
@@ -1386,6 +1386,37 @@ class Commands(Logger):
         return wallet.export_request(req)
 
     @command('wnl')
+    async def add_lightning_offer(
+            self,
+            amount: Optional[Decimal] = None,
+            description: Optional[str] = None,
+            relative_expiry: Optional[int] = None,
+            issuer_name: Optional[str] = None,
+            allow_unblined: bool = False,
+            wallet: Abstract_Wallet = None
+    ):
+        """Create a bolt12 offer.
+
+        arg:decimal:amount:Requested amount (in btc)
+        arg:str:description:Description of the request
+        arg:int:relative_expiry:Time in seconds.
+        arg:str:issuer_name:Issuer name string
+        arg:bool:allow_unblinded:Allow revealing node id for unblinded offers
+        """
+        amount_msat = satoshis(amount) * 1000 if amount else None
+        offer = wallet.lnworker.create_offer(
+            amount_msat=amount_msat,
+            description=description,
+            relative_expiry=relative_expiry,
+            issuer_name=issuer_name,
+            allow_unblinded=allow_unblined,
+        )
+
+        return {
+            'offer': offer.encode(as_bech32=True)
+        }
+
+    @command('wnl')
     async def add_hold_invoice(
             self,
             payment_hash: str,

spesmilo/electrum

electrum/commands.py
@@ -1403,6 +1403,37 @@ class Commands(Logger):
         return wallet.export_request(req)
 
     @command('wnl')
+    async def add_lightning_offer(
+            self,
+            amount: Optional[Decimal] = None,
+            description: Optional[str] = None,
+            relative_expiry: Optional[int] = None,
+            issuer_name: Optional[str] = None,
+            allow_unblinded: bool = False,
+            wallet: Abstract_Wallet = None
+    ):
+        """Create a bolt12 offer.
+
+        arg:decimal:amount:Requested amount (in btc)
+        arg:str:description:Description of the request
+        arg:int:relative_expiry:Time in seconds.
+        arg:str:issuer_name:Issuer name string
+        arg:bool:allow_unblinded:Allow revealing node id for unblinded offers
+        """
+        amount_msat = satoshis(amount) * 1000 if amount else None
+        offer = wallet.lnworker.create_offer(
+            amount_msat=amount_msat,
+            description=description,
+            relative_expiry=relative_expiry,
+            issuer_name=issuer_name,
+            allow_unblinded=allow_unblinded,
+        )
+
+        return {
+            'offer': offer.encode(as_bech32=True)
+        }
+
+    @command('wnl')
     async def add_hold_invoice(
             self,
             payment_hash: str,