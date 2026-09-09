fced17e7d1c7ef735d8fa4b6b5942b86f4b8b0d9
SomberNight <somber.night@protonmail.com>
2026-09-09 15:17:43 +0000

    android build: bump openssl (3.0.21 -> 3.0.22)

--- contrib/android/p4a_recipes/openssl/__init__.py
+++ contrib/android/p4a_recipes/openssl/__init__.py
@@ -12,8 +12,8 @@ assert OpenSSLRecipe.python_depends == []
 
 
 class OpenSSLRecipePinned(util.InheritedRecipeMixin, OpenSSLRecipe):
-    version = "3.0.21"
-    sha512sum = "9be1c8c11a2d55417bb177804d1b5369aa54a9dcd2e136929509457d549697407ae9611079e94c61b58a95be598ce35b94edb336e050d1019e7269f4d2f52cda"
+    version = "3.0.22"
+    sha512sum = "88f5cffd8949b9e236f989f414eefcd4e924145b9e8cb303a969534908e055a9ff2a4b29bd1619425c84240d9ae40aab0d84dbee0f338d20fac81cf7b2f20492"
 
 
 recipe = OpenSSLRecipePinned()
