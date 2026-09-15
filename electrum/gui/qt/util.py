c169f2073191a10202831bc23033c711ba6902cd
SomberNight <somber.night@protonmail.com>
2026-09-15 16:37:41 +0000

    qt: util.custom_message_box: use explicit RichText instead of AutoText
    - InjectNoRichTextEventFilter is changing AutoText to PlainText,
      and that also affects the internal QLabel inside the message box

--- electrum/gui/qt/util.py
+++ electrum/gui/qt/util.py
@@ -384,11 +384,7 @@ def custom_message_box(
     d.setDefaultButton(defaultButton)
     if rich_text:
         d.setTextInteractionFlags(Qt.TextInteractionFlag.TextSelectableByMouse | Qt.TextInteractionFlag.LinksAccessibleByMouse)
-        # set AutoText instead of RichText
-        # AutoText lets Qt figure out whether to render as rich text.
-        # e.g. if text is actually plain text and uses "\n" newlines;
-        #      and we set RichText here, newlines would be swallowed
-        d.setTextFormat(Qt.TextFormat.AutoText)
+        d.setTextFormat(Qt.TextFormat.RichText)
     else:
         d.setTextInteractionFlags(Qt.TextInteractionFlag.TextSelectableByMouse)
         d.setTextFormat(Qt.TextFormat.PlainText)
