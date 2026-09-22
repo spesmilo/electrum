dd0596f4ae2f6a2caa556212f334deb8dd206e48
f321x <f@f321x.com>
2026-09-22 11:24:26 +0200

    lnwallet: onchain backup: prevent picking incorrect funding address
    Prevent picking an incorrect funding address when analyzing
    a funding transaction with multiple p2wsh outputs for an
    onchain backup.
    We don't create such funding transactions right now, but the fix is simple
    and seems sane to do anyway.

--- electrum/lnworker.py
+++ electrum/lnworker.py
@@ -4012,16 +4012,17 @@ class LNWallet(Logger):
             return
         funding_address = None
         node_id_prefix = None
+        # note: loop is quadratic but that's ok as we require at least 1 tx input to be is_mine
         for i, o in enumerate(tx.outputs()):
             script_type = get_script_type_from_output_script(o.scriptpubkey)
             if script_type == 'p2wsh':
-                funding_index = i
-                funding_address = o.address
                 for o2 in tx.outputs():
                     if o2.scriptpubkey.startswith(bytes([opcodes.OP_RETURN])):
                         encrypted_data = o2.scriptpubkey[2:]
-                        data = self.decrypt_cb_data(encrypted_data, funding_address)
+                        data = self.decrypt_cb_data(encrypted_data, o.address)
                         if data.startswith(CB_MAGIC_BYTES):
+                            funding_index = i
+                            funding_address = o.address
                             node_id_prefix = data[len(CB_MAGIC_BYTES):]
         if node_id_prefix is None:
             return
